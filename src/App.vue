3a45348c4b5ab63d2388279dbce737775c77993d
Bastian Haverkamp <bastihav@mail.upb.de>
2020-05-29 10:37:52 +0200

    setup vue project
    Co-authored-by: tobias-wiese <tobiaswiese@online.de>
    Co-authored-by: Theo Harkenbusch<theo@harkenbusch.com>

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <i class="fas fa-university text-3xl mr-3" style="color: #97D6D7;"></i>
+     <h1 class="text-3xl text-blue-300">Theo stinkt</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,11 @@
+// import Home from './components/Home.vue';
+// import Register from './components/Register.vue';
+// import Login from './components/Login.vue';
+
+// const routes = [
+//     { path: '/', component: Home },
+//     { path: '/register', component: Register },
+//     { path: '/login', component: Login },
+// ];
+
+// export default routes;
