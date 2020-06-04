f16c3d82f05f5ad2d86cc7511b1d97562678252f
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-04 15:33:26 +0200

    Correctly add get courses

--- src/views/student/Home.vue
+++ src/views/student/Home.vue
@@ -2,46 +2,20 @@
   <div>
     <div class="mt-32 text-4xl text-center font-semibold text-gray-900">Available Courses</div>
     <div class="mt-8 flex justify-center">
-      <course-list :courses="courses"></course-list>
+      <Suspense>
+      <course-list :courses="{courses}"></course-list>
+      </Suspense>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import CourseList from "../../components/StudentCourseList.vue";
-import { onMounted } from "vue";
 
 export default {
   name: "Student.Home",
   components: {
     CourseList
-  },
-  setup() {
-    var courses = {}
-    onMounted(() => {
-      const axios = require("axios");
-      const instance = axios.create({
-        baseURL: "http://192.168.0.143:9000",
-        headers: {
-          "Accept": "*/*",
-          "Content-Type": "application/json;charset=UTF-8"
-        }
-      });
-
-      instance
-        .get("/course")
-        .then(function(response) {
-          console.log(response);
-          courses=response.data
-          console.log(courses)
-        })
-        .catch(function(error: object) {
-          console.log(error);
-        });
-    });
-    return {
-        courses
-    }
   },
   data: () => ({
   })
