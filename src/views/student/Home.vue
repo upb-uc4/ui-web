e5166d3990a4d1cad2d38d46c8bb5c05b8361941
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-04 12:45:34 +0200

    add first api call for debugging

--- src/views/student/Home.vue
+++ src/views/student/Home.vue
@@ -1,18 +1,39 @@
 <template>
-    <div>
-        <div class="mt-32 text-4xl text-center font-semibold text-gray-900">Available Courses</div>
-        <div class="mt-8 flex justify-center">
-            <course-list :courses="courses"></course-list>
-        </div>
+  <div>
+    <div class="mt-32 text-4xl text-center font-semibold text-gray-900">Available Courses</div>
+    <div class="mt-8 flex justify-center">
+      <course-list :courses="courses"></course-list>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import CourseList from "../../components/StudentCourseList.vue";
+import {onMounted} from 'vue'
+
 export default {
     name: 'Student.Home',
     components: {
         CourseList,
+    },
+    setup() {
+        onMounted(() => {
+        const axios = require("axios");
+        const instance = axios.create({
+            baseURL: 'http://192.168.0.143:9000',
+            timeout: 1000
+        });
+
+        instance.get('/course')
+        .then(function(response: object) {
+            console.log(response);
+        }).catch(function(error: object){
+            console.log(error);
+        }).then(function(){
+
+        });
+        return;
+    });
     },
     data: () => ({
         courses: [
@@ -64,6 +85,6 @@ export default {
                 endDate: 'Sat, 27.06.2020 [17:00]',
             },
         ],
-    })
+    }),
 }
 </script>
