9ef3da94f2a3ca78284adc402ac3c26af38455ea
theovier <Theo@Harkenbusch.com>
2020-06-06 00:01:47 +0200

    change font from semibold to medium

--- src/views/lecturer/CreateCourseForm.vue
+++ src/views/lecturer/CreateCourseForm.vue
@@ -13,42 +13,42 @@
             <section class="border-t-2 py-8 border-gray-400">
                 <div class="lg:flex">
                     <div class="w-full lg:w-1/3 lg:block mr-12 flex flex-col mb-4">
-                        <label class="block text-gray-700 text-lg font-semibold mb-2">Basics</label>
+                        <label class="block text-gray-700 text-lg font-medium mb-2">Basics</label>
                         <label class="block text-gray-600">
                             This is some long detailed description which is part towards a better form.
                         </label>
                     </div>
                     <div class="w-full lg:w-2/3">
                         <div class="mb-4 flex flex-col">
-                            <label for="name" class="text-gray-700 text-md font-semibold mb-3">Name</label>
+                            <label for="name" class="text-gray-700 text-md font-medium mb-3">Name</label>
                             <input type="text" id="name" class="w-full border-2 border-gray-400 rounded-lg py-3 text-gray-600 form-input">
                         </div>
                         <div class="mb-4 flex flex-col">
-                            <label for="description" class="text-gray-700 text-md font-semibold mb-3">Description</label>
+                            <label for="description" class="text-gray-700 text-md font-medium mb-3">Description</label>
                             <textarea name="description" id="description" cols="30" rows="10" class="w-full form-textarea border-2 border-gray-400 rounded-lg text-gray-600"
                                       placeholder="Add an optional description...">
                             </textarea>
                         </div>
                         <div class="mb-4 flex flex-col">
                             <!-- TODO: create cards for better visual impact -->
-                            <label class="text-gray-700 text-md font-semibold mb-3">Type</label>
+                            <label class="text-gray-700 text-md font-medium mb-3">Type</label>
                             <div class="flex">
                                 <div class="mr-4">
                                     <label class="flex items-center">
                                         <input type="radio" class="form-radio focus:shadow-none text-indigo-600" name="radio-colors" value="1" checked>
-                                        <span class="ml-2 text-gray-700 text-md font-semibold">Seminar</span>
+                                        <span class="ml-2 text-gray-700 text-md font-medium">Seminar</span>
                                     </label>
                                 </div>
                                 <div>
                                     <label class="flex items-center">
                                         <input type="radio" class="form-radio focus:shadow-none text-indigo-600" name="radio-colors" value="1" checked>
-                                        <span class="ml-2 text-gray-700 text-md font-semibold">Lecture</span>
+                                        <span class="ml-2 text-gray-700 text-md font-medium">Lecture</span>
                                     </label>
                                 </div>
                             </div>
                         </div>
                         <div class="mb-4 flex flex-col">
-                            <label class="text-gray-700 text-md font-semibold mb-3">Language</label>
+                            <label class="text-gray-700 text-md font-medium mb-3">Language</label>
                             <select name="language" id="language" class="w-full form-select block border-2 border-gray-400 rounded-lg text-gray-600 py-3">
                                 <option>German</option>
                                 <option>English</option>
@@ -61,14 +61,14 @@
             <section class="border-t-2 py-8 border-gray-400">
                 <div class="lg:flex">
                     <div class="w-full lg:w-1/3 lg:block mr-12 flex flex-col mb-4">
-                        <label class="block text-gray-700 text-md font-semibold mb-2">Restrictions</label>
+                        <label class="block text-gray-700 text-md font-medium mb-2">Restrictions</label>
                         <label class="block text-gray-600">
                             This is some long detailed description which is part towards a better form.
                         </label>
                     </div>
                     <div class="w-full lg:w-2/3">
                         <div class="mb-4 flex flex-col">
-                            <label for="limit" class="text-gray-700 text-md font-semibold mb-3">Participation Limit</label>
+                            <label for="limit" class="text-gray-700 text-md font-medium mb-3">Participation Limit</label>
                             <input type="number" id="limit" class="w-full border-2 border-gray-400 rounded-lg py-3 text-gray-600 form-input">
                         </div>
                     </div>
