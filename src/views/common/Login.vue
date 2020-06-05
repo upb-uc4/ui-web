83b06eb028e52ad7c83c2e55559417687d4ccfeb
Tobias <tobiaswiese@online.de>
2020-06-05 12:33:43 +0200

    setup vuex store with typescript + add lecturerId/studentId to store

--- src/views/common/Login.vue
+++ src/views/common/Login.vue
@@ -9,12 +9,21 @@
         </div>
         <div class="md:flex w-full h-screen">
           <div
-            class="md:w-1/2 bg-red-300 text-gray-900 flex items-center justify-center cursor-pointer hover:bg-red-500"
-            @click="goto('/student')"
+            class="md:w-1/2 bg-red-300 text-gray-900 flex flex-col items-center justify-center cursor-pointer"
           >
             <h1 class="text-6xl">Student</h1>
+            <input
+              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-1/2 py-2 px-4 text-gray-500 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              type="text"
+              @input="updateStudentId($event.target.value)"
+              placeholder="Please provide your Lecturer-ID"
+            />
+          <div class="p-5">
+            <button
+              class="bg-white text-blue-600 border-2 shadow-md border-gray-300 py-2 px-8 rounded-lg focus:outline-none font-semibold" @click="enterStudent()"
+            >Enter</button>
           </div>
-
+          </div>
           <div
             class="md:w-1/2 bg-blue-300 text-gray-900 flex flex-col items-center justify-center cursor-pointer"
           >
@@ -39,11 +48,13 @@
 
 <script lang="ts">
 import Router from "../../router/";
+import { useStore } from "../../store/store"
 export default {
-  name: "Login",
+  name: "Login", 
   data()  {
       return {
-      lecturerId: -1
+      lecturerId: -1,
+      studentId: -1
       }
   },
   methods: {
@@ -51,20 +62,49 @@ export default {
       Router.push(target);
     },
     updateLecturerId: function(value:string) {
-        this.lecturerId = value;
+        if(value == "") {
+            this.lecturerId = -1;
+        }
+        else {
+          this.lecturerId = +value;
+        }
         console.log(this.lecturerId)
     },
+    updateStudentId: function(value:string) {
+        if(value == "") {
+            this.studentId = -1;
+        }
+        else {
+          this.studentId = +value;
+        }
+        console.log(this.studentId)
+    },
     enterProf: function() {
-        if(this.lecturerId == -1 || this.lecturerId == "") {
+        if(this.lecturerId < 0 ) {
             console.log("Error: lecturerId not set")
             return
         }
         else {
-            //TODO add lecturerId to the vuex store
+            const store = useStore();
+            store.state.myRole = "Lecturer"
+            store.state.myId = this.lecturerId
             this.goto("/lecturer")
             return
         }
-    } 
+    },
+    enterStudent: function() {
+        if(this.studentId <0 ) {
+            console.log("Error: studentId not set")
+            return
+        }
+        else {
+            const store = useStore();
+            store.state.myRole = "Student"
+            store.state.myId = this.studentId
+            this.goto("/student")
+            return
+        } 
+    }
   },
 };
 </script>
