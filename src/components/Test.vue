3a5e14776a336f0abca26c8be0d02da3beeb2403
Tobias <tobiaswiese@online.de>
2020-06-10 16:44:16 +0200

    test setup (not working)
    Co-authored-by: Theo Harkenbusch <theo@harkenbusch.com>

---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        {{ course.name }}
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "Test",
+        props: ['course'],
+    };
+</script>
