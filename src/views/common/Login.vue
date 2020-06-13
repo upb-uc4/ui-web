dfcfd0f789593eb262b31ad13b6373003de24815
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-13 14:55:52 +0200

    fix centering of login page

--- src/views/common/Login.vue
+++ src/views/common/Login.vue
@@ -3,29 +3,23 @@
     <dev-nav-bar></dev-nav-bar>
     <div class="container max-w-full h-full h-auto flex flex-col lg:flex-row lg:items-center">
         <form v-on:submit.prevent="login" method="POST" action="" class="xl:w-3/4 w-full flex items-center flex-col mx-auto">
-            <h1 class="lg:text-6xl mt-2 text-4xl font-bold mb-4 text-center text-gray-900 mb-10">Login to Your Account</h1>
+            <h1 class="lg:text-5xl mt-2 text-4xl font-bold mb-4 text-center text-gray-900 mb-10">Login to Your Account</h1>
 
-            <div class="w-full flex">
-                <div class="sm:pl-40 md:pl-56 lg:pl-64 xl:pl-56 mx-4 w-full">
-
-                    <div class="mb-6 sm:w-1/4 md:w-3/4 lg:w-4/4">
-                        <label class="sr-only-focusable sr-only font-semibold text-gray-900 block mb-2">Email</label>
+            <div class="w-full lg:w-3/5 mx-4 flex-auto items-center justify-center">
+                    <div class="mb-6 text-center">
                         <i class="m-3 fas fa-envelope absolute text-gray-500"></i>
-                        <input v-model="email" class="justify-start lg:w-3/4 block appearance-none shadow-md font-semibold bg-gray-200 text-gray-600 placeholder-gray-600 focus:text-gray-600 p-2 pl-10 rounded hover:border-gray-300 focus:outline-none focus:shadow-outline" type="text" placeholder="Email" name="email">
+                        <input v-model="email" class="inline-block center lg:w-3/4 block appearance-none shadow-md font-semibold bg-gray-200 text-gray-600 placeholder-gray-600 focus:text-gray-600 p-2 pl-10 rounded hover:border-gray-300 focus:outline-none focus:shadow-outline" type="text" placeholder="Email" name="email">
                     </div>
 
-                    <div class="mb-6 sm:w-1/4 md:w-3/4 lg:w-4/4">
-                        <label class="sr-only-focusable sr-only font-semibold text-gray-900 block">Password</label>
+                    <div class="mb-6 text-center ">
                         <i class="m-3 fas fa-lock absolute text-gray-500"></i>
-                        <div class="flex">
-                            <input v-model="password" ref="password" :type="passwordFieldType" v-on:input="hideErrors" name="password" class="lg:w-3/4 shadow-md block appearance-none  font-semibold bg-gray-200 text-gray-600 placeholder-gray-600 focus:text-gray-600 p-2 pl-10 rounded hover:border-gray-300 focus:outline-none focus:shadow-outline" placeholder="Password">
-                            <button @click="togglePassword" type=button tabIndex="-1" class="relative ml-3 visible text-gray-500 text-sm hover:text-gray-600 focus:outline-none">
-                                <i :class="[isPasswordVisible ? 'fa-eye-slash' : 'fa-eye']" class="fas mr-1"></i>
-                            </button>
-                        </div>
+                        <input v-model="password" ref="password" :type="passwordFieldType" v-on:input="hideErrors" name="password" class="lg:w-3/4 shadow-md inline-block center appearance-none  font-semibold bg-gray-200 text-gray-600 placeholder-gray-600 focus:text-gray-600 p-2 pl-10 rounded hover:border-gray-300 focus:outline-none focus:shadow-outline" placeholder="Password">
+                        <button @click="togglePassword" type=button tabIndex="-1" class="display-none absolute inline-block center ml-3 visible text-gray-500 text-sm hover:text-gray-600 focus:outline-none">
+                            <i :class="[isPasswordVisible ? 'fa-eye-slash' : 'fa-eye']" class="display-none absolute mt-3 ml-1 fas mr-1"></i>
+                        </button>
                     </div>
 
-                    <div class="w-full lg:w-3/4 sm:items-center justify-between">
+                    <div class="w-full text-center lg:text-left lg:pl-16 lg:ml-3">
                         <label class="block text-gray-500 font-semibold">
                             <input class="mr-2 form-checkbox text-blue-500 hover:bg-blue-400 hover:shadow-inner border-gray-500 border-1 focus:border-none" type="checkbox" checked>
                             <span class="text-sm">Remember me</span>
@@ -35,13 +29,11 @@
                         </a>
                     </div>
 
-                    <div class="mt-10 flex mb-6 sm:w-1/4 md:w-3/4 lg:w-4/4  lg:justify-start text-center">
-                        <button type=submit :disabled="isInputEmpty" class="w-3/4 bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none text-white font-semibold py-2 px-4 rounded focus:shadow-outline hover:bg-blue-400 disabled:bg-blue-500">
+                    <div class="mt-10 mb-6 justify-center text-center">
+                        <button type=submit :disabled="isInputEmpty" class="w-2/5 sm:w-2/5 md:w-2/5 lg:w-2/4 inline-block center bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none text-white font-semibold py-2 px-4 rounded focus:shadow-outline hover:bg-blue-400 disabled:bg-blue-500">
                             Login
                         </button>
                     </div>
-
-                </div>
             </div>
         </form>
     </div>
