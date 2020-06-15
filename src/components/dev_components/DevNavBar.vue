549661dc530a79e0fd0d9a9a36c856223b08e114
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-15 11:47:15 +0200

    add simple logout to dev navbar

--- src/components/dev_components/DevNavBar.vue
+++ src/components/dev_components/DevNavBar.vue
@@ -16,12 +16,16 @@
             <li class="bg-blue-200 px-4 py-2 hover:bg-blue-100 cursor-pointer" @click="goto('/createAccount')">   
                  <a>createAccount</a>
             </li>
+            <li class="ml-auto bg-blue-200 px-4 py-2 hover:bg-blue-100 cursor-pointer" @click="logout()">
+                <a>logOut</a>
+            </li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
 import Router from "../../router/";
+import { Roles } from "../../entities/Role"
 
 export default {
   name: "DevNavBar",
@@ -30,6 +34,12 @@ export default {
   methods: {
       goto: function(where:string) {
           Router.push(where);
+      },
+      logout: function() {
+          this.$store.state.loginData = {username:"", password:""};
+          this.$store.state.myRole = Roles.NONE;
+          this.$store.state.myId = "";
+          Router.push("/login")
       }
   }
 };
