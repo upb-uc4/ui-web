e8a63d1b8d8b9630d1e6a41bc748a435b1e2548a
Tobias <tobiaswiese@online.de>
2020-06-19 10:39:57 +0200

    remove unused data() method that causes warnings

--- src/components/LecturerCourse.vue
+++ src/components/LecturerCourse.vue
@@ -39,9 +39,6 @@ export default {
     components: {
         ReadMore
     },
-    data() {
-
-    },
     methods: {
         editCourse() {
             router.push({path: "/editCourse/" + this.course.courseId})
