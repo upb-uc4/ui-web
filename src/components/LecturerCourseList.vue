d9bac530952d48e6dfef6e7df6ae97a1c6fa421e
theovier <Theo@Harkenbusch.com>
2020-06-19 17:49:57 +0200

    fix margin issue with filterbar and courses

--- src/components/LecturerCourseList.vue
+++ src/components/LecturerCourseList.vue
@@ -1,46 +1,46 @@
 <template>
-	<div class="w-full max-w-4xl">
-		<div class="flex">
-			<div class="w-full">
-				<div class="pt-2 relative mx-auto text-gray-600">
-					<i class="fas fa-search absolute left-0 top-0 mt-6 ml-4"></i>
-					<input class="w-full border-2 border-gray-300 bg-white h-12 px-5 pl-12 rounded-lg focus:outline-none"
-						   type="search" placeholder="Filter"
-						   v-model="message">
-				</div>
-      		</div>
-		</div>
-		<suspense>
-			<template #default>
-				<courseList></courseList>
-			</template>
-			<template #fallback>
-				<p class="text-center text-lg pt-20">
-				Loading Courses...
-				</p>
-			</template>
-		</suspense>
-		
-		<div class="flex justify-center mt-16">
-			<router-link to="/createCourse">
-				<button class="px-4 btn btn-green-primary-500">New Course</button>
-			</router-link>
-		</div>
-	</div>
+    <div class="w-full max-w-4xl">
+        <div class="flex">
+            <div class="w-full">
+                <div class="pt-2 mb-8 relative mx-auto text-gray-600">
+                    <i class="fas fa-search absolute left-0 top-0 mt-6 ml-4"></i>
+                    <input class="w-full border-2 border-gray-300 bg-white h-12 px-5 pl-12 rounded-lg focus:outline-none"
+                           type="search" placeholder="Filter"
+                           v-model="message">
+                </div>
+            </div>
+        </div>
+        <suspense>
+            <template #default>
+                <courseList></courseList>
+            </template>
+            <template #fallback>
+                <p class="text-center text-lg pt-20">
+                    Loading Courses...
+                </p>
+            </template>
+        </suspense>
+
+        <div class="flex justify-center mt-16">
+            <router-link to="/createCourse">
+                <button class="px-4 btn btn-green-primary-500">New Course</button>
+            </router-link>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-import CourseList from "./CourseList.vue"
+    import CourseList from "./CourseList.vue"
 
-export default {
-	name: "LecturerCourseList",
-	components: {
-		CourseList,
-	},
-	data() {
-		return {
-			message: "",
-		}
-	},
-};
+    export default {
+        name: "LecturerCourseList",
+        components: {
+            CourseList,
+        },
+        data() {
+            return {
+                message: "",
+            }
+        },
+    };
 </script>
--- src/components/StudentCourseList.vue
+++ src/components/StudentCourseList.vue
@@ -1,26 +1,26 @@
 <template>
-	<div class="w-full max-w-4xl">
-		<div class="flex">
-			<div class="w-full">
-				<div class="pt-2 relative mx-auto text-gray-600">
-					<i class="fas fa-search absolute left-0 top-0 mt-6 ml-4"></i>
-					<input class="w-full border-2 border-gray-300 bg-white h-12 px-5 pl-12 rounded-lg focus:outline-none"
-						   type="search" placeholder="Filter"
-						   v-model="message">
-				</div>
-      		</div>
-		</div>
-		<suspense>
-			<template #default>
-				<courseList></courseList>
-			</template>
-			<template #fallback>
-				<p class="text-center text-lg pt-20">
-				Loading Courses...
-				</p>
-			</template>
-		</suspense>
-	</div>
+    <div class="w-full max-w-4xl">
+        <div class="flex">
+            <div class="w-full">
+                <div class="pt-2 mb-8 relative mx-auto text-gray-600">
+                    <i class="fas fa-search absolute left-0 top-0 mt-6 ml-4"></i>
+                    <input class="w-full border-2 border-gray-300 bg-white h-12 px-5 pl-12 rounded-lg focus:outline-none"
+                           type="search" placeholder="Filter"
+                           v-model="message">
+                </div>
+            </div>
+        </div>
+        <suspense>
+            <template #default>
+                <courseList></courseList>
+            </template>
+            <template #fallback>
+                <p class="text-center text-lg pt-20">
+                    Loading Courses...
+                </p>
+            </template>
+        </suspense>
+    </div>
 </template>
 
 <script lang="ts">
