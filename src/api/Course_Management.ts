b8031210de29fa7c644e3ff1ec406e9928a3497f
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-24 13:08:19 +0200

    add more return values to course api

--- src/api/Course_Management.ts
+++ src/api/Course_Management.ts
@@ -20,6 +20,7 @@ export default class Course_Management extends Common {
                         courses = response.data;
                     })
                     .catch((error: any) => {
+                        console.log(error)
                         if (!error.reponse) {
                             return console.log("Network Error")
                         }
@@ -60,10 +61,12 @@ export default class Course_Management extends Common {
         return {course: course, found: found};
     }
 
-    async createCourse(course: Course) {
+    async createCourse(course: Course): Promise<boolean> {
+        let success = false;
         await this._axios.post("/courses", course, this._authHeader)
                     .then((response: any) => {
                         console.log(response)
+                        success = true;
                     })
                     .catch((error: any) => {
                         if (error.response.status == "401") {
@@ -74,13 +77,16 @@ export default class Course_Management extends Common {
                             console.log(error.response)
                         }
                     });    
+        return success
     }
 
-    async updateCourse(course: Course) {
+    async updateCourse(course: Course): Promise<boolean> {
         const id = course.courseId;
+        let success = false;
         await this._axios.put(`/courses/${id}`, course, this._authHeader)
                     .then((response: any) => {
                         console.log(response)
+                        success = true
                     })
                     .catch((error: any) => {
                         if (error.response.status == "401") {
@@ -89,12 +95,15 @@ export default class Course_Management extends Common {
                             console.log(error)
                         }
                     });    
+        return success;
     }    
 
-    async deleteCourse(id: string) {
+    async deleteCourse(id: string): Promise<boolean> {
+        let success = false;
         await this._axios.delete(`/courses/${id}`, this._authHeader)
                     .then((response: any) => {
                         console.log(response)
+                        success = true;
                     })
                     .catch((error: any) => {
                         if (error.response.status == "401") {
@@ -103,6 +112,7 @@ export default class Course_Management extends Common {
                             console.log(error)
                         }
                     });    
+        return success;
     }  
 
 }
