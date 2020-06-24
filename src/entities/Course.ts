e2fb2266ea20aa1fd4ad8766f2b12d165b8353a8
Bastian Haverkamp <bastihav@mail.upb.de>
2020-06-24 13:39:22 +0200

    add course interface to course class and add equals method

--- src/entities/Course.ts
+++ src/entities/Course.ts
@@ -1,8 +1,9 @@
 import { CourseType } from './CourseType';
 import { Language } from './Language';
+import ICourse from '@/api/api_models/course_management/ICourse';
 
 
-export class Course {
+export class Course implements ICourse {
     courseType: CourseType = CourseType.NONE;
     courseId: string = "";
     courseName: string = "";
@@ -14,4 +15,29 @@ export class Course {
     courseLanguage:Language = Language.NONE;
     startDate: string ="";
     endDate: string = "";
+
+    public equals(obj: any): boolean {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Course))
+            return false;
+        
+        const course: Course = obj as Course;
+        let equal: boolean = true;
+        equal = equal && (this.courseType === course.courseType);
+        equal = equal && (this.courseId === course.courseId);
+        equal = equal && (this.courseName === course.courseName);
+        equal = equal && (this.lecturerId === course.lecturerId);
+        equal = equal && (this.ects === course.ects);
+        equal = equal && (this.maxParticipants === course.maxParticipants);
+        equal = equal && (this.currentParticipants === course.currentParticipants);
+        equal = equal && (this.courseDescription === course.courseDescription);
+        equal = equal && (this.courseLanguage === course.courseLanguage);
+        equal = equal && (this.startDate === course.startDate);
+        equal = equal && (this.endDate === course.endDate);
+        
+        return equal;
+    }
 }
