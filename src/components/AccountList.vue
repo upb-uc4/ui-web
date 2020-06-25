e59df7b192dd0b24a425db0e3620b0992f61a288
Tobias <tobiaswiese@online.de>
2020-06-25 13:33:40 +0200

    add admin account list + account list analogous to lecturer home

---
+++ src/components/AccountList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-for="account in accounts" :key="account.username">
+			<!-- show Account Object -->
+		</div>
+    </div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: "AccountList",
+    components: {
+       
+    },
+    
+    async setup() {
+        //TODO get all accounts via API call + export "accounts" s.t. we can show them in the template
+        return {
+           
+        }
+    }
+    
+}
+</script>
