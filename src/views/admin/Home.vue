494fc2e7389f0c4290b5dec293f2a2b4bade622f
Tobias <tobiaswiese@online.de>
2020-06-25 13:33:53 +0200

    add account list to admin home

--- src/views/admin/Home.vue
+++ src/views/admin/Home.vue
@@ -3,7 +3,7 @@
 		<dev-nav-bar/>
 		<div class="mt-32 text-4xl text-center font-semibold text-gray-900">Available Accounts</div>
 		<div class="mt-8 flex justify-center">
-            <account-list/>
+            <admin-account-list/>
 		</div>
 	</div>
 </template>
@@ -12,13 +12,13 @@
 	import { store } from "../../store/store"
 	import { Role } from "../../entities/Role"
     import DevNavBar from "../../components/dev_components/DevNavBar.vue"
-    import AccountList from "../../components/AccountList.vue"
+    import AdminAccountList from "../../components/AdminAccountList.vue"
 
 	export default {
 		name: "Admin.Home",
 		components: {
             DevNavBar,
-            AccountList
+            AdminAccountList
 		},
 		
 		beforeRouteEnter(_from: any, _to: any, next: any) {
