f8bca28291cc60b2e9ff22b6b65e58c40363a576
Tobias <tobiaswiese@online.de>
2020-06-25 13:32:45 +0200

    add Home directory for the admin

---
+++ src/views/admin/Home.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<dev-nav-bar/>
+		<div class="mt-32 text-4xl text-center font-semibold text-gray-900">Available Accounts</div>
+		<div class="mt-8 flex justify-center">
+            <account-list/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import { store } from "../../store/store"
+	import { Role } from "../../entities/Role"
+    import DevNavBar from "../../components/dev_components/DevNavBar.vue"
+    import AccountList from "../../components/AccountList.vue"
+
+	export default {
+		name: "Admin.Home",
+		components: {
+            DevNavBar,
+            AccountList
+		},
+		
+		beforeRouteEnter(_from: any, _to: any, next: any) {
+			const myRole = store.state.myRole;
+			if (myRole != Role.ADMIN) {
+				return next("/redirect");
+			}
+			return next();
+		},
+	};
+</script>
