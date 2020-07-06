f55ecb89110e6a576aa48695788077532d9967c1
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-06 12:21:40 +0200

    add generic response handler
    can be used for all api calls where we do not have special error handling

---
+++ src/use/GenericResponseHandler.ts
@@ -0,0 +1,36 @@
+import ResponseHandler from "./ResponseHandler"
+import APIResponse from "../api/helpers/models/APIResponse"
+
+export default class GenericResponseHandler implements ResponseHandler<boolean> {
+    handleReponse<T>(response: APIResponse<T>): T {
+        console.log(response)
+        if (response.networkError) {
+            //TODO show toast
+            alert("Network Error")
+            return response.returnValue;
+        }
+        switch (response.statusCode) {
+            case 400: {
+                //TODO show toast
+                return response.returnValue;
+            }
+            case 401: {
+                alert("Authentication failed. Please login again!")
+                return response.returnValue;
+            }
+            case 403: {
+                alert("You do not have the neccessary user rights for this action!")
+            }
+            case 404: {
+                //TODO show toast
+                return response.returnValue;
+            }
+            case 200: {
+                return response.returnValue;
+            }
+        }
+
+        return response.returnValue;
+    }
+
+}
