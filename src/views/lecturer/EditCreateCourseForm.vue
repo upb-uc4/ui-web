4825cda33d4eeb1ea0515389ab97b07b94ed2ec9
Tobias <tobiaswiese@online.de>
2020-07-09 14:12:46 +0200

    remove the paddings in course form

--- src/views/lecturer/EditCreateCourseForm.vue
+++ src/views/lecturer/EditCreateCourseForm.vue
@@ -34,7 +34,8 @@
                         <div class="mb-4 flex flex-col">
                             <label for="name" class="text-gray-700 text-md font-medium mb-3">Name</label>
                             <input type="text" id="name" name="courseName" v-model="course.courseName"
-                                   class="w-full py-3 form-input-field">
+                                   class="w-full form-input-field"
+                                   placeholder="Course Name">
                             <p v-if="hasError('courseName')" class="text-red-600 ml-1 mt-1">{{ showError('courseName') }}</p>
                         </div>
                         <div class="mb-4 flex flex-col">
@@ -72,7 +73,7 @@
                     <div class="w-full lg:w-2/3">
                         <div class="mb-4 flex flex-col">
                             <label for="limit" class="text-gray-700 text-md font-medium mb-3">Participation Limit</label>
-                            <input type="number" name="maxParticipants" id="limit" min="0" max="999" class="w-full py-3 form-input-field"
+                            <input type="number" name="maxParticipants" id="limit" min="0" max="999" class="w-full form-input-field"
                                    v-model="course.maxParticipants">
                             <p v-if="hasError('maxParticipants')" class="text-red-600 ml-1 mt-1">{{ showError('maxParticipants') }}</p>
                         </div>
@@ -92,13 +93,13 @@
                         <div class="w-1/2 mb-4 mr-12 flex flex-col">
                             <label for="start" class="text-gray-700 text-md font-medium mb-3">Start Date</label>
                             <input type="text" disabled name="startDate" id="start" v-model="course.startDate"
-                                   class="w-full py-3 form-input-field">
+                                   class="w-full form-input-field">
                             <p v-if="hasError('startDate')" class="text-red-600 ml-1 mt-1">{{ showError('startDate') }}</p>
                         </div>
                         <div class="w-1/2 mb-4 flex flex-col">
                             <label for="end" class="text-gray-700 text-md font-medium mb-3">End Date</label>
                             <input type="text" disabled name="endDate" id="end" v-model="course.endDate"
-                                   class="w-full py-3 form-input-field">
+                                   class="w-full form-input-field">
                             <p v-if="hasError('endDate')" class="text-red-600 ml-1 mt-1">{{ showError('endDate') }}</p>
                         </div>
                     </div>
