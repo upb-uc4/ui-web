06456da028e8ec377b0e497be91efd06e51099db
theovier <Theo@Harkenbusch.com>
2020-07-12 10:51:31 +0200

    add back button and adjust styling

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,6 +51,7 @@ const router = createRouter({
 		},
 		{
 			path: "/",
+			name: 'home',
 			component: LoginView,
 		},
 		{
--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="flex w-full h-screen items-center -mt-16">
+    <div class="flex w-full h-screen items-center -mt-16 mx-12">
 
-        <div class="md:flex w-full">
-            <div class="w-1/2 flex justify-center lg:-mr-32 text-6xl font-semibold text-blue-600" style="font-size: 96px">
+        <div class="md:flex md:flex-row flex-col justify-center w-full">
+            <div class="mr-12 lg:mr-24 text-6xl font-semibold text-center text-blue-700" style="font-size: 96px">
                 404
             </div>
 
-            <div class="w-1/2 flex flex-col justify-center">
-                <div class="flex text-5xl font-bold text-blue-600">
-                    Ooops...
+            <div class="flex flex-col justify-center">
+                <div class="text-5xl font-bold text-blue-700">
+                    Oops...
                 </div>
                 <div class="text-xl font-medium">
                     Unfortunately, this page does not exist :(
+                </div>
+                <div class="mt-10 text-xl font-medium">
+                    <router-link :to="{ name: 'home'}"
+                                 class="p-3 btn btn-blue-secondary">
+                        Take me Home
+                    </router-link>
                 </div>
             </div>
         </div>
