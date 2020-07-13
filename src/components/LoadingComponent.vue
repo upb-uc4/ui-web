2eb4c2da7506eb4678eb767d7c1445c8baa8442b
Tobias <tobiaswiese@online.de>
2020-07-13 14:00:42 +0200

    add a simple animated loading component

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full items-center justify-center flex flex-col">
+        <h2 class="text-xl text-gray-800">Loading Contents...</h2>
+        <img src="../assets/loading-spinner.svg" class="w-1/12"/>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "LoadingComponent"
+}
+</script>>
