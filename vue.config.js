ecf34493f8bdf1e1db6d121e18d92d0afbc97c03
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-15 14:03:03 +0200

    change api url in deployment because the GET request is sent from the client

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version;
 
 // do not add trailing forward slash
 process.env.VUE_APP_API_BASE_URL = process.env.NODE_ENV === 'production'
-    ? 'http://localhost/api'
+    ? 'https://uc4.cs.upb.de/api'
     : 'http://localhost:9000';
 
 module.exports = {
