485fe0f9859cae4f8de5e6a74e26b7bb8ad6b766
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-17 14:18:40 +0200

    change deployed version placement

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
     <div id="root" class="container mx-auto px-4">
         <router-view />
         <div id="modal-wrapper" />
+        <div class="w-3/4 fixed bottom-0">
+            <p class="text-xs text-right text-gray-700 mb-2 mr-2">Frontend Version {{ version }}</p>
+        </div>
     </div>
 </template>
 
@@ -11,6 +14,11 @@
     export default {
         name: "App",
         components: {},
-        setup() {},
+        setup() {
+            const version = "v" + process.env.VUE_APP_VERSION;
+            return {
+                version,
+            };
+        },
     };
 </script>
