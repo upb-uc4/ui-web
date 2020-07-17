4bd2555044912019d6ee8100a29f369064dd66d8
theovier <Theo@Harkenbusch.com>
2020-07-17 17:16:13 +0200

    lint vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,8 @@ process.env.VUE_APP_VERSION = require("./package.json").version;
 process.env.VUE_APP_API_BASE_URL = process.env.NODE_ENV === "production" ? "https://uc4.cs.upb.de/api" : "http://localhost:9000";
 
 module.exports = {
-    chainWebpack: config => {
-        config.module
-          .rule('eslint')
-          .use('eslint-loader')
-          .options({
+    chainWebpack: (config) => {
+        config.module.rule("eslint").use("eslint-loader").options({
             fix: false,
         });
     },
