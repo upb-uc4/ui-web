13d0f15897057b75af2f3148a75e68046e6bb8c4
theovier <Theo@Harkenbusch.com>
2020-07-18 19:38:28 +0200

    extract restrictions section from create/edit course

--- src/views/lecturer/EditCreateCourseForm.vue
+++ src/views/lecturer/EditCreateCourseForm.vue
@@ -16,34 +16,10 @@
                 :error-bag="errorBag"
             />
 
-            <section class="border-t-2 py-8 border-gray-400">
-                <div class="lg:flex">
-                    <div class="w-full lg:w-1/3 lg:block mr-12 flex flex-col mb-4">
-                        <label class="block text-gray-700 text-md font-medium mb-2">Restrictions</label>
-                        <label class="block text-gray-600">
-                            This is some long detailed description which is part towards a better form.
-                        </label>
-                    </div>
-                    <div class="w-full lg:w-2/3">
-                        <div class="mb-4 flex flex-col">
-                            <label for="limit" class="text-gray-700 text-md font-medium mb-3">Participation Limit</label>
-                            <input
-                                id="limit"
-                                v-model="course.maxParticipants"
-                                type="number"
-                                name="maxParticipants"
-                                min="0"
-                                max="999"
-                                class="w-full form-input input-text"
-                                :class="{ error: errorBag.has('maxParticipants') }"
-                            />
-                            <p v-if="errorBag.has('maxParticipants')" class="error-message">
-                                {{ errorBag.get("maxParticipants") }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <restrictions-section
+                v-model:participants-limit="course.maxParticipants"
+                :error-bag="errorBag"
+            />
 
             <section class="border-t-2 py-8 border-gray-400">
                 <div class="lg:flex">
@@ -145,11 +121,13 @@
     import ValidationResponseHandler from "@/use/ValidationResponseHandler";
     import GenericResponseHandler from "@/use/GenericResponseHandler";
     import BasicsSection from "@/components/course/edit/BasicsSection.vue";
+    import RestrictionsSection from "@/components/course/edit/RestrictionsSection.vue";
 
     export default {
         name: "LecturerCreateCourseForm",
         components: {
             BasicsSection,
+            RestrictionsSection,
             DeleteCourseModal,
         },
         props: {
