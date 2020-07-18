3caeeab27e16719de6cb6d41e6431c5a7bcafb33
theovier <Theo@Harkenbusch.com>
2020-07-18 14:38:58 +0200

    add generic error class with name and reason

--- src/use/ErrorHandler.ts
+++ src/use/ErrorHandler.ts
@@ -1,20 +1,17 @@
-import ValidationError from "@/api/api_models/errors/ValidationError";
 
-export default function useErrorHandler() {
-    var errorList: { name: string; reason: string }[] = [];
+export default class ErrorHandler {
+    errors: { name: string; reason: string }[] = [];
 
-    function hasError(name: string): boolean {
-        return errorList.filter((e) => e.name == name).length > 0;
+    hasError(name: string): boolean {
+        return this.errors.filter((e) => e.name == name).length > 0;
     }
 
-    function showError(name: string): string {
-        const result = errorList.filter((e) => e.name == name)[0]?.reason;
+    showError(name: string): string {
+        const result = this.errors.filter((e) => e.name == name)[0]?.reason;
         return result ? result : "";
     }
 
-    return {
-        errorList,
-        hasError,
-        showError,
-    };
-}
+    replaceErrors() {
+
+    }
+};
