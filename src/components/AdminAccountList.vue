bb1f33ec30fce48c83a51315bf137e3c54b2cd6c
Tobias <tobiaswiese@online.de>
2020-07-20 23:33:01 +0200

    add new button to account list

--- src/components/AdminAccountList.vue
+++ src/components/AdminAccountList.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="w-full max-w-4xl">
         <div class="flex">
-            <div class="w-full">
-                <div class="pt-2 mb-8 relative mx-auto text-gray-600 flex">
-                    <i class="fas fa-search absolute left-0 top-0 mt-6 ml-4"></i>
+            <div class="w-full flex flex-row pt-2 mb-8">
+                <div class="w-full relative mx-auto text-gray-600 flex">
+                    <i class="fas fa-search absolute left-0 top-0 mt-4 h-12 ml-4"></i>
                     <input
                         v-model="message"
                         class="w-full border-2 border-gray-300 bg-white h-12 px-12 rounded-lg focus:outline-none"
                         type="search"
                         placeholder="Filter"
                     />
-                    <button class="-ml-8 my-1 focus:outline-none" title="Refresh" @click="refresh">
+                    <button class="-ml-8 mr-5 focus:outline-none" title="Refresh" @click="refresh">
                         <i class="inline fas fa-redo-alt text-gray-600 hover:text-gray-700" />
                     </button>
                 </div>
+                <router-link to="/createAccount" class="w-1/12 ml-4">
+                    <button title="Add a new User" class="w-full h-full btn-icon-green items-center justify-center flex flex-row">
+                        <p class="mr-1 text-xl font-semibold">Add</p><i class="inline fas fa-user-plus text-xl" />
+                    </button> 
+                </router-link>
             </div>
         </div>
         <suspense>
@@ -27,7 +32,7 @@
 
         <div class="flex justify-center mt-16">
             <router-link to="/createAccount">
-                <button class="px-4 btn btn-green-primary-500">New Account</button>
+                <button title="Add a new User" class="px-4 btn btn-green-primary-500">New Account</button>
             </router-link>
         </div>
     </div>
