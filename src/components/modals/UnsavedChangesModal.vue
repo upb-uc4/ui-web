675e150d11a72098924d18bef905c95c3ed0265a
Tobias <tobiaswiese@online.de>
2020-07-20 21:40:25 +0200

    add IDs in modals

--- src/components/modals/UnsavedChangesModal.vue
+++ src/components/modals/UnsavedChangesModal.vue
@@ -7,8 +7,8 @@
         Do you really want to continue and leave this page? You have unsaved changes.
 
         <template #footer>
-            <button class="mr-10 btn-tertiary" @click="close(action.CANCEL)">Cancel</button>
-            <button class="w-24 py-2 px-2 btn btn-blue-primary" @click="close(action.CONFIRM)">Leave</button>
+            <button id="modalCancel" class="mr-10 btn-tertiary" @click="close(action.CANCEL)">Cancel</button>
+            <button id="modalConfirmLeave" class="w-24 py-2 px-2 btn btn-blue-primary" @click="close(action.CONFIRM)">Leave</button>
         </template>
     </modal>
 </template>
