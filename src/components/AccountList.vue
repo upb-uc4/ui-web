f8a652a1f683167bdc2b8b74f14c8869464da5dc
theovier <Theo@Harkenbusch.com>
2020-07-20 23:08:59 +0200

    extract user card into subcomponent

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,120 +1,7 @@
 <template>
     <div class="bg-white rounded-lg shadow flex flex-col">
-        <div class="px-8 py-4 whitespace-no-wrap border-b border-gray-200 cursor-pointer rounded-t-lg hover:bg-gray-200">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center justify-between">
-                    <div class="flex">
-                        <div class="hidden sm:flex flex-shrink-0 w-12 h-12">
-                            <img
-                                class="w-12 h-12 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                            />
-                        </div>
-                        <div class="sm:ml-8">
-                            <div class="text leading-5 font-medium text-blue-900 mb-1 lg:w-48 w-32 truncate">
-                                Maximilian Mustermann von und zu Lange
-                            </div>
-                            <div class="text leading-5 text-gray-500 lg:w-48 w-32 truncate">@MaximilianMustermannLange</div>
-                        </div>
-                    </div>
-
-                    <div class="mx-8 w-24">
-                        <div class="text leading-5 text-blue-900 mb-1">Student</div>
-                    </div>
-                    <div class="flex flex-col items-baseline">
-                        <div class="leading-5 text-blue-900 ml-1 mb-1">421769</div>
-                        <div class="hidden sm:flex items-center leading-5 text-gray-500">
-                            <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em;">
-                                <i class="fas fa-circle text-green-500 fa-stack-2x"></i>
-                                <i class="fas fa-check fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <div class="hidden sm:block">
-                                Immatriculated
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex">
-                    <i class="fas fa-chevron-right text-gray-500" />
-                </div>
-            </div>
-        </div>
-        <div class="px-8 py-4 whitespace-no-wrap border-b border-gray-200 cursor-pointer hover:bg-gray-200">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center justify-between">
-                    <div class="flex">
-                        <div class="hidden sm:flex flex-shrink-0 w-12 h-12">
-                            <img
-                                class="w-12 h-12 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                            />
-                        </div>
-                        <div class="sm:ml-8">
-                            <div class="text leading-5 font-medium text-blue-900 mb-1 lg:w-48 w-32 truncate">Maximilian Mustermann</div>
-                            <div class="text leading-5 text-gray-500 lg:w-48 w-32 truncate">@MaxiMax</div>
-                        </div>
-                    </div>
-
-                    <div class="mx-8 w-24">
-                        <div class="text leading-5 text-blue-900 mb-1">Student</div>
-                    </div>
-                    <div class="flex flex-col items-baseline">
-                        <div class="leading-5 text-blue-900 ml-1 mb-1">421769</div>
-                        <div class="hidden sm:flex items-center leading-5 text-gray-500">
-                            <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em;">
-                                <i class="fas fa-circle text-red-500 fa-stack-2x"></i>
-                                <i class="fas fa-times fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <div class="hidden sm:block">
-                                Exmatriculated
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex">
-                    <i class="fas fa-chevron-right text-gray-500" />
-                </div>
-            </div>
-        </div>
-        <div class="px-8 py-4 whitespace-no-wrap rounded-b-lg cursor-pointer hover:bg-gray-200">
-            <div class="flex items-center justify-between">
-                <div class="flex items-center justify-between">
-                    <div class="flex">
-                        <div class="hidden sm:flex flex-shrink-0 w-12 h-12">
-                            <img
-                                class="w-12 h-12 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                            />
-                        </div>
-                        <div class="sm:ml-8">
-                            <div class="text leading-5 font-medium text-blue-900 mb-1 lg:w-48 w-32 truncate">Maximilian Mustermann</div>
-                            <div class="text leading-5 text-gray-500 lg:w-48 w-32 truncate">@MaxiMax</div>
-                        </div>
-                    </div>
-
-                    <div class="mx-8 w-24">
-                        <div class="text leading-5 text-blue-900 mb-1">Lecturer</div>
-                    </div>
-                    <div class="invisible flex flex-col items-baseline">
-                        <div class="leading-5 text-blue-900 ml-1 mb-1">421769</div>
-                        <div class="hidden sm:flex items-center leading-5 text-gray-500">
-                            <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em;">
-                                <i class="fas fa-circle text-red-500 fa-stack-2x"></i>
-                                <i class="fas fa-times fa-stack-1x fa-inverse"></i>
-                            </span>
-                            <div class="hidden sm:block">
-                                Exmatriculated
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex">
-                    <i class="fas fa-chevron-right text-gray-500" />
-                </div>
-            </div>
+        <div v-for="(user, index) in users" :key="user">
+            <user-row :user="user" :is-first-row="index === 0" :is-last-row="index === users.length - 1" />
         </div>
     </div>
 </template>
@@ -122,16 +9,18 @@
 <script lang="ts">
     import UserManagement from "../api/UserManagement";
     import User_List from "../api/api_models/user_management/User_List";
-    import { Role } from "../entities/Role";
-    import Admin from "../api/api_models/user_management/Admin";
+    import { Role } from "@/entities/Role";
     import { ref } from "vue";
-    import Lecturer from "../api/api_models/user_management/Lecturer";
-    import Student from "../api/api_models/user_management/Student";
     import router from "../router";
     import GenericResponseHandler from "@/use/GenericResponseHandler";
+    import UserRow from "@/components/account/UserRow.vue";
+    import User from "@/api/api_models/user_management/User";
 
     export default {
         name: "AccountList",
+        components: {
+            UserRow,
+        },
         async setup() {
             const userManagement: UserManagement = new UserManagement();
             const roles = Object.values(Role).filter((e) => e != Role.NONE);
@@ -148,10 +37,17 @@
                 router.push({ path: "/editAccount/" + username });
             }
 
+            //todo move into UserList?
+            let users: User[] = [];
+            result.students.forEach((student) => users.push(student));
+            result.lecturer.forEach((lecturer) => users.push(lecturer));
+            result.admins.forEach((admin) => users.push(admin));
+
             return {
                 roles,
                 usersByRole,
                 editAccount,
+                users,
             };
         },
     };
---
+++ src/components/account/UserRow.vue
@@ -0,0 +1,72 @@
+<template>
+    <div
+        class="px-8 py-4 whitespace-no-wrap border-gray-200 cursor-pointer hover:bg-gray-200"
+        :class="{ 'rounded-t-lg': isFirstRow, 'rounded-b-lg': isLastRow, 'border-b': !isLastRow }"
+        @click="editAccount(user.username)"
+    >
+        <div class="flex items-center justify-between">
+            <div class="flex items-center justify-between">
+                <div class="flex">
+                    <div class="hidden sm:flex flex-shrink-0 w-12 h-12">
+                        <img
+                            class="w-12 h-12 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                        />
+                    </div>
+                    <div class="sm:ml-8">
+                        <div class="text leading-5 font-medium text-blue-900 mb-1 lg:w-48 w-32 truncate">
+                            {{ user.firstName }} {{ user.lastName }}
+                        </div>
+                        <div class="text leading-5 text-gray-500 lg:w-48 w-32 truncate">@{{ user.username }}</div>
+                    </div>
+                </div>
+                <div class="mx-8 w-24">
+                    <div class="text leading-5 text-blue-900 mb-1">{{ user.role }}</div>
+                </div>
+                <div class="flex flex-col items-baseline">
+                    <div class="leading-5 text-blue-900 ml-1 mb-1">421769</div>
+                    <div class="hidden sm:flex items-center leading-5 text-gray-500">
+                        <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em;">
+                            <i class="fas fa-circle text-green-500 fa-stack-2x"></i>
+                            <i class="fas fa-check fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <div class="hidden sm:block">
+                            Immatriculated
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex">
+                <i class="fas fa-chevron-right text-gray-500" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import User from "../../api/api_models/user_management/User";
+    import router from "@/router";
+
+    export default {
+        name: "AccountRow",
+        props: {
+            user: {
+                required: true,
+                type: Object as () => User,
+            },
+            isLastRow: {
+                type: Boolean,
+            },
+            isFirstRow: {
+                type: Boolean,
+            },
+        },
+        setup(props: any) {
+            function editAccount(username: string) {
+                router.push({ path: "/editAccount/" + username });
+            }
+            return { editAccount };
+        },
+    };
+</script>
