3e89d7ecaab0aac93bbe99d5a58a11cc4c0b3fd3
theovier <Theo@Harkenbusch.com>
2020-07-22 17:23:26 +0200

    add alt for img

--- src/components/account/UserRow.vue
+++ src/components/account/UserRow.vue
@@ -12,7 +12,7 @@
                         <img
                             class="w-12 h-12 rounded-full"
                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
+                            alt="profile_picture"
                         />
                     </div>
                     <div class="sm:ml-8">
