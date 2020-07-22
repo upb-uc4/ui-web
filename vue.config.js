f681e7d87236ccc81778bf03d659d051c0b662ff
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-22 14:17:55 +0200

    change api to uni-paderborn instead of upb so workflows work

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 process.env.VUE_APP_VERSION = require("./package.json").version;
 
 // do not add trailing forward slash
-process.env.VUE_APP_API_BASE_URL = process.env.NODE_ENV === "production" ? "https://uc4.cs.upb.de/api" : "https://uc4.cs.upb.de/api";
+process.env.VUE_APP_API_BASE_URL =
+    process.env.NODE_ENV === "production" ? "https://uc4.cs.uni-paderborn.de/api" : "https://uc4.cs.uni-paderborn.de/api";
 
 module.exports = {
     chainWebpack: (config) => {
