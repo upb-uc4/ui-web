b7a26145d7745474cc2211ce3eee3c3d820cf3f8
theovier <Theo@Harkenbusch.com>
2020-07-23 20:31:46 +0200

    add display of navbar based on user role

--- src/components/navigation/navbar/Navbar.vue
+++ src/components/navigation/navbar/Navbar.vue
@@ -1,58 +1,29 @@
 <template>
-    <header class="lg:px-8 px-4 bg-white justify-between flex bg-gray-800 items-center lg:py-0 py-2 h-20">
-        <div class="flex">
-            <div class="flex items-center pr-8 mr-8 border-gray-100 border-r-4 my-2">
-                <a href="#" class="flex text-3xl font-semibold text-gray-100 tracking-wider">
-                    UC4
-                </a>
-            </div>
-
-            <div class="w-auto items-center flex w-full">
-                <nav>
-                    <ul class="lg:flex items-center justify-between font-semibold text-gray-100 tracking-wider pt-4 lg:pt-0">
-                        <li>
-                            <a
-                                class="py-1 mx-2 block border-b-2 border-transparent hover:border-gray-100 transition duration-500 ease-in-out"
-                                href="#"
-                            >
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <course-menu
-                                class="py-1 mx-2 block border-b-2 border-transparent hover:border-gray-100 transition duration-500 ease-in-out"
-                            />
-                        </li>
-                        <li>
-                            <a
-                                class="py-1 mx-2 block border-b-2 border-transparent hover:border-gray-100 transition duration-500 ease-in-out"
-                                href="#"
-                            >Documentation</a>
-                        </li>
-                        <li>
-                            <a
-                                class="py-1 mx-2 block border-b-2 border-transparent hover:border-gray-100 lg:mb-0 mb-2 transition duration-500 ease-in-out"
-                                href="#"
-                            >Support</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-
-        <profile-menu class="pt-4" />
-    </header>
+    <admin-navbar v-if="role === Role.ADMIN" />
+    <lecturer-navbar v-else-if="role === Role.LECTURER" />
+    <student-navbar v-else-if="role === Role.STUDENT" />
+    <guest-navbar v-else />
 </template>
 
 <script lang="ts">
-    import ProfileMenu from "@/components/navigation/navbar/common/profile/ProfileMenu.vue";
-    import CourseMenu from "@/components/navigation/navbar/student/CoursesMenu/CourseMenu.vue";
+    import GuestNavbar from "@/components/navigation/navbar/guest/Navbar.vue";
+    import LecturerNavbar from "@/components/navigation/navbar/lecturer/Navbar.vue";
+    import StudentNavbar from "@/components/navigation/navbar/student/Navbar.vue";
+    import AdminNavbar from "@/components/navigation/navbar/admin/Navbar.vue";
+    import { Role } from "@/entities/Role";
 
     export default {
         name: "Navbar",
         components: {
-            ProfileMenu,
-            CourseMenu,
+            GuestNavbar,
+            LecturerNavbar,
+            StudentNavbar,
+            AdminNavbar,
+        },
+        setup() {
+            //mock role for demo; todo: get current role from store
+            const role = Role.STUDENT;
+            return { role, Role };
         },
     };
 </script>
