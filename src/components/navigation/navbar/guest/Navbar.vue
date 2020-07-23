0d76835f1a8f185881fd22b4d87217c82c3a095b
theovier <Theo@Harkenbusch.com>
2020-07-23 20:23:33 +0200

    add guest navbar draft

---
+++ src/components/navigation/navbar/guest/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <base-navbar>
+        <ul class="lg:flex items-center justify-between font-semibold text-gray-100 tracking-wider pt-4 lg:pt-0">
+            <li>
+                <a href="#" class="py-1 mx-2 block border-b-2 border-transparent hover:border-gray-100 transition duration-500 ease-in-out">
+                    Some Welcome Stuff
+                </a>
+            </li>
+        </ul>
+    </base-navbar>
+</template>
+
+<script lang="ts">
+    import BaseNavbar from "@/components/navigation/navbar/common/BaseNavbar.vue";
+
+    export default {
+        name: "LecturerNavbar",
+        components: {
+            BaseNavbar,
+        },
+    };
+</script>
