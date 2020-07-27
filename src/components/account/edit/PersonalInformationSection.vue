2e3c837a090d5c0443f9b7cc01c93ba9b9a5bef8
Tobias <tobiaswiese@online.de>
2020-07-27 23:33:43 +0200

    add personal section component

---
+++ src/components/account/edit/PersonalInformationSection.vue
@@ -0,0 +1,219 @@
+<template>
+    <section class="py-8 border-t-2 border-gray-400">
+        <div class="lg:flex">
+            <div class="flex flex-col w-full mb-4 mr-12 lg:w-1/3 lg:block">
+                <label class="block mb-2 text-lg font-medium text-gray-700">Personal Information</label>
+                <label class="block text-gray-600">
+                    Personal Information and Contact Data for the User
+                </label>
+            </div>
+            <div class="w-full lg:w-2/3">
+                <div class="flex flex-col mb-4">
+                    <label class="mb-3 font-medium text-gray-700 text-md">
+                        Name
+                    </label>
+                    <div class="flex flex-row">
+                        <div class="flex-col w-full pr-2">
+                            <label class="text-sm text-gray-700">Firstname</label>
+                            <input
+                                id="firstName"
+                                v-model="accountFirstName"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('firstName') }"
+                                placeholder="Firstname"
+                            />
+                            <p v-if="errorBag.has('firstName')" class="error-message">
+                                {{ errorBag.get("firstName") }}
+                            </p>
+                        </div>
+                        <div class="flex-col w-full pl-2">
+                            <label class="text-sm text-gray-700">Lastname</label>
+                            <input
+                                id="lastName"
+                                v-model="accountLastName"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('lastName') }"
+                                placeholder="Lastname"
+                            />
+                            <p v-if="errorBag.has('lastName')" class="error-message">
+                                {{ errorBag.get("lastName") }}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col mb-4">
+                    <label class="mb-3 font-medium text-gray-700 text-md">
+                        Birthdate
+                    </label>
+                    <birth-date-picker
+                        v-model:year="accountBirthdayYear"
+                        v-model:month="accountBirthdayMonth"
+                        v-model:day="accountBirthdayDay"
+                    />
+                    <p v-if="errorBag.has('birthDate')" class="error-message">{{ errorBag.get("birthDate") }}</p>
+                </div>
+                <div class="flex flex-col mb-4">
+                    <label class="mb-3 font-medium text-gray-700 text-md">
+                        Adress
+                    </label>
+                    <p v-if="errorBag.has('address')" class="error-message">{{ errorBag.get("address") }}</p>
+                    <div class="flex flex-col w-full">
+                        <label class="text-sm text-gray-700">Country</label>
+                        <select
+                            id="country"
+                            v-model="accountCountry"
+                            class="w-1/2 mb-4 form-select input-select"
+                            :class="{ error: errorBag.has('country') }"
+                        >
+                            <option :value="''">Select a Country</option>
+                            <option v-for="country in countries" :id="'country-' + country" :key="country">{{ country }}</option>
+                        </select>
+                        <p v-if="errorBag.has('country')" class="error-message">{{ errorBag.get("country") }}</p>
+                    </div>
+                    <div class="flex flex-row">
+                        <div class="flex flex-col w-full pr-2 mb-4">
+                            <label class="text-sm text-gray-700">Street</label>
+                            <input
+                                id="street"
+                                v-model="accountStreet"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('street') }"
+                                placeholder="Street"
+                            />
+                            <p v-if="errorBag.has('street')" class="error-message">
+                                {{ errorBag.get("street") }}
+                            </p>
+                        </div>
+                        <div class="flex flex-col pl-2">
+                            <label class="text-sm text-gray-700">Number</label>
+                            <input
+                                id="houseNumber"
+                                v-model="accountHouseNumber"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('houseNumber') }"
+                                placeholder="Number"
+                            />
+                            <p v-if="errorBag.has('houseNumber')" class="error-message">
+                                {{ errorBag.get("houseNumber") }}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="flex flex-row">
+                        <div class="flex flex-col pr-2">
+                            <label class="text-sm text-gray-700">Zip Code</label>
+                            <input
+                                id="zipCode"
+                                v-model="accountZipCode"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('zipCode') }"
+                                placeholder="Zip Code"
+                            />
+                            <p v-if="errorBag.has('zipCode')" class="error-message">
+                                {{ errorBag.get("zipCode") }}
+                            </p>
+                        </div>
+                        <div class="flex flex-col w-full pl-2">
+                            <label class="text-sm text-gray-700">City</label>
+                            <input
+                                id="city"
+                                v-model="accountCity"
+                                type="text"
+                                class="w-full form-input input-text"
+                                :class="{ error: errorBag.has('city') }"
+                                placeholder="City"
+                            />
+                            <p v-if="errorBag.has('city')" class="error-message">{{ errorBag.get("city") }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+    import { useModelWrapper } from "@/use/ModelWrapper";
+    import { Country } from "@/entities/Country";
+    import ErrorBag from "@/use/ErrorBag";
+    import BirthDatePicker from "../../BirthDatePicker.vue";
+
+    export default {
+        name: "RoleSection",
+        components: {
+            BirthDatePicker,
+        },
+        props: {
+            errorBag: {
+                type: ErrorBag,
+                required: true,
+            },
+            editMode: {
+                type: Boolean,
+                required: true,
+            },
+            firstName: {
+                type: String,
+                required: true,
+            },
+            lastName: {
+                type: String,
+                required: true,
+            },
+            birthdayDay: {
+                type: String,
+                required: true,
+            },
+            birthdayMonth: {
+                type: String,
+                required: true,
+            },
+            birthdayYear: {
+                type: String,
+                required: true,
+            },
+            country: {
+                type: String,
+                required: true,
+            },
+            street: {
+                type: String,
+                required: true,
+            },
+            houseNumber: {
+                type: String,
+                required: true,
+            },
+            zipCode: {
+                type: String,
+                required: true,
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+        },
+
+        setup(props: any, { emit }: any) {
+            let countries = Object.values(Country).filter((e) => e != Country.NONE);
+
+            return {
+                countries,
+                accountFirstName: useModelWrapper(props, emit, "firstName"),
+                accountLastName: useModelWrapper(props, emit, "lastName"),
+                accountBirthdayDay: useModelWrapper(props, emit, "birthdayDay"),
+                accountBirthdayMonth: useModelWrapper(props, emit, "birthdayMonth"),
+                accountBirthdayYear: useModelWrapper(props, emit, "birthdayYear"),
+                accountCountry: useModelWrapper(props, emit, "country"),
+                accountStreet: useModelWrapper(props, emit, "street"),
+                accountHouseNumber: useModelWrapper(props, emit, "houseNumber"),
+                accountZipCode: useModelWrapper(props, emit, "zipCode"),
+                accountCity: useModelWrapper(props, emit, "city"),
+            };
+        },
+    };
+</script>
