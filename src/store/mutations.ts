d3d04a0cddf333d0769ede8e6521e012cbc22776
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-27 21:12:39 +0200

    add login data mutation

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -6,6 +6,7 @@ import { Role } from "@/entities/Role";
 export type Mutations<S = State> = {
     [MutationTypes.SET_ID](state: S, payload: string): void;
     [MutationTypes.SET_ROLE](state: S, payload: string): void;
+    [MutationTypes.SET_LOGINDATA](state: S, payload: { username: string; password: string }): void;
 };
 
 export const mutations: MutationTree<State> & Mutations = {
@@ -14,5 +15,8 @@ export const mutations: MutationTree<State> & Mutations = {
     },
     [MutationTypes.SET_ROLE](state: State, payload: Role) {
         state.myRole = payload;
+    },
+    [MutationTypes.SET_LOGINDATA](state: State, payload: { username: string; password: string }) {
+        state.loginData = payload;
     },
 };
