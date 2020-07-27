be7ca7cea87c0a84cbaf1eb0ff897a689fa2d323
Tobias <tobiaswiese@online.de>
2020-07-27 14:15:01 +0200

    replace searchbar in admin account list

--- src/components/AdminAccountList.vue
+++ src/components/AdminAccountList.vue
@@ -1,22 +1,10 @@
 <template>
     <div class="w-full max-w-4xl">
         <div class="flex">
-            <div class="w-full flex flex-row pt-2 mb-8">
-                <div class="w-full relative mx-auto text-gray-600 flex">
-                    <i class="fas fa-search absolute left-0 top-0 mt-4 h-12 ml-4"></i>
-                    <input
-                        id="message"
-                        v-model="message"
-                        class="w-full border-2 border-gray-300 bg-white h-12 px-12 rounded-lg focus:outline-none"
-                        type="search"
-                        placeholder="Filter"
-                    />
-                    <button id="refresh" class="-ml-8 mr-5 focus:outline-none" title="Refresh" @click="refresh">
-                        <i class="inline fas fa-redo-alt text-gray-600 hover:text-gray-700" />
-                    </button>
-                </div>
+            <div class="w-full h-auto items-center flex flex-row pt-2 mb-8">
+                <seach-bar v-model:message="message" @refresh="refresh" />
                 <router-link to="/createAccount" class="w-2/12 w- ml-4">
-                    <button title="Add a new User" class="w-full h-full btn-icon-green items-center justify-center flex flex-row">
+                    <button title="Add a new User" class="w-full p-2 btn-icon-green items-center justify-center flex flex-row">
                         <p class="mr-3 text-lg font-semibold">Add</p>
                         <i class="inline fas fa-user-plus text-lg" />
                     </button>
@@ -43,6 +31,7 @@
 <script lang="ts">
     import AccountList from "./AccountList.vue";
     import LoadingComponent from "./loading/Spinner.vue";
+    import SeachBar from "./SearchBar.vue";
     import { ref } from "vue";
 
     export default {
@@ -50,6 +39,7 @@
         components: {
             AccountList,
             LoadingComponent,
+            SeachBar,
         },
         setup() {
             let message = ref("");
