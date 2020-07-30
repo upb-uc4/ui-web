fbc869903487c32e194fc76c5db91ce8d26707f1
Bastian Haverkamp <bastihav@mail.upb.de>
2020-07-30 16:57:35 +0200

    add profile to admin navbar

--- src/components/navigation/navbar/admin/Navbar.vue
+++ src/components/navigation/navbar/admin/Navbar.vue
@@ -6,18 +6,23 @@
         <li>
             <manage-accounts-menu class="navbar-menu" />
         </li>
+        <template #right>
+            <profile-menu class="pt-4" />
+        </template>
     </base-navbar>
 </template>
 
 <script lang="ts">
     import BaseNavbar from "@/components/navigation/navbar/common/BaseNavbar.vue";
     import ManageAccountsMenu from "@/components/navigation/navbar/admin/administration/ManageAccountsMenu.vue";
+    import ProfileMenu from "../common/profile/ProfileMenu.vue";
 
     export default {
         name: "AdminNavbar",
         components: {
             BaseNavbar,
             ManageAccountsMenu,
+            ProfileMenu,
         },
     };
 </script>
