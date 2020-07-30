3b6a3c847a5a6be7e34cdb3a1f5fb45f62a235a2
theovier <Theo@Harkenbusch.com>
2020-07-30 16:12:03 +0200

    remove placeholder course entries from admin menu

--- src/components/navigation/navbar/admin/administration/ManageAccountsMenuBody.vue
+++ src/components/navigation/navbar/admin/administration/ManageAccountsMenuBody.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="flex flex-col lg:flex-row px-8 py-6 border-b -mx-4">
-        <ul class="w-full lg:w-1/2 px-2">
+        <ul class="w-full px-2">
             <li class="mb-4 hover:bg-gray-200 rounded-lg p-2">
                 <menu-item
                     title="All Users"
@@ -9,24 +9,8 @@
                     target-route-name="admin.home"
                 />
             </li>
-            <li class="mb-4 hover:bg-gray-200 rounded-lg p-2">
-                <menu-item title="All Lecturers" description="List of all lecturers" icon-class="fa-star" target-route-name="admin.home" />
-            </li>
             <li class="hover:bg-gray-200 rounded-lg p-2">
-                <menu-item
-                    title="New Account"
-                    description="Create a new user account"
-                    icon-class="fa-star"
-                    target-route-name="admin.home"
-                />
-            </li>
-        </ul>
-        <ul class="w-full lg:w-1/2 px-4">
-            <li class="mb-4 hover:bg-gray-200 rounded-lg p-2">
-                <menu-item title="All Students" description="List of all students" icon-class="fa-meteor" target-route-name="admin.home" />
-            </li>
-            <li class="hover:bg-gray-200 rounded-lg p-2">
-                <menu-item title="All Admins" description="List of all admins" icon-class="fa-sitemap" target-route-name="admin.home" />
+                <menu-item title="New Account" description="Create a new user" icon-class="fa-star" target-route-name="admin.home" />
             </li>
         </ul>
     </section>
