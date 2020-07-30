bc3e0997b38d4df4adffad31e7e3441046ea0695
theovier <Theo@Harkenbusch.com>
2020-07-30 15:28:52 +0200

    add profile menu to lecturer navbar

--- src/components/navigation/navbar/lecturer/Navbar.vue
+++ src/components/navigation/navbar/lecturer/Navbar.vue
@@ -6,18 +6,23 @@
         <li>
             <course-menu class="navbar-menu" />
         </li>
+        <template #right>
+            <profile-menu class="pt-4" />
+        </template>
     </base-navbar>
 </template>
 
 <script lang="ts">
     import BaseNavbar from "@/components/navigation/navbar/common/BaseNavbar.vue";
     import CourseMenu from "@/components/navigation/navbar/lecturer/CoursesMenu/CourseMenu.vue";
+    import ProfileMenu from "@/components/navigation/navbar/common/profile/ProfileMenu.vue";
 
     export default {
         name: "LecturerNavbar",
         components: {
             BaseNavbar,
             CourseMenu,
+            ProfileMenu,
         },
     };
 </script>
