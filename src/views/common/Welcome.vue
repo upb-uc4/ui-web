938e8404bb2ce971db8939c513b11752759bcbfc
theovier <Theo@Harkenbusch.com>
2020-07-31 17:03:34 +0200

    adjust welcome page text

--- src/views/common/Welcome.vue
+++ src/views/common/Welcome.vue
@@ -1,23 +1,22 @@
 <template>
     <div class="flex flex-col items-center justify-center w-full mt-20">
         <h1 class="text-4xl font-semibold text-blue-800">Welcome back, {{ name }}!</h1>
-        <h2 class="mt-5 text-2xl font-semibold">
-            Welcome to <label class="text-blue-800 text"> University Credits 4.0</label>, the best awesome tool for your university
-            management.
-        </h2>
         <div class="flex flex-col mt-5">
             <h2 class="text-xl">
-                This application is work in progress. If you find any bugs, please report them at
+                This application is work in progress. If you encounter any problems, please let us know on
                 <a
                     target="_blank"
                     class="text-blue-800 cursor-pointer"
                     href="https://github.com/upb-uc4/ui-web/issues/new?assignees=&labels=&template=bug_report.md&title="
-                >GitHub</a
-                >
+                >GitHub</a>
                 and refer to the version numbers below.
-                <a class="text-blue-800 cursor-pointer" target="_blank" href="https://github.com/upb-uc4/ui-web/blob/master/CHANGELOG.md">(Changelog)</a>
             </h2>
-            <p class="mt-2 text-lg">Frontend version: {{ frontEndVersion }}</p>
+            <p class="mt-2 text-lg">
+                Frontend version:
+                <a class="text-blue-800 cursor-pointer" target="_blank" href="https://github.com/upb-uc4/ui-web/blob/master/CHANGELOG.md">{{
+                    frontEndVersion
+                }}</a>
+            </p>
             <p class="text-lg">Authentication version: {{ authenticationManagementVersion }}</p>
             <p class="text-lg">Course management version: {{ courseManagementVersion }}</p>
             <p class="text-lg">User management version: {{ userManagementVersion }}</p>
