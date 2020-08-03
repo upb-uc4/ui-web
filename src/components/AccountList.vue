490b1fb72be345aaf0d04501aa2c3001f4fbb5ec
Tobias <tobiaswiese@online.de>
2020-08-04 01:00:12 +0200

    add first very simple idea

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,7 +1,22 @@
 <template>
-    <div class="bg-white rounded-lg shadow flex flex-col">
-        <div v-for="(user, index) in users" :key="user">
-            <user-row :user="user" :is-first-row="index === 0" :is-last-row="index === users.length - 1" />
+    <div class="flex flex-col">
+        <div class="flex justify-between w-1/2 mb-3">
+            <label >Show:</label>
+            <div v-for="vrole in roles" :key="vrole" class="mb-3 mr-4">
+                <label class="flex items-center">
+                    <input
+                        :id="'role-' + vrole"
+                        type="radio"
+                        class="form-radio radio"
+                        v-model="selectedRole"
+                        :value="vrole"
+                    />
+                    <span class="ml-2 font-medium text-gray-700 text-md">{{ vrole }}</span>
+                </label>
+            </div>
+        </div> 
+        <div class="bg-white rounded-lg shadow" v-for="(user, index) in shownUsers" :key="user">
+            <user-row :user="user" :is-first-row="index === 0" :is-last-row="index === shownUsers.length - 1" />
         </div>
     </div>
 </template>
@@ -11,6 +26,8 @@
     import router from "../router";
     import GenericResponseHandler from "@/use/GenericResponseHandler";
     import UserRow from "@/components/account/UserRow.vue";
+    import { Role } from "@/entities/Role"
+    import { computed, ref } from 'vue';
 
     export default {
         name: "AccountList",
@@ -18,15 +35,22 @@
             UserRow,
         },
         async setup() {
+            let roles = Object.values(Role).filter(e => e != Role.NONE);
+            roles.push("All" as Role)
+            let selectedRole = ref("All" as Role);
+
             const userManagement: UserManagement = new UserManagement();
 
             const genericResponseHandler = new GenericResponseHandler();
             const response = await userManagement.getAllUsers();
             const userLists = genericResponseHandler.handleReponse(response);
             let users = Object.values(userLists).flat();
-
+            let shownUsers = computed(() => {
+                return selectedRole.value == "All" as Role ? users : users.filter(e => e.role == selectedRole.value)
+            })
+            
             return {
-                users,
+                roles, shownUsers,selectedRole
             };
         },
     };
