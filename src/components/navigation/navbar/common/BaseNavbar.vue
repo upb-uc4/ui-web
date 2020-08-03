aebb7f1e956580efb25c4f6d8845034d50eabfad
Tobias <tobiaswiese@online.de>
2020-08-03 15:48:18 +0200

    add routing behind UC4 logo

--- src/components/navigation/navbar/common/BaseNavbar.vue
+++ src/components/navigation/navbar/common/BaseNavbar.vue
@@ -1,15 +1,15 @@
 <template>
-    <header class="flex lg:px-8 px-4 bg-white justify-between bg-gray-800 items-center md:h-20 min-h-20">
-        <div class="flex w-full items-center">
-            <div class="flex items-center pr-8 mr-8 border-gray-100 border-r-4">
-                <a href="#" class="flex text-3xl font-semibold text-gray-100 tracking-wider">
+    <header class="flex items-center justify-between px-4 bg-gray-800 lg:px-8 md:h-20 min-h-20">
+        <div class="flex items-center w-full">
+            <div class="flex items-center pr-8 mr-8 border-r-4 border-gray-100">
+                <button @click="routeLogo" class="flex text-3xl font-semibold tracking-wider text-gray-100 outline-none">
                     UC4
-                </a>
+                </button>
             </div>
 
             <div class="flex w-full">
-                <nav class="md:flex items-center justify-between w-full" :class="{ hidden: !isBurgerMenuOpen }">
-                    <ul class="md:flex justify-between font-semibold text-gray-100 tracking-wider pt-2">
+                <nav class="items-center justify-between w-full md:flex" :class="{ hidden: !isBurgerMenuOpen }">
+                    <ul class="justify-between pt-2 font-semibold tracking-wider text-gray-100 md:flex">
                         <slot></slot>
                     </ul>
                     <div class="pb-3">
@@ -18,8 +18,8 @@
                 </nav>
             </div>
 
-            <button type="button" class="block md:hidden text-gray-100 hover:text-gray-500 focus:outline-none" @click="toggleBurgerMenu">
-                <svg class="h-8 w-8 fill-current" viewBox="0 0 24 24">
+            <button type="button" class="block text-gray-100 md:hidden hover:text-gray-500 focus:outline-none" @click="toggleBurgerMenu">
+                <svg class="w-8 h-8 fill-current" viewBox="0 0 24 24">
                     <path
                         v-if="isBurgerMenuOpen"
                         fill-rule="evenodd"
@@ -37,18 +37,27 @@
 </template>
 
 <script lang="ts">
-    import { ref } from "vue";
+    import { ref, computed } from "vue";
+    import { useStore } from "@/store/store";
+    import Router from "@/router/index"
 
     export default {
         name: "BaseNavbar",
         setup() {
             const isBurgerMenuOpen = ref(false);
 
+            function routeLogo() {
+                let store = useStore();
+                let loggedIn = store.getters.loggedIn;
+                let logoTargetRoute = loggedIn ? "welcome" : "login";
+                Router.push(logoTargetRoute);
+            }
+
             function toggleBurgerMenu() {
                 isBurgerMenuOpen.value = !isBurgerMenuOpen.value;
             }
 
-            return { isBurgerMenuOpen, toggleBurgerMenu };
+            return { isBurgerMenuOpen, routeLogo, toggleBurgerMenu };
         },
     };
 </script>
