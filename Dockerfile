970e4834f68f4c8e5761b8c01abc89cd0940113c
Bastian Haverkamp <bastihav@mail.upb.de>
2020-08-04 12:31:52 +0200

    add localhost environment and move production endpoints to..
    docker build arguments

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM node:latest as build-stage
+ARG ENV_PUBLIC_PATH
+ARG ENV_BACKEND_URL
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY ./ .
+RUN sed -i "s|\$ENV_BACKEND_URL|$ENV_BACKEND_URL|g" .env.production
+RUN sed -i "s|\$ENV_PUBLIC_PATH|$ENV_PUBLIC_PATH|g" vue.config.js
 RUN npm run build
 
 FROM nginx as production-stage
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,6 @@
-process.env.VUE_APP_VERSION = require("./package.json").version;
+require("custom-env").env();
 
-// do not add trailing forward slash
-process.env.VUE_APP_API_BASE_URL =
-    process.env.NODE_ENV === "production"
-        ? "https://uc4.cs.uni-paderborn.de/api/production"
-        : "https://uc4.cs.uni-paderborn.de/api/develop";
+process.env.VUE_APP_VERSION = require("./package.json").version;
 
 module.exports = {
     chainWebpack: (config) => {
@@ -12,5 +8,6 @@ module.exports = {
             fix: false,
         });
     },
-    publicPath: process.env.NODE_ENV === "production" ? "/deploy/" : "/",
+    //     /deploy/
+    publicPath: process.env.NODE_ENV === "production" ? "$ENV_PUBLIC_PATH" : "/",
 };
