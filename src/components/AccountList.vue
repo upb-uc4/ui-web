ca10fd0d95eb914d8955bcdcfdf59f3be3157686
Tobias <tobiaswiese@online.de>
2020-08-04 12:14:31 +0200

    move role selection to parent
    now the selection will persist on list refresh

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="flex flex-col">
-        <role-filter v-model:selected-role="selectedRole"/>
         <div class="bg-white rounded-lg shadow" v-for="(user, index) in shownUsers" :key="user">
             <user-row :user="user" :is-first-row="index === 0" :is-last-row="index === shownUsers.length - 1" />
         </div>
@@ -14,17 +13,20 @@
     import UserRow from "@/components/account/UserRow.vue";
     import { Role } from "@/entities/Role";
     import { computed, ref } from 'vue';
-    import RoleFilter from "./RoleFilter.vue";
+    
 
     export default {
         name: "AccountList",
         components: {
-            RoleFilter,
             UserRow,
         },
-        async setup() {
-            let selectedRole = ref("All" as Role);
-
+        props: {
+            selectedRole: {
+                type: String,
+                required: true,
+            }
+        },
+        async setup(props:any) {
             const userManagement: UserManagement = new UserManagement();
 
             const genericResponseHandler = new GenericResponseHandler();
@@ -32,12 +34,12 @@
             const userLists = genericResponseHandler.handleReponse(response);
             let users = Object.values(userLists).flat();
             let shownUsers = computed(() => {
-                return selectedRole.value == "All" as Role ? users : users.filter(e => e.role == selectedRole.value)
+                return props.selectedRole == "All" as Role ? users : users.filter(e => e.role == props.selectedRole)
             })
 
             
             return {
-                shownUsers,selectedRole
+                shownUsers
             };
         },
     };
