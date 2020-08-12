aafd9524b59e900a1263d0967d00800cbe17c28f
theovier <Theo@Harkenbusch.com>
2020-08-12 16:47:11 +0200

    change cursor on menu hook to default instead of being select cursor

--- src/components/navigation/navbar/common/BaseMenu.vue
+++ src/components/navigation/navbar/common/BaseMenu.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="relative" @mouseleave="hide" @mouseover="show">
-        <slot name="hook"></slot>
+        <div class="cursor-default">
+            <slot name="hook"></slot>
+        </div>
+
         <div v-show="isVisible">
             <slot name="content"></slot>
         </div>
