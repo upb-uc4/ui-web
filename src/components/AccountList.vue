07aac9176f79caf06fffe2b9742f1162206df226
Tobias <tobiaswiese@online.de>
2020-08-12 17:31:28 +0200

    remove suspense for account list

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class="flex flex-col bg-white rounded-lg shadow">
+    <div v-if="!busy" class="flex flex-col bg-white rounded-lg shadow">
         <div v-for="(user, index) in shownUsers" :key="user">
             <user-row :user="user" :is-first-row="index === 0" :is-last-row="index === shownUsers.length - 1" />
         </div>
+    </div>
+    <div v-else>
+        <loading-spinner />
     </div>
 </template>
 
@@ -12,12 +15,15 @@
     import GenericResponseHandler from "@/use/GenericResponseHandler";
     import UserRow from "@/components/account/UserRow.vue";
     import { Role } from "@/entities/Role";
-    import { computed, ref } from "vue";
+    import { computed, ref, onBeforeMount, watch } from "vue";
+    import LoadingSpinner from "@/components/loading/Spinner.vue";
+    import User from "@/api/api_models/user_management/User";
 
     export default {
         name: "AccountList",
         components: {
             UserRow,
+            LoadingSpinner,
         },
         props: {
             selectedRole: {
@@ -25,19 +31,32 @@
                 required: true,
             },
         },
-        async setup(props: any) {
-            const userManagement: UserManagement = new UserManagement();
+        setup(props: any) {
+            let busy = ref(false);
+            let users = ref([] as User[]);
 
-            const genericResponseHandler = new GenericResponseHandler();
-            const response = await userManagement.getAllUsers();
-            const userLists = genericResponseHandler.handleReponse(response);
-            let users = Object.values(userLists).flat();
+            onBeforeMount(() => {
+                getUsers();
+            });
+
+            async function getUsers() {
+                busy.value = true;
+                const userManagement: UserManagement = new UserManagement();
+
+                const genericResponseHandler = new GenericResponseHandler();
+                const response = await userManagement.getAllUsers();
+                const userLists = genericResponseHandler.handleReponse(response);
+                users.value = Object.values(userLists).flat();
+                busy.value = false;
+            }
+
             let shownUsers = computed(() => {
-                return props.selectedRole == ("All" as Role) ? users : users.filter((e) => e.role == props.selectedRole);
+                return props.selectedRole == ("All" as Role) ? users.value : users.value.filter((e) => e.role == props.selectedRole);
             });
 
             return {
                 shownUsers,
+                busy,
             };
         },
     };
