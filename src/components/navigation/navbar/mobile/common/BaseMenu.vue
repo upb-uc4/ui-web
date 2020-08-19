0c7c00d918e89e5a3aaea0eae328f6429331fac2
theovier <Theo@Harkenbusch.com>
2020-08-19 20:27:51 +0200

    add toggle on mobile menus

--- src/components/navigation/navbar/mobile/common/BaseMenu.vue
+++ src/components/navigation/navbar/mobile/common/BaseMenu.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <div class="cursor-pointer">
+        <div class="cursor-pointer" @click="toggleOpen">
+            <i class="fas mr-2 w-4" :class="[isOpen ? 'fa-chevron-down' : 'fa-chevron-right']" />
             <slot name="hook"></slot>
         </div>
-        <slot name="content"></slot>
+        <div v-show="isOpen">
+            <slot name="content"></slot>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
+    import { ref } from "vue";
+
     export default {
         name: "MobileBaseMenu",
         setup() {
-            //todo: make the hook collapse its content
+            const isOpen = ref(true);
+            function toggleOpen() {
+                isOpen.value = !isOpen.value;
+            }
+            return { isOpen, toggleOpen };
         },
     };
 </script>
