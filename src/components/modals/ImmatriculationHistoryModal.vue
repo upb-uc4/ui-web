5562ba2fe2e7434d803350fb2791e428bea7a70d
Tobias Wiese <tobiaswiese@online.de>
2020-08-19 14:02:27 +0200

    remove caching of history modal and add refresh option

--- src/components/modals/ImmatriculationHistoryModal.vue
+++ src/components/modals/ImmatriculationHistoryModal.vue
@@ -1,14 +1,17 @@
 <template>
     <modal ref="baseModal" :action="action" @cancel="close(action.CLOSE)">
         <template #header>
-            <p class="text-2xl text-gray-900">Immatriculation History</p>
+            <h1 class="text-2xl text-gray-900">Immatriculation History</h1>
         </template>
         <div v-if="busy" class="w-full items-center justify-center">
             <loading-spinner />
         </div>
-        <immatriculation-history v-if="alreadyLoadedOnce" v-model:busy="busy" :username="username" />
+        <immatriculation-history :key="historyRefreshKey" v-model:busy="busy" :username="username" />
         <template #footer>
-            <button id="immatriculationHistoryClose" class="mr-10 btn-tertiary" @click="close(action.CLOSE)">Close</button>
+            <div class="flex justify-between">
+                <button id="immatriculationHistoryRefresh" title="Refresh" class="btn-tertiary mr-10" @click="refresh">Refresh</button>
+                <button id="immatriculationHistoryClose" class="mr-10 btn-tertiary" @click="close(action.CLOSE)">Close</button>
+            </div>
         </template>
     </modal>
 </template>
@@ -31,6 +34,7 @@
             let busy = ref(false);
             let alreadyLoadedOnce = ref(false);
             let username = ref("");
+            let historyRefreshKey = ref(false);
 
             async function getOwnUserName() {
                 const store = useStore();
@@ -42,10 +46,7 @@
             }
 
             async function show() {
-                if (!alreadyLoadedOnce.value) {
-                    getOwnUserName();
-                }
-                alreadyLoadedOnce.value = true;
+                getOwnUserName();
                 return await baseModal.value.show();
             }
 
@@ -53,7 +54,11 @@
                 baseModal.value.close(action);
             }
 
-            return { baseModal, show, close, action, alreadyLoadedOnce, busy, username };
+            function refresh() {
+                historyRefreshKey.value = !historyRefreshKey.value;
+            }
+
+            return { baseModal, show, close, action, alreadyLoadedOnce, busy, username, refresh, historyRefreshKey };
         },
     };
 </script>
