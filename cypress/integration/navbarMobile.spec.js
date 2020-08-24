03f76fad5e2e586871ca591456f77d7f661324f4
theovier <Theo@Harkenbusch.com>
2020-08-24 18:16:37 +0200

    add further admin mobile navbar asserts

--- cypress/integration/navbarMobile.spec.js
+++ cypress/integration/navbarMobile.spec.js
@@ -1,5 +1,6 @@
 const navbar_burger_menu = 'button[id="nav_mobile_toggle_menu"]';
 const navbar_about = "#nav_mobile_common_about";
+const navbar_guest_login = "#nav_mobile_guest_login";
 
 beforeEach(() => {
     cy.viewport('iphone-x');
@@ -7,7 +8,6 @@ beforeEach(() => {
 
 describe("guest", () => {
     it("login", () => {
-        const navbar_guest_login = "#nav_mobile_guest_login";
         cy.visit("/");
         cy.get(navbar_burger_menu).click();
         cy.get(navbar_guest_login).click();
@@ -23,7 +23,27 @@ describe("guest", () => {
 });
 
 describe("admin", () => {
-    //TODO implement
+    it("login", () => {
+        cy.visit("/");
+        cy.get(navbar_burger_menu).click();
+        cy.get(navbar_guest_login).click();
+        cy.get("input[id='email']").type("admin");
+        cy.get("input[id='password']").type("admin");
+        cy.get('button[id="login"]').click();
+        cy.url().should("contain", "welcome");
+    });
+
+    it("courses", () => {
+        const navbar_admin_manage_accounts = "#nav_mobile_admin_menu_manage_accounts";
+        cy.get(navbar_burger_menu).click();
+        cy.get(navbar_admin_manage_accounts).trigger("mouseover");
+
+        // TODO test menu items
+    });
+
+    it("profile", () => {
+        // TODO implement
+    });
 });
 
 describe("lecturer", () => {
