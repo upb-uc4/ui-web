5c7ba88b27af9436c9b05ba813efe7925d1c64ec
theovier <Theo@Harkenbusch.com>
2020-08-25 17:20:41 +0200

    add ids to mobile menu items

--- src/components/navigation/navbar/mobile/lecturer/courses/CourseMenuBody.vue
+++ src/components/navigation/navbar/mobile/lecturer/courses/CourseMenuBody.vue
@@ -2,13 +2,28 @@
     <section class="flex flex-col px-2 pt-3 pb-1">
         <ul class="w-full px-2">
             <li class="cursor-pointer mb-2 rounded-lg p-2 hover:bg-blue-800">
-                <menu-item title="All Courses" icon-class="fa-globe-americas" target-route-name="lecturer.courses" />
+                <menu-item
+                    id="nav_mobile_lecturer_menu_courses_all"
+                    title="All Courses"
+                    icon-class="fa-globe-americas"
+                    target-route-name="lecturer.courses"
+                />
             </li>
             <li class="cursor-pointer mb-2 rounded-lg p-2 hover:bg-blue-800">
-                <menu-item title="Create Course" icon-class="fa-star" target-route-name="courseForm.create" />
+                <menu-item
+                    id="nav_mobile_lecturer_menu_courses_create"
+                    title="Create Course"
+                    icon-class="fa-star"
+                    target-route-name="courseForm.create"
+                />
             </li>
             <li class="cursor-pointer rounded-lg p-2 hover:bg-blue-800">
-                <menu-item title="My Courses" icon-class="fa-meteor" target-route-name="lecturer.courses" />
+                <menu-item
+                    id="nav_mobile_lecturer_menu_courses_my_courses"
+                    title="My Courses"
+                    icon-class="fa-meteor"
+                    target-route-name="lecturer.courses"
+                />
             </li>
         </ul>
     </section>
--- src/components/navigation/navbar/mobile/student/courses/CourseMenuBody.vue
+++ src/components/navigation/navbar/mobile/student/courses/CourseMenuBody.vue
@@ -2,7 +2,12 @@
     <section class="flex flex-col px-2 pt-3 pb-1">
         <ul class="w-full px-2">
             <li class="cursor-pointer mb-2 rounded-lg p-2 hover:bg-blue-800">
-                <menu-item title="All Courses" icon-class="fa-globe-americas" target-route-name="student.courses" />
+                <menu-item
+                    id="nav_mobile_student_menu_courses_all"
+                    title="All Courses"
+                    icon-class="fa-globe-americas"
+                    target-route-name="student.courses"
+                />
             </li>
         </ul>
     </section>
