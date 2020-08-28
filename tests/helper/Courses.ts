67c54585a4992e5d9d8eaf374c2544de9063731c
Bastian Haverkamp <bastihav@mail.upb.de>
2020-08-28 12:14:59 +0200

    fix course unit test helper

--- tests/helper/Courses.ts
+++ tests/helper/Courses.ts
@@ -3,7 +3,7 @@ import Course from "@/api/api_models/course_management/Course";
 
 export function getRandomizedCourse(): Course {
     const random = Math.floor(Math.random() * 9999);
-    const course = JSON.parse(fs.readFileSync("@/../tests/fixtures/course.json", "utf-8")) as Course;
+    const course = JSON.parse(fs.readFileSync("tests/fixtures/course.json", "utf-8")) as Course;
     course.courseName = course.courseName + "-" + random;
 
     return course;
