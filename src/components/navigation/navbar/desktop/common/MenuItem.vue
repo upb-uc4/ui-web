2c209d988440f30b2dcecfedb722fd021c996acb
theovier <Theo@Harkenbusch.com>
2020-09-01 16:25:40 +0200

    fix admin menus click area

--- src/components/navigation/navbar/desktop/common/MenuItem.vue
+++ src/components/navigation/navbar/desktop/common/MenuItem.vue
@@ -1,16 +1,18 @@
 <template>
     <router-link :to="{ name: targetRouteName }">
-        <div class="flex items-center">
-            <i class="fas text-lg text-blue-500 group-hover:text-blue-800 w-4" :class="iconClass" />
-            <span class="ml-4">
-                <span class="block font-bold text-blue-800 group-hover:text-blue-800 flex items-center">{{ title }}</span>
-            </span>
-            <div v-if="isHorizontallyAligned" class="ml-4">
+        <div class="hover:bg-gray-200 rounded-lg p-2">
+            <div class="flex items-center">
+                <i class="fas text-lg text-blue-500 group-hover:text-blue-800 w-4" :class="iconClass" />
+                <span class="ml-4">
+                    <span class="block font-bold text-blue-800 group-hover:text-blue-800 flex items-center">{{ title }}</span>
+                </span>
+                <div v-if="isHorizontallyAligned" class="ml-4">
+                    <span class="block text-sm text-gray-600 group-hover:text-blue-800">{{ description }}</span>
+                </div>
+            </div>
+            <div v-if="!isHorizontallyAligned" class="ml-8">
                 <span class="block text-sm text-gray-600 group-hover:text-blue-800">{{ description }}</span>
             </div>
-        </div>
-        <div v-if="!isHorizontallyAligned" class="ml-8">
-            <span class="block text-sm text-gray-600 group-hover:text-blue-800">{{ description }}</span>
         </div>
     </router-link>
 </template>
