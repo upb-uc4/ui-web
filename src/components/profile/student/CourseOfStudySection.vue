f12877affce49474af1244ef17fd2196ab80e2b0
theovier <Theo@Harkenbusch.com>
2020-09-02 19:36:58 +0200

    adjust size of matriuclation id on mobile screens

--- src/components/profile/student/CourseOfStudySection.vue
+++ src/components/profile/student/CourseOfStudySection.vue
@@ -5,35 +5,27 @@
                 <div class="flex mb-2 align-baseline">
                     <label class="block text-gray-700 text-lg font-medium">Course of Study</label>
                 </div>
-                <label class="block text-gray-600">
-                    Information related to your individual academic career.
-                </label>
+                <label class="block text-gray-600"> Information related to your individual academic career. </label>
             </div>
-            <div class="w-full lg:w-2/3">
-                <div class="mb-6 flex flex-col w-2/3">
-                    <div class="flex flex-row">
-                        <div class="flex flex-col w-1/2">
-                            <label class="mb-3 text-sm font-medium text-gray-700 w-full">Matriculation-ID</label>
-                            <input
-                                id="matriculationId"
-                                :value="matriculationId"
-                                disabled
-                                type="text"
-                                class="w-full form-input input-text"
-                                placeholder="Matriculation-ID"
-                            />
-                        </div>
-                        <div v-if="latest != ''" class="ml-4 flex flex-col w-1/2">
-                            <label class="mb-3 text-sm font-medium text-gray-700 flex">
-                                Latest Immatriculation (
-                                <button class="cursor-pointer text-sm font-medium navigation-link" @click="showHistory">
-                                    View History
-                                </button>
-                                )
-                            </label>
-                            <input id="latestImmatriculation" disabled class="form-input input-text" :value="latest" />
-                        </div>
-                    </div>
+            <div class="mb-6 flex flex-col w-full lg:w-2/3">
+                <div class="flex flex-col lg:w-1/2 w-full">
+                    <label class="mb-3 text-sm font-medium text-gray-700">Matriculation-ID</label>
+                    <input
+                        id="matriculationId"
+                        :value="matriculationId"
+                        disabled
+                        type="text"
+                        class="w-full form-input input-text"
+                        placeholder="Matriculation-ID"
+                    />
+                </div>
+                <div v-if="latest != ''" class="ml-4 flex flex-col w-1/2">
+                    <label class="mb-3 text-sm font-medium text-gray-700 flex">
+                        Latest Immatriculation (
+                        <button class="cursor-pointer text-sm font-medium navigation-link" @click="showHistory">View History</button>
+                        )
+                    </label>
+                    <input id="latestImmatriculation" disabled class="form-input input-text" :value="latest" />
                 </div>
             </div>
         </div>
