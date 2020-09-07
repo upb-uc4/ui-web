07825dc98488006e5fa727d790988eefdcb56bed
Tobias Wiese <tobiaswiese@online.de>
2020-09-07 12:53:49 +0200

    add mobile menu items

---
+++ src/components/navigation/navbar/mobile/admin/courses/CourseMenuBody.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="flex flex-col px-2 pt-3 pb-1">
+        <ul class="w-full px-2">
+            <li class="cursor-pointer mb-2 rounded-lg p-2 hover:bg-blue-800">
+                <menu-item
+                    id="nav_mobile_admin_menu_courses_all"
+                    title="All Courses"
+                    icon-class="fa-globe-americas"
+                    target-route-name="shared.courses"
+                />
+            </li>
+            <li class="cursor-pointer mb-2 rounded-lg p-2 hover:bg-blue-800">
+                <menu-item
+                    id="nav_mobile_admin_menu_courses_create"
+                    title="Create Course"
+                    icon-class="fa-star"
+                    target-route-name="courseForm.create"
+                />
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script lang="ts">
+    import MenuItem from "../../common/MenuItem.vue";
+
+    export default {
+        name: "MobileLecturerCourseMenuBody",
+        components: {
+            MenuItem,
+        },
+    };
+</script>
