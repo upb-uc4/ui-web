74b2542f449cc8f57cbfcd860a5502554e42635c
Bastian Haverkamp <bastihav@mail.upb.de>
2020-09-15 17:38:28 +0200

    try again

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,26 @@
 process.env.VUE_APP_VERSION = require("./package.json").version;
 const fs = require("fs");
 
+let endpoint = "https://uc4.cs.uni-paderborn.de/api/development/";
+
 // do not add trailing forward slash
 switch (process.env.NODE_ENV) {
     case "production":
         process.env.VUE_APP_API_BASE_URL = "https://uc4.cs.uni-paderborn.de/api/production";
+        endpoint = "https://uc4.cs.uni-paderborn.de/api/production/";
         break;
     case "development":
         process.env.VUE_APP_API_BASE_URL = "/api1";
+        endpoint = "https://uc4.cs.uni-paderborn.de/api/development/";
         break;
     case "experimental":
         process.env.VUE_APP_API_BASE_URL = "/api2";
+        endpoint = "https://uc4.cs.uni-paderborn.de/api/experimental/";
         break;
     default:
         process.env.VUE_APP_API_BASE_URL = "/api3";
         break;
 }
-
-const endpoint = "https://uc4.cs.uni-paderborn.de/api/experimental/";
-// const endpoint = "https://uc4.cs.uni-paderborn.de/api/development/"
 
 module.exports = {
     chainWebpack: (config) => {
@@ -27,7 +29,8 @@ module.exports = {
         });
         config.plugins.delete("prefetch");
     },
-    publicPath: process.env.NODE_ENV === "production" ? "/deploy/" : "",
+    //publicPath: process.env.NODE_ENV === "production" ? "/deploy/" : "",
+    publicPath: "/deploy/",
     devServer: {
         https: {
             key: fs.readFileSync("./certs/server-key.pem"),
@@ -37,7 +40,12 @@ module.exports = {
         proxy: {
             "/api1/": {
                 target: endpoint,
-                pathRewrite: { "^/api": "" },
+                pathRewrite: { "^/api1": "" },
+                changeOrigin: true,
+            },
+            "/api2/": {
+                target: endpoint,
+                pathRewrite: { "^/api2": "" },
                 changeOrigin: true,
             },
         },
