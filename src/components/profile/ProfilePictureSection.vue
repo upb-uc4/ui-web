c7c713c81f02fc2a72786308cc48af8bc380dd31
Tobias Wiese <tobiaswiese@online.de>
2020-09-22 11:27:49 +0200

    add api calls

--- src/components/profile/ProfilePictureSection.vue
+++ src/components/profile/ProfilePictureSection.vue
@@ -8,19 +8,23 @@
                 <img class="h-48 w-48 object-cover mb-5 rounded-full border border-gray-500 block" :src="selectedPicture" />
                 <input id="uploadFile" hidden type="file" accept="*.jpeg, *.png, *.jpg" @change="uploadPicture" />
                 <div class="flex">
-                    <button id="uploadPicture" class="btn btn-blue-primary w-48" @click="triggerFileUpload">Select Image</button>
+                    <button id="uploadPicture" :disabled="busy" class="btn btn-blue-primary w-48" @click="triggerFileUpload">
+                        Select Image
+                    </button>
                     <button
                         v-if="pictureChanged"
                         id="confirmPicture"
+                        :disabled="busy"
                         title="Confirm Profile Picture"
                         class="btn btn-icon-green ml-3 text-xl w-10"
                         @click="confirmPicture"
                     >
-                        <i class="far fa-check"></i>
+                        <i class="fas fa-check"></i>
                     </button>
                     <button
                         v-if="pictureChanged"
                         id="resetPicture"
+                        :disabled="busy"
                         title="Reset Profile Picture"
                         class="btn btn-icon-red ml-3 text-xl w-10"
                         @click="resetPicture"
@@ -37,16 +41,15 @@
     import { Role } from "@/entities/Role";
     import { useModelWrapper } from "@/use/helpers/ModelWrapper";
     import ErrorBag from "@/use/helpers/ErrorBag";
-    import { computed, ref, watch } from "vue";
+    import { computed, onBeforeMount, ref, watch } from "vue";
     import { cloneDeep } from "lodash";
+    import UserManagement from "@/api/UserManagement";
+    import GenericResponseHandler from "@/use/helpers/GenericResponseHandler";
+    import ProfilePictureUpdateResponseHandler from "@/use/helpers/ProfilePictureUpdateResponseHandler";
 
     export default {
         name: "PrivateProfilePictureSection",
         props: {
-            errorBag: {
-                type: ErrorBag,
-                required: true,
-            },
             username: {
                 type: String,
                 required: true,
@@ -54,8 +57,31 @@
         },
 
         setup(props: any, { emit }: any) {
-            const selectedPicture = ref(props.picture);
-            const fallbackPicture = cloneDeep(props.picture);
+            const selectedPicture = ref();
+            let fallbackPicture: any;
+            const busy = ref(false);
+
+            onBeforeMount(() => {
+                getProfilePicture();
+            });
+
+            async function getProfilePicture() {
+                busy.value = true;
+                const userManagement = new UserManagement();
+                const response = await userManagement.getProfilePicture(props.username);
+                const handler = new GenericResponseHandler();
+                const result = handler.handleReponse(response);
+                if (result.arrayBuffer != undefined) {
+                    selectedPicture.value = result;
+                    fallbackPicture = selectedPicture.value;
+                } else {
+                    //TODO Show Toast
+                    console.log("Error: Loading Profile Picture Failed");
+                    selectedPicture.value = "";
+                    fallbackPicture = selectedPicture.value;
+                }
+                busy.value = false;
+            }
 
             const pictureChanged = computed(() => {
                 const value = selectedPicture.value !== fallbackPicture;
@@ -78,16 +104,33 @@
                 };
             }
 
+            async function confirmPicture() {
+                busy.value = true;
+                console.log("fired");
+                const userManagement = new UserManagement();
+                const response = await userManagement.updateProfilePicture(props.username, selectedPicture.value);
+                const handler = new ProfilePictureUpdateResponseHandler();
+                const result = await handler.handleReponse(response);
+                if (result) {
+                    fallbackPicture = selectedPicture.value;
+                } else {
+                    console.log("Error: Uploading profile picture failed!");
+                }
+                busy.value = false;
+            }
+
             function resetPicture() {
                 selectedPicture.value = fallbackPicture;
             }
 
             return {
+                busy,
                 uploadPicture,
                 selectedPicture,
                 triggerFileUpload,
                 resetPicture,
                 pictureChanged,
+                confirmPicture,
             };
         },
     };
