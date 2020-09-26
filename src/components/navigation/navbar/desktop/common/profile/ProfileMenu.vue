5e8920dd2a4c2bddb229917b392de42fa68efd9e
theovier <Theo@Harkenbusch.com>
2020-09-26 14:42:02 +0200

    fix a visual bug that the sign out menu item had a line break

--- src/components/navigation/navbar/desktop/common/profile/ProfileMenu.vue
+++ src/components/navigation/navbar/desktop/common/profile/ProfileMenu.vue
@@ -9,8 +9,8 @@
             </div>
         </template>
         <template #content>
-            <aside class="absolute bg-transparent md:w-48 right-0 h-8" />
-            <div class="absolute top-auto right-0 bg-white z-30 lg:z-10 shadow-md rounded-lg overflow-hidden border lg:w-48">
+            <aside class="absolute bg-transparent md:w-56 right-0 h-8" />
+            <div class="absolute top-auto right-0 bg-white z-30 lg:z-10 shadow-md rounded-lg overflow-hidden border lg:w-56">
                 <menu-body />
             </div>
         </template>
