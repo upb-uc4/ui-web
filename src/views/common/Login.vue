d8f2277e2b12b3ea1ac6a8d6a7f023c41108b126
Bastian Haverkamp <bastihav@mail.upb.de>
2020-10-13 21:51:23 +0200

    decode proposal protobuf draft

--- src/views/common/Login.vue
+++ src/views/common/Login.vue
@@ -75,7 +75,8 @@
     import { ref, onMounted } from "vue";
     import LoginResponseHandler from "@/use/helpers/LoginResponseHandler";
     import AuthenticationManagement from "@/api/AuthenticationManagement";
-    import { createKeyPair, createCSR, deriveKeyFromPassword } from "@/use/crypto/certificates";
+    import { createKeyPair, createCSR, deriveKeyFromPassword, base64ToArrayBuffer } from "@/use/crypto/certificates";
+    import { load, Root } from "protobufjs";
 
     export default {
         components: {},
@@ -89,6 +90,34 @@
 
             async function togglePassword() {
                 passwordFieldType.value = isPasswordVisible() ? "password" : "text";
+                let r = new Root({});
+
+                await load("hlf-proto.json", function (err, root) {
+                    if (err) throw err;
+                    if (!root) return;
+                    const proposalMessage = root.lookupType("protos.Proposal");
+                    const chaincodeProposalPayload = root.lookupType("protos.ChaincodeProposalPayload");
+                    const chaincodeInvocationSpec = root.lookupType("protos.ChaincodeInvocationSpec");
+                    const chaincodeInput = root.lookupType("protos.ChaincodeInput");
+                    let base64 =
+                        "CpoJCmsIAxABGgwIovOW/AUQuMKLtAMiCW15Y2hhbm5lbCpANDU1YzFlNjhlODE2NDhkYjFmNDg2ZTg1NGM4YzAwZDAyYmZlNjhlNWI1OTJiMTcyZTJlZmQxYmVkMmEzOTkwYjoKEggSBnVjNC1jYxKqCAqNCAoHb3JnMU1TUBKBCC0tLS0tQkVHSU4gQ0VSVElGSUNBVEUtLS0tLQpNSUlDeHpDQ0FtMmdBd0lCQWdJVVpIQWszaDZ2OTVqQjFRNWlYOTJaZlBiTGV3OHdDZ1lJS29aSXpqMEVBd0l3CllERUxNQWtHQTFVRUJoTUNWVk14RnpBVkJnTlZCQWdURGs1dmNuUm9JRU5oY205c2FXNWhNUlF3RWdZRFZRUUsKRXd0SWVYQmxjbXhsWkdkbGNqRVBNQTBHQTFVRUN4TUdSbUZpY21sak1SRXdEd1lEVlFRREV3aHlZMkV0YjNKbgpNVEFlRncweU1ERXdNVE14TkRJek1EQmFGdzB5TVRFd01UTXhOREk0TURCYU1EZ3hEakFNQmdOVkJBc1RCV0ZrCmJXbHVNU1l3SkFZRFZRUURFeDF6WTJGc1lTMXlaV2RwYzNSeVlYUnBiMjR0WVdSdGFXNHRiM0puTVRCWk1CTUcKQnlxR1NNNDlBZ0VHQ0NxR1NNNDlBd0VIQTBJQUJBQndhOG1DMXF0ZldXZFQxV241dUE1bmdzMUlkeFFpcG81egpHN0k3RVFUQm1QQTRDNjljWHJtR2dJeEVONEtTVnpQR2RsV0Zzdy9wVWNqaWRQK2RLYnFqZ2dFck1JSUJKekFPCkJnTlZIUThCQWY4RUJBTUNBNmd3SFFZRFZSMGxCQll3RkFZSUt3WUJCUVVIQXdFR0NDc0dBUVVGQndNQ01Bd0cKQTFVZEV3RUIvd1FDTUFBd0hRWURWUjBPQkJZRUZBRzZ4TGpPem9WZTVxMS9GcjRiMjJjb1A1RlpNQjhHQTFVZApJd1FZTUJhQUZKYkF2M0VWU2kzcGs1TGV1R3JkQ1VMTXZjNEpNQ2dHQTFVZEVRUWhNQitDSFhOallXeGhMWEpsCloybHpkSEpoZEdsdmJpMWhaRzFwYmkxdmNtY3hNSDRHQ0NvREJBVUdCd2dCQkhKN0ltRjBkSEp6SWpwN0ltRmsKYldsdUlqb2lkSEoxWlNJc0ltaG1Ma0ZtWm1sc2FXRjBhVzl1SWpvaUlpd2lhR1l1Ulc1eWIyeHNiV1Z1ZEVsRQpJam9pYzJOaGJHRXRjbVZuYVhOMGNtRjBhVzl1TFdGa2JXbHVMVzl5WnpFaUxDSm9aaTVVZVhCbElqb2lZV1J0CmFXNGlmWDB3Q2dZSUtvWkl6ajBFQXdJRFNBQXdSUUloQU1ESndadVc0WUhVaDNxNThuWG1FaTdud05xVWdlcTQKcFZjWTArWG5uSmR4QWlCdS9sdXluWk9qbU45MDhURUdpaDExZjNGM1pkUDFMUityQVRGSURONm1Rdz09Ci0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0KEhh4ouW91qGu0AcpeWHgHDEsu4WjcIpuls8SQQo/Cj0IARIIEgZ1YzQtY2MaLwoeVUM0LkNlcnRpZmljYXRlOmFkZENlcnRpZmljYXRlCgMxMDEKCFdoYXRldmVy";
+                    let buf = base64ToArrayBuffer(base64);
+
+                    let message = proposalMessage.decode(new Uint8Array(buf));
+                    console.log(message);
+                    let message2 = chaincodeProposalPayload.decode((message as any)["payload"]);
+                    console.log(message2);
+                    let message3 = chaincodeInvocationSpec.decode((message2 as any)["input"]);
+                    console.log(message3);
+                    console.log((message3 as any)["chaincodeSpec"]["input"]);
+                    //let message4 = chaincodeInput.decode((message3 as any)["chaincodeSpec"]["input"])
+
+                    let args = (message3 as any)["chaincodeSpec"]["input"]["args"] as Uint8Array[];
+
+                    args.forEach((e) => console.log(new TextDecoder("utf-8").decode(e)));
+
+                    //console.log(message4);
+                });
             }
 
             function hideErrors() {
