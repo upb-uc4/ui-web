94aa472a5f56c8dfb13115dae948eee673f140fa
theovier <Theo@Harkenbusch.com>
2020-10-16 16:22:44 +0200

    adjust course for mobile AND desktop

--- src/components/course/list/lecturer/LecturerCourse.vue
+++ src/components/course/list/lecturer/LecturerCourse.vue
@@ -1,14 +1,13 @@
 <template>
     <div :id="'course_' + course.courseId" class="flex shadow-xl">
-        <!-- TODO mobile view only for testing purposes -->
-
-        <div class="flex flex-col w-full p-4 bg-white rounded-lg">
-            <div class="flex items-baseline justify-between">
+        <div class="flex flex-col w-full p-4 sm:px-8 bg-white rounded-lg">
+            <div class="flex items-baseline justify-between sm:justify-start">
                 <span class="inline-block px-2 text-xs font-semibold tracking-wide text-teal-800 uppercase bg-teal-200 rounded-full">
                     {{ course.courseType }}
                 </span>
                 <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">{{ course.ects }} ECTS</div>
                 <div class="ml-4 text-xl font-semibold tracking-wide text-gray-600 uppercase">🇬🇧</div>
+                <!-- TODO use flags for mobile and text for non-mobile -->
                 <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">
                     <i class="inline text-lg fas fa-users" />
                     {{ course.currentParticipants }} / {{ course.maxParticipants }}
@@ -26,14 +25,22 @@
                     >
                         {{ lecturerDisplayName }}
                     </router-link>
-                    <div class="mt-3">
-                        <read-more more-str="Show more" :text="course.courseDescription" less-str="Show less" :max-chars="180"></read-more>
-                    </div>
 
-                    <div class="w-full mt-6">
-                        <button v-if="allowEdit" id="editCourse" class="w-full py-2 btn btn-gray-primary" @click="editCourse()">
-                            Edit
-                        </button>
+                    <div class="w-full sm:flex sm:items-center">
+                        <div class="mt-3 sm:w-5/6 sm:mr-4">
+                            <read-more
+                                more-str="Show more"
+                                :text="course.courseDescription"
+                                less-str="Show less"
+                                :max-chars="180"
+                            ></read-more>
+                        </div>
+
+                        <div class="w-full sm:w-1/6 mt-6 sm:mt-0">
+                            <button v-if="allowEdit" id="editCourse" class="w-full py-2 btn btn-gray-primary" @click="editCourse()">
+                                Edit
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
