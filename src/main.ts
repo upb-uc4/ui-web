74cb85e6acf533d122efc90c2990bd453f018f2e
Tobias Wiese <tobiaswiese@online.de>
2020-10-19 11:57:54 +0200

    add global toast configuration

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import Router from "./use/router";
 import { store } from "./use/store/store";
 import "./assets/styles/index.css";
-import Toast from "vue-toastification";
+import Toast, { POSITION } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 const app = createApp(App);
 app.use(Router);
 app.use(store);
-app.use(Toast);
+app.use(Toast, {
+    maxToasts: 5,
+    position: POSITION.BOTTOM_RIGHT,
+    timeout: 3000,
+});
 app.mount("#app");
