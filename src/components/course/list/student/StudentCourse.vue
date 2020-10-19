f7d36cc1cfb6146bedc6a3c3a3de866223c9c375
theovier <Theo@Harkenbusch.com>
2020-10-19 17:31:12 +0200

    adjust student courses to look like the other courses

--- src/components/course/list/student/StudentCourse.vue
+++ src/components/course/list/student/StudentCourse.vue
@@ -1,21 +1,29 @@
 <template>
     <div :id="'course_' + course.courseId" class="flex shadow-xl">
-        <div class="flex flex-col w-full px-8 py-4 bg-white rounded-lg">
-            <div class="flex items-baseline">
-                <span class="inline-block px-2 text-xs font-semibold tracking-wide text-teal-800 uppercase bg-teal-200 rounded-full">{{
-                    course.courseType
-                }}</span>
-                <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">{{ course.courseLanguage }}</div>
+        <div class="flex flex-col w-full p-4 sm:px-8 bg-white rounded-lg">
+            <div class="flex items-center justify-between sm:justify-start">
+                <span class="inline-block px-2 text-xs font-semibold tracking-wide text-teal-800 uppercase bg-teal-200 rounded-full">
+                    {{ course.courseType }}
+                </span>
                 <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">{{ course.ects }} ECTS</div>
+                <div class="hidden sm:flex ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+                    {{ course.courseLanguage }}
+                </div>
+                <!-- todo replace with flag component -->
+                <img
+                    src="https://raw.githubusercontent.com/lipis/flag-icon-css/bb5b59c381b04c651f12bbd7d21c3486da157c88/flags/4x3/gb.svg"
+                    class="sm:hidden inline rounded ml-4 w-6 h-4 opacity-90"
+                />
                 <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-                    <i class="inline text-lg fas fa-users"></i>
+                    <i class="inline text-lg fas fa-users" />
                     {{ course.currentParticipants }} / {{ course.maxParticipants }}
                 </div>
             </div>
 
-            <div class="flex">
-                <div class="flex flex-col items-start w-2/3 lg:w-5/6">
-                    <div class="mt-2 text-2xl font-semibold leading-tight text-gray-900 truncate">{{ course.courseName }}</div>
+            <div class="flex mb-4">
+                <div class="flex flex-col items-start w-full">
+                    <div class="mt-2 text-2xl font-semibold leading-tight text-gray-900">{{ course.courseName }}</div>
+
                     <router-link
                         id="showLecturer"
                         :to="{ name: 'profile.public', params: { username: course.lecturerId } }"
@@ -23,30 +31,21 @@
                     >
                         {{ lecturerDisplayName }}
                     </router-link>
-                    <div class="mt-3">
-                        <read-more more-str="Show more" :text="course.courseDescription" less-str="Show less" :max-chars="180"></read-more>
-                    </div>
-                </div>
-                <div class="w-1/3 mb-4 ml-12 lg:w-1/6 lg:ml-8">
-                    <div class="flex items-center justify-center h-full mt-6">
-                        <!-- v-if directive just for design purposes here -> replace with state of myCourses-->
-                        <button
-                            v-if="course.id === 123456789"
-                            id="leaveCourse"
-                            class="px-8 py-2 font-semibold text-red-600 bg-white border-2 border-gray-300 rounded-lg shadow-md focus:outline-none"
-                        >
-                            Leave
-                        </button>
-                        <button
-                            v-else-if="course.currentParticipants < course.maxParticipants"
-                            id="joinCourse"
-                            class="w-48 py-2 btn btn-blue-primary"
-                        >
-                            Join
-                        </button>
-                        <p v-else class="flex px-8 py-2 font-semibold text-center text-red-600 text-opacity-50 rounded-lg">
-                            Course is full
-                        </p>
+
+                    <div class="w-full sm:flex sm:items-center">
+                        <div class="mt-3 sm:w-5/6 sm:mr-4">
+                            <read-more
+                                more-str="Show more"
+                                :text="course.courseDescription"
+                                less-str="Show less"
+                                :max-chars="180"
+                            ></read-more>
+                        </div>
+
+                        <div class="w-full sm:w-1/6 mt-6 sm:mt-0">
+                            <button id="joinCourse" class="w-full py-2 btn btn-blue-primary" :disabled="isCourseFull">Join</button>
+                            <p v-if="isCourseFull" class="font-semibold text-red-700">This course is already full.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -57,9 +56,6 @@
 <script lang="ts">
     import ReadMore from "@/components/common/ReadMore.vue";
     import Course from "@/api/api_models/course_management/Course";
-    import Router from "@/use/router/";
-    import UserManagement from "@/api/UserManagement";
-    import ProfileResponseHandler from "@/use/helpers/ProfileResponseHandler";
     import Lecturer from "@/api/api_models/user_management/Lecturer";
 
     export default {
@@ -80,9 +76,11 @@
         setup(props: any) {
             //todo this might not be a lecturer in the future
             const lecturerDisplayName = props.lecturer.firstName + " " + props.lecturer.lastName;
+            const isCourseFull = props.course.currentParticipants >= props.course.maxParticipants;
 
             return {
                 lecturerDisplayName,
+                isCourseFull,
             };
         },
     };
