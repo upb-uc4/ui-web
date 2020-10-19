afb43423b2762bcd3b5863aacf8ae0e995e130be
Bastian Haverkamp <bastihav@mail.upb.de>
2020-10-19 09:43:44 +0200

    add thumbnail api

--- tests/unit/api/userManagement.spec.ts
+++ tests/unit/api/userManagement.spec.ts
@@ -20,6 +20,7 @@ const lecturerAuth = JSON.parse(readFileSync("tests/fixtures/logins/lecturer.jso
     password: string;
 };
 const picture: File = new File([readFileSync("src/assets/blank_profile_picture.png")], "image.png", { type: "image/png" });
+let dummySize = 0;
 
 jest.setTimeout(30000);
 
@@ -122,7 +123,8 @@ test("Update user", async () => {
 
 test("get dummy profile picture", async () => {
     const pic = (await userManagement.getProfilePicture(student.username)).returnValue;
-    expect(pic.size).not.toEqual(picture.size);
+    dummySize = pic.size;
+    expect(dummySize).not.toEqual(picture.size);
 });
 
 test("upload profile picture", async () => {
@@ -132,7 +134,10 @@ test("upload profile picture", async () => {
 
 test("get profile picture", async () => {
     const pic = (await userManagement.getProfilePicture(student.username)).returnValue;
-    expect(pic.size).toEqual(picture.size);
+    expect(pic.size).toBeLessThanOrEqual(picture.size);
+    expect(pic.size).not.toEqual(dummySize);
+    const thumb = (await userManagement.getThumbnail(student.username)).returnValue;
+    expect(thumb.size).toBeGreaterThan(0);
 });
 
 test("delete profile picture", async () => {
@@ -142,7 +147,7 @@ test("delete profile picture", async () => {
 
 test("get dummy profile picture", async () => {
     const pic = (await userManagement.getProfilePicture(student.username)).returnValue;
-    expect(pic.size).not.toEqual(picture.size);
+    expect(pic.size).toEqual(dummySize);
 });
 
 test("Delete user", async () => {
