dcbc7d207e8f3f8f7b84b2047b38d38b0cd74e96
theovier <Theo@Harkenbusch.com>
2020-10-19 18:50:57 +0200

    align everything but the name to the right-hand side

--- src/components/account/list/UserRow.vue
+++ src/components/account/list/UserRow.vue
@@ -6,10 +6,10 @@
         @click="editAccount(user.username)"
     >
         <div class="flex items-center">
-            <div class="w-full flex items-center">
+            <div class="w-full flex justify-between">
                 <div class="sm:ml-1">
-                    <div class="text leading-5 font-medium text-blue-900 mb-1 w-48 truncate">{{ user.firstName }} {{ user.lastName }}</div>
-                    <div class="hidden sm:flex text leading-5 text-gray-500 w-48 truncate">@{{ user.username }}</div>
+                    <div class="text leading-5 font-medium text-blue-900 mb-1 truncate">{{ user.firstName }} {{ user.lastName }}</div>
+                    <div class="hidden sm:flex text leading-5 text-gray-500 truncate">@{{ user.username }}</div>
 
                     <span
                         class="sm:hidden inline-block text-xs px-2 rounded-lg font-semibold leading-5 tracking-wide mb-1 w-16 text-center"
@@ -22,29 +22,32 @@
                         {{ user.role }}
                     </span>
                 </div>
-                <div class="mx-8 hidden sm:flex w-24">
-                    <span
-                        class="inline-block text-xs px-2 rounded-lg font-semibold leading-5 tracking-wide mb-1 w-16 text-center"
-                        :class="{
-                            'bg-blue-200 text-blue-800': isStudent,
-                            'bg-red-200 text-red-800': isAdmin,
-                            'bg-green-200 text-green-800': isLecturer,
-                        }"
-                    >
-                        {{ user.role }}
-                    </span>
-                </div>
 
-                <div class="hidden sm:flex flex-col items-baseline" :class="[isStudent ? 'flex' : 'hidden']">
-                    <div class="leading-5 text-blue-900 ml-1 mb-1">{{ student.matriculationId }}</div>
-                    <div class="hidden sm:flex items-center leading-5 text-gray-500">
-                        <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em">
-                            <i class="fas fa-circle text-green-500 fa-stack-2x"></i>
-                            <i class="fas fa-check fa-stack-1x fa-inverse"></i>
+                <div class="flex items-center">
+                    <div class="mx-8 hidden sm:flex w-24">
+                        <span
+                            class="inline-block text-xs px-2 rounded-lg font-semibold leading-5 tracking-wide mb-1 w-16 text-center"
+                            :class="{
+                                'bg-blue-200 text-blue-800': isStudent,
+                                'bg-red-200 text-red-800': isAdmin,
+                                'bg-green-200 text-green-800': isLecturer,
+                            }"
+                        >
+                            {{ user.role }}
                         </span>
-                        <div class="hidden sm:block">
-                            <!-- TODO when new API is active -->
-                            Immatriculated
+                    </div>
+
+                    <div class="flex-col hidden items-baseline" :class="[isStudent ? 'sm:flex' : 'sm:invisible']">
+                        <div class="leading-5 text-blue-900 ml-1 mb-1">{{ student.matriculationId }}</div>
+                        <div class="hidden sm:flex items-center leading-5 text-gray-500">
+                            <span class="mr-2 fa-stack text-xs" style="font-size: 0.63em">
+                                <i class="fas fa-circle text-green-500 fa-stack-2x"></i>
+                                <i class="fas fa-check fa-stack-1x fa-inverse"></i>
+                            </span>
+                            <div class="hidden sm:block">
+                                <!-- TODO when new API is active -->
+                                Immatriculated
+                            </div>
                         </div>
                     </div>
                 </div>
