effc4e7f901c086a8adf34899848356684d9c4dd
Tobias Wiese <tobiaswiese@online.de>
2020-10-27 21:39:28 +0100

    Revert "add prettier plugin that automatically sorts input"
    This reverts commit 271d502001c446ca3bcb08d1fd79a63d7b5c0b42.

--- src/use/helpers/AccountValidationResponseHandler.ts
+++ src/use/helpers/AccountValidationResponseHandler.ts
@@ -1,6 +1,5 @@
 import Error from "@/api/api_models/errors/Error";
 import APIResponse from "@/api/helpers/models/APIResponse";
-
 import ValidationResponseHandler from "./ValidationResponseHandler";
 
 export default class AccountValidationResponseHandler extends ValidationResponseHandler {
