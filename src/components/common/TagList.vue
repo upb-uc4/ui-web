af45e12e881b7d3461b4723c32a60786665e84f8
theovier <Theo@Harkenbusch.com>
2020-11-04 18:50:27 +0100

    Add list component for tags
    Co-authored-by: Paul Kramer <pkramer1@mail.uni-paderborn.de>

---
+++ src/components/common/TagList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex">
+        <div v-for="(element, index) in elements" :key="index">
+            <tag :title="element" @on-remove="onRemove(index)" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Tag from "@/components/common/Tag.vue";
+
+    export default {
+        name: "TagList",
+        components: {
+            Tag,
+        },
+        props: {
+            elements: Array,
+            default: () => [],
+        },
+        emits: ["on-remove"],
+        setup(props: any, { emit }: any) {
+            function onRemove(index: Number) {
+                emit("on-remove", index);
+            }
+            return {
+                onRemove,
+            };
+        },
+    };
+</script>
