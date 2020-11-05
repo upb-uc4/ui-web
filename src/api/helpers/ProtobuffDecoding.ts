40e3236ead4ec4a93c5ef4992b13956a16e8855b
Bastian Haverkamp <bastihav@mail.upb.de>
2020-11-05 09:28:56 +0100

    add promise rejection if decoding fails

--- src/api/helpers/ProtobuffDecoding.ts
+++ src/api/helpers/ProtobuffDecoding.ts
@@ -8,52 +8,58 @@ export async function decodeProposal(base64Proposal: string, protoURL?: string):
     const buf = base64ToArrayBuffer(base64Proposal);
 
     return load(protoURL).then((root) => {
-        const proposalMessage = root.lookupType("protos.Proposal");
-        const headerMessage = root.lookupType("common.Header");
-        const channelHeaderType = root.lookupType("common.ChannelHeader");
-        const signatureHeaderType = root.lookupType("common.SignatureHeader");
-        const chaincodeHeaderExtensionType = root.lookupType("ChaincodeHeaderExtension");
-        const chaincodeProposalPayload = root.lookupType("protos.ChaincodeProposalPayload");
-        const chaincodeInvocationSpec = root.lookupType("protos.ChaincodeInvocationSpec");
-        const serializedIdentityType = root.lookupType("msp.SerializedIdentity");
+        try {
+            const proposalMessage = root.lookupType("protos.Proposal");
+            const headerMessage = root.lookupType("common.Header");
+            const channelHeaderType = root.lookupType("common.ChannelHeader");
+            const signatureHeaderType = root.lookupType("common.SignatureHeader");
+            const chaincodeHeaderExtensionType = root.lookupType("ChaincodeHeaderExtension");
+            const chaincodeProposalPayload = root.lookupType("protos.ChaincodeProposalPayload");
+            const chaincodeInvocationSpec = root.lookupType("protos.ChaincodeInvocationSpec");
+            const serializedIdentityType = root.lookupType("msp.SerializedIdentity");
 
-        const message = proposalMessage.decode(new Uint8Array(buf));
-        const mHeader = headerMessage.decode((message as any)["header"]);
-        const cHeader = channelHeaderType.decode((mHeader as any)["channelHeader"]);
-        const sHeader = signatureHeaderType.decode((mHeader as any)["signatureHeader"]);
+            const message = proposalMessage.decode(new Uint8Array(buf));
+            const mHeader = headerMessage.decode((message as any)["header"]);
+            const cHeader = channelHeaderType.decode((mHeader as any)["channelHeader"]);
+            const sHeader = signatureHeaderType.decode((mHeader as any)["signatureHeader"]);
 
-        const channelHeader = cHeader.toJSON() as ChannelHeader;
-        channelHeader.extension = chaincodeHeaderExtensionType.decode((cHeader as any)["extension"]).toJSON() as ChaincodeHeaderExtension;
+            const channelHeader = cHeader.toJSON() as ChannelHeader;
+            channelHeader.extension = chaincodeHeaderExtensionType
+                .decode((cHeader as any)["extension"])
+                .toJSON() as ChaincodeHeaderExtension;
 
-        const serializedIdentity = serializedIdentityType.decode((sHeader as any)["creator"]);
+            const serializedIdentity = serializedIdentityType.decode((sHeader as any)["creator"]);
 
-        const signatureHeader: SignatureHeader = {
-            nonce: (sHeader as any)["nonce"],
-            creator: serializedIdentity.toJSON() as { mspid: string; idBytes: string },
-        };
-        signatureHeader.creator.idBytes = Utf8ArrayToStr((serializedIdentity as any)["idBytes"]);
+            const signatureHeader: SignatureHeader = {
+                nonce: (sHeader as any)["nonce"],
+                creator: serializedIdentity.toJSON() as { mspid: string; idBytes: string },
+            };
+            signatureHeader.creator.idBytes = Utf8ArrayToStr((serializedIdentity as any)["idBytes"]);
 
-        const header = {
-            signatureHeader,
-            channelHeader,
-        };
+            const header = {
+                signatureHeader,
+                channelHeader,
+            };
 
-        const proposalPayload = chaincodeProposalPayload.decode((message as any)["payload"]);
-        let chainInvocSpec = chaincodeInvocationSpec.decode((proposalPayload as any)["input"]);
+            const proposalPayload = chaincodeProposalPayload.decode((message as any)["payload"]);
+            let chainInvocSpec = chaincodeInvocationSpec.decode((proposalPayload as any)["input"]);
 
-        const payload: ProposalPayload = {
-            transientMap: (proposalPayload as any)["TransientMap"],
-            input: (chainInvocSpec.toJSON() as any).chaincodeSpec,
-        };
+            const payload: ProposalPayload = {
+                transientMap: (proposalPayload as any)["TransientMap"],
+                input: (chainInvocSpec.toJSON() as any).chaincodeSpec,
+            };
 
-        payload.input.input.args = [];
-        let args = (chainInvocSpec as any)["chaincodeSpec"]["input"]["args"] as Uint8Array[];
-        args.forEach((e) => payload.input.input.args.push(Utf8ArrayToStr(e)));
+            payload.input.input.args = [];
+            let args = (chainInvocSpec as any)["chaincodeSpec"]["input"]["args"] as Uint8Array[];
+            args.forEach((e) => payload.input.input.args.push(Utf8ArrayToStr(e)));
 
-        return {
-            header,
-            payload,
-        };
+            return {
+                header,
+                payload,
+            };
+        } catch (error) {
+            Promise.reject("Proposal decoding failed with error: " + error);
+        }
     });
 
     // http://www.onicos.com/staff/iz/amuse/javascript/expert/utf.txt
