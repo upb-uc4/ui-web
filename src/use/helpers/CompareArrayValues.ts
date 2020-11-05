33af2b02793658f7357bcb0896338009982b154a
Tobias Wiese <tobiaswiese@online.de>
2020-11-05 12:53:36 +0100

    remove console log

--- src/use/helpers/CompareArrayValues.ts
+++ src/use/helpers/CompareArrayValues.ts
@@ -1,5 +1,4 @@
 export default function compareArrays(one: any[], two: any[]): boolean {
-    console.log(one, two);
     if (one.length != two.length) return false;
     for (let i = 0; i < one.length - 1; i++) {
         if (!one.includes(two[i]) || !two.includes(one[i])) return false;
