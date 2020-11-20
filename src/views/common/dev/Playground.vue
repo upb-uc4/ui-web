977e879fc1fc647bb1cb9e56461603f39f7946ce
Paul Kramer <pkramer1503@gmail.com>
2020-11-20 17:23:10 +0100

    Add some button classes and examples to Playground.vue, fix temp tmp, typos.

--- src/views/common/dev/Playground.vue
+++ src/views/common/dev/Playground.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-100 p-10 mt-10 rounded-lg w-full">
-        <div>
+    <div class="bg-gray-100 p-10 mt-10 rounded-lg w-full flex flex-row">
+        <div class="mr-16">
             <div class="">
                 <label class="input-label-tmp">default</label>
                 <input type="text" autofocus class="w-64 input-text-tmp" value="empty" />
@@ -31,6 +31,96 @@
                 <label class="input-label-tmp">warning</label>
                 <input type="text" class="w-64 input-text-warning-tmp" value="empty" />
                 <label class="input-label-warning-temp">This is a warning.</label>
+            </div>
+        </div>
+        <div class="mr-16">
+            <div class="">
+                <label class="input-label-tmp">primary normal</label>
+                <button class="w-64 btn-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">primary add</label>
+                <button class="w-64 btn-add-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">primary remove</label>
+                <button class="w-64 btn-remove-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary normal</label>
+                <button class="w-64 btn-secondary-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary add</label>
+                <button class="w-64 btn-secondary-add-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary remove</label>
+                <button class="w-64 btn-secondary-remove-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary normal</label>
+                <button class="w-64 btn-tertiary-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary add</label>
+                <button class="w-64 btn-tertiary-add-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary remove</label>
+                <button class="w-64 btn-tertiary-remove-tmp">Click Me</button>
+            </div>
+        </div>
+        <div>
+            <div class="">
+                <label class="input-label-tmp">primary normal</label>
+                <button disabled class="w-64 btn-tmp">Click Me</button>
+            </div>
+            <div class="mt-6">
+                <label class="input-label-tmp">primary add</label>
+                <button disabled class="w-64 btn-add-tmp">Click Me</button>
+            </div>
+            <div class="mt-6">
+                <label class="input-label-tmp">primary remove</label>
+                <button disabled class="w-64 btn-remove-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary normal</label>
+                <button disabled class="w-64 btn-secondary-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary add</label>
+                <button disabled class="w-64 btn-secondary-add-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">secondary remove</label>
+                <button disabled class="w-64 btn-secondary-remove-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary normal</label>
+                <button disabled class="w-64 btn-tertiary-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary add</label>
+                <button disabled class="w-64 btn-tertiary-add-tmp">Click Me</button>
+            </div>
+
+            <div class="mt-6">
+                <label class="input-label-tmp">tertiary remove</label>
+                <button disabled class="w-64 btn-tertiary-remove-tmp">Click Me</button>
             </div>
         </div>
     </div>
