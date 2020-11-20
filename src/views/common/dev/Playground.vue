d05ae2389f72ee4b299002d4f17199828f677581
theovier <Theo@Harkenbusch.com>
2020-11-20 18:25:53 +0100

    change playground layout and disable hover effect on disabled primary buttons

--- src/views/common/dev/Playground.vue
+++ src/views/common/dev/Playground.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="bg-gray-100 p-10 mt-10 rounded-lg w-full flex flex-row">
-        <div class="mr-16">
+    <div class="bg-gray-100 p-10 mt-10 rounded-lg w-full flex justify-between">
+        <div class="">
+            <h1 class="font-medium text-2xl text-gray-600 mb-2">Inputs</h1>
             <div class="">
                 <label class="input-label-tmp">default</label>
                 <input type="text" autofocus class="w-64 input-text-tmp" value="empty" />
@@ -33,95 +34,124 @@
                 <label class="input-label-warning-tmp">This is a warning.</label>
             </div>
         </div>
-        <div class="mr-16">
-            <div class="">
-                <label class="input-label-tmp">primary normal</label>
-                <button class="w-64 btn-tmp">Click Me</button>
+
+        <div class="">
+            <div>
+                <h1 class="font-medium text-2xl text-gray-600 mb-2">Primary Buttons</h1>
+                <div class="flex">
+                    <div class="mr-8">
+                        <div class="">
+                            <label class="input-label-tmp">default</label>
+                            <button class="w-64 btn-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add</label>
+                            <button class="w-64 btn-add-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove</label>
+                            <button class="w-64 btn-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="">
+                            <label class="input-label-tmp">default disabled</label>
+                            <button disabled class="w-64 btn-tmp">Click Me</button>
+                        </div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add disabled</label>
+                            <button disabled class="w-64 btn-add-tmp">Click Me</button>
+                        </div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove disabled</label>
+                            <button disabled class="w-64 btn-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">primary add</label>
-                <button class="w-64 btn-add-tmp">Click Me</button>
+            <div class="mt-10">
+                <h1 class="font-medium text-2xl text-gray-600 mb-2">Secondary Buttons</h1>
+                <div class="flex">
+                    <div class="mr-8">
+                        <div class="mt-6">
+                            <label class="input-label-tmp">default</label>
+                            <button class="w-64 btn-secondary-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add</label>
+                            <button class="w-64 btn-secondary-add-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove</label>
+                            <button class="w-64 btn-secondary-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">normal disabled</label>
+                            <button disabled class="w-64 btn-secondary-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add disabled</label>
+                            <button disabled class="w-64 btn-secondary-add-tmp">Click Me</button>
+                        </div>
+
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove disabled</label>
+                            <button disabled class="w-64 btn-secondary-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">primary remove</label>
-                <button class="w-64 btn-remove-tmp">Click Me</button>
-            </div>
+            <div class="mt-10">
+                <h1 class="font-medium text-2xl text-gray-600 mb-2">Tertiary Buttons</h1>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary normal</label>
-                <button class="w-64 btn-secondary-tmp">Click Me</button>
-            </div>
+                <div class="flex">
+                    <div class="">
+                        <div class="mt-6">
+                            <label class="input-label-tmp">default</label>
+                            <button class="w-64 btn-tertiary-tmp">Click Me</button>
+                        </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary add</label>
-                <button class="w-64 btn-secondary-add-tmp">Click Me</button>
-            </div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add</label>
+                            <button class="w-64 btn-tertiary-add-tmp">Click Me</button>
+                        </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary remove</label>
-                <button class="w-64 btn-secondary-remove-tmp">Click Me</button>
-            </div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove</label>
+                            <button class="w-64 btn-tertiary-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">normal default</label>
+                            <button disabled class="w-64 btn-tertiary-tmp">Click Me</button>
+                        </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary normal</label>
-                <button class="w-64 btn-tertiary-tmp">Click Me</button>
-            </div>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">add default</label>
+                            <button disabled class="w-64 btn-tertiary-add-tmp">Click Me</button>
+                        </div>
 
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary add</label>
-                <button class="w-64 btn-tertiary-add-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary remove</label>
-                <button class="w-64 btn-tertiary-remove-tmp">Click Me</button>
+                        <div class="mt-6">
+                            <label class="input-label-tmp">remove default</label>
+                            <button disabled class="w-64 btn-tertiary-remove-tmp">Click Me</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
         <div>
-            <div class="">
-                <label class="input-label-tmp">primary normal</label>
-                <button disabled class="w-64 btn-tmp">Click Me</button>
-            </div>
-            <div class="mt-6">
-                <label class="input-label-tmp">primary add</label>
-                <button disabled class="w-64 btn-add-tmp">Click Me</button>
-            </div>
-            <div class="mt-6">
-                <label class="input-label-tmp">primary remove</label>
-                <button disabled class="w-64 btn-remove-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary normal</label>
-                <button disabled class="w-64 btn-secondary-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary add</label>
-                <button disabled class="w-64 btn-secondary-add-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">secondary remove</label>
-                <button disabled class="w-64 btn-secondary-remove-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary normal</label>
-                <button disabled class="w-64 btn-tertiary-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary add</label>
-                <button disabled class="w-64 btn-tertiary-add-tmp">Click Me</button>
-            </div>
-
-            <div class="mt-6">
-                <label class="input-label-tmp">tertiary remove</label>
-                <button disabled class="w-64 btn-tertiary-remove-tmp">Click Me</button>
-            </div>
+            <h1 class="font-medium text-2xl text-gray-600 mb-2">Radio Buttons</h1>
         </div>
     </div>
 </template>
