74f569c759daf98c5da78e95a08401f24cb3daf9
theovier <Theo@Harkenbusch.com>
2020-11-23 15:37:29 +0100

    add listbox example

--- src/views/common/dev/Playground.vue
+++ src/views/common/dev/Playground.vue
@@ -151,7 +151,7 @@
                 </div>
             </div>
 
-            <div class="mt-12 sm:mt-0">
+            <div class="mt-12 sm:mt-0 space-y-10 w-64">
                 <div>
                     <h1 class="font-medium text-2xl text-gray-600 mb-2">Switch (Toggle)</h1>
                     <div class="flex items-center space-x-4">
@@ -171,6 +171,86 @@
                         <i class="fa fa-moon text-3xl" :class="isDarkModeEnabled ? 'text-yellow-500' : 'text-gray-500'" />
                     </div>
                 </div>
+                <div>
+                    <h1 class="font-medium text-2xl text-gray-600 mb-2">Selects</h1>
+                    <div class="flex items-center justify-center">
+                        <div class="w-full max-w-xs mx-auto">
+                            <Listbox v-slot="{ open }" v-model="selectedPerson" as="div" class="space-y-1">
+                                <ListboxLabel class="block text-sm leading-5 font-medium text-gray-700"> Assigned to </ListboxLabel>
+                                <div class="relative">
+                                    <span class="inline-block w-full rounded-md shadow-sm">
+                                        <ListboxButton
+                                            class="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                                        >
+                                            <span class="block truncate">
+                                                {{ selectedPerson }}
+                                            </span>
+                                            <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="none" stroke="currentColor">
+                                                    <path
+                                                        d="M7 7l3-3 3 3m0 6l-3 3-3-3"
+                                                        strokeWidth="1.5"
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    />
+                                                </svg>
+                                            </span>
+                                        </ListboxButton>
+                                    </span>
+
+                                    <transition
+                                        leave-active-class="transition ease-in duration-100"
+                                        leave-from-class="opacity-100"
+                                        leave-to-class="opacity-0"
+                                    >
+                                        <div v-if="open" class="absolute mt-1 w-full rounded-md bg-white shadow-lg">
+                                            <ListboxOptions
+                                                static
+                                                class="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5"
+                                            >
+                                                <ListboxOption
+                                                    v-for="person in people"
+                                                    :key="person"
+                                                    v-slot="{ selected, active }"
+                                                    :value="person"
+                                                >
+                                                    <div
+                                                        :class="`${
+                                                            active ? 'text-white bg-blue-600' : 'text-gray-900'
+                                                        } cursor-default select-none relative py-2 pl-8 pr-4`"
+                                                    >
+                                                        <span :class="`${selected ? 'font-semibold' : 'font-normal'} block truncate`">
+                                                            {{ person }}
+                                                        </span>
+                                                        <span
+                                                            v-if="selected"
+                                                            :class="`${
+                                                                active ? 'text-white' : 'text-blue-600'
+                                                            } absolute inset-y-0 left-0 flex items-center pl-1.5`"
+                                                        >
+                                                            <svg
+                                                                class="h-5 w-5"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 20 20"
+                                                                fill="currentColor"
+                                                            >
+                                                                <path
+                                                                    fill-rule="evenodd"
+                                                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                                                    clip-rule="evenodd"
+                                                                />
+                                                            </svg>
+                                                        </span>
+                                                    </div>
+                                                </ListboxOption>
+                                            </ListboxOptions>
+                                        </div>
+                                    </transition>
+                                </div>
+                            </Listbox>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -178,16 +258,36 @@
 
 <script lang="ts">
     import { ref } from "vue";
-    import { Switch } from "@headlessui/vue";
+    import { Switch, Listbox, ListboxLabel, ListboxButton, ListboxOptions, ListboxOption } from "@headlessui/vue";
 
     export default {
         name: "Playground",
         components: {
             Switch,
+            Listbox,
+            ListboxLabel,
+            ListboxButton,
+            ListboxOptions,
+            ListboxOption,
         },
         setup() {
+            const people = [
+                "Wade Cooper",
+                "Arlene Mccoy",
+                "Devon Webb",
+                "Tom Cook",
+                "Tanya Fox",
+                "Hellen Schmidt",
+                "Caroline Schultz",
+                "Mason Heaney",
+                "Claudie Smitham",
+                "Emil Schaefer",
+            ];
+
+            const selectedPerson = ref(people[0]);
+
             const isDarkModeEnabled = ref(false);
-            return { isDarkModeEnabled };
+            return { isDarkModeEnabled, people, selectedPerson };
         },
     };
 </script>
