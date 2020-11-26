d7ef7cbfb4627bb5282aead369e4cca5f2ac93da
Tobias Wiese <tobiaswiese@online.de>
2020-11-26 15:26:43 +0100

    fix routing

--- src/use/router/index.ts
+++ src/use/router/index.ts
@@ -69,7 +69,7 @@ const router = createRouter({
             },
         },
         {
-            path: "/courses/:courseId",
+            path: "/courses/:courseId/drop",
             name: "student.course.drop",
             component: StudentCourseInfoPage,
             props: { isRegistered: true },
@@ -79,7 +79,7 @@ const router = createRouter({
             },
         },
         {
-            path: "/courses/:courseId",
+            path: "/courses/:courseId/join",
             name: "student.course.join",
             component: StudentCourseInfoPage,
             props: { isRegistered: false },
