cdf908fa66cbc1a7a2f770000f95673a2488a106
theovier <Theo@Harkenbusch.com>
2020-12-07 16:57:44 +0100

    extract modal headline and description style

--- src/components/modals/DeleteProfilePictureModal.vue
+++ src/components/modals/DeleteProfilePictureModal.vue
@@ -1,17 +1,11 @@
 <template>
     <modal ref="baseModal" :action="action" @cancel="close(action.CANCEL)">
         <template #header>
-            <p class="text-gray-800 text-lg font-medium mb-2">Delete Profile Picture</p>
+            <p class="modal-headline mb-2">Delete Profile Picture</p>
         </template>
-        <div class="text-gray-500 text-sm">Are you sure you want to delete the profile picture? You won't be able to restore it.</div>
+        <div class="modal-description">Are you sure you want to delete the profile picture? You won't be able to restore it.</div>
         <template #footer>
-            <button
-                id="deleteProfilePictureModalCancel"
-                class="mr-10 text-gray-600 hover:text-gray-700 btn-tertiary-tmp"
-                @click="close(action.CANCEL)"
-            >
-                Cancel
-            </button>
+            <button id="deleteProfilePictureModalCancel" class="mr-10 btn-tertiary-modal" @click="close(action.CANCEL)">Cancel</button>
             <button id="deleteProfilePictureModalDelete" class="w-24 py-2 px-2 btn btn-remove-tmp" @click="close(action.DELETE)">
                 Delete
             </button>
