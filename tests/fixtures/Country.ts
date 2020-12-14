3f01f95807b0f12e8e72736a6c4d73b7e0a62c85
Tobias Wiese <tobiaswiese@online.de>
2020-12-14 13:17:53 +0100

    re-add country enums for e2e tests

---
+++ tests/fixtures/Country.ts
@@ -0,0 +1,14 @@
+export enum Country {
+    NONE = "",
+    GERMANY = "Germany",
+    USA = "United States",
+    ITALY = "Italy",
+    FRANCE = "France",
+    UNITED_KINGDOM = "United Kingdom",
+    BELGIUM = "Belgium",
+    NETHERLANDS = "Netherlands",
+    SPAIN = "Spain",
+    AUSTRIA = "Austria",
+    SWITZERLAND = "Switzerland",
+    POLAND = "Poland",
+}
