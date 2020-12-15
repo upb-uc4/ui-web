1c60aa36b414455a5f5e9714bc82584e11a75d56
Bastian Haverkamp <bastihav@mail.upb.de>
2020-12-15 16:37:50 +0100

    add first draft of operation api
    subject to change as there is no actual rest api definition yet

---
+++ src/api/OperationsManagement.ts
@@ -0,0 +1,143 @@
+import { AxiosError, AxiosResponse } from "axios";
+import APIError from "./api_models/errors/APIError";
+import Operation from "./api_models/operations_management/Operation";
+import handleAuthenticationError from "./AuthenticationHelper";
+import CommonHyperledger from "./CommonHyperledger";
+import APIResponse from "./helpers/models/APIResponse";
+
+export default class OperationsManagement extends CommonHyperledger {
+    protected static endpoint = "/operations-management";
+
+    constructor() {
+        super(OperationsManagement.endpoint);
+    }
+
+    static async getVersion(): Promise<string> {
+        return super.getVersion();
+    }
+
+    async getOperations(username?: string, onlyOwn?: boolean, onlyWorkable?: boolean): Promise<APIResponse<Operation[]>> {
+        const requestParameter = { params: {} as any };
+        if (username) requestParameter.params.username = username;
+        if (onlyOwn !== undefined) requestParameter.params.onlyOwn = onlyOwn;
+        if (onlyWorkable !== undefined) requestParameter.params.onlyWorkable = onlyWorkable;
+
+        return await this._axios
+            .get(`/operations`, requestParameter)
+            .then((response: AxiosResponse) => {
+                return {
+                    statusCode: response.status,
+                    returnValue: response.data,
+                    networkError: false,
+                    error: {} as APIError,
+                };
+            })
+            .catch(async (error: AxiosError) => {
+                if (error.response) {
+                    if (
+                        await handleAuthenticationError({
+                            statusCode: error.response.status,
+                            error: error.response.data as APIError,
+                            returnValue: [] as Operation[],
+                            networkError: false,
+                        })
+                    ) {
+                        return await this.getOperations(username);
+                    }
+                    return {
+                        statusCode: error.response.status,
+                        error: error.response.data as APIError,
+                        returnValue: [] as Operation[],
+                        networkError: false,
+                    };
+                } else {
+                    return {
+                        statusCode: 0,
+                        error: {} as APIError,
+                        returnValue: [] as Operation[],
+                        networkError: true,
+                    };
+                }
+            });
+    }
+
+    async getOperation(operationId: string): Promise<APIResponse<Operation>> {
+        return await this._axios
+            .get(`/operations/${operationId}`)
+            .then((response: AxiosResponse) => {
+                return {
+                    statusCode: response.status,
+                    returnValue: response.data,
+                    networkError: false,
+                    error: {} as APIError,
+                };
+            })
+            .catch(async (error: AxiosError) => {
+                if (error.response) {
+                    if (
+                        await handleAuthenticationError({
+                            statusCode: error.response.status,
+                            error: error.response.data as APIError,
+                            returnValue: {} as Operation,
+                            networkError: false,
+                        })
+                    ) {
+                        return await this.getOperation(operationId);
+                    }
+                    return {
+                        statusCode: error.response.status,
+                        error: error.response.data as APIError,
+                        returnValue: {} as Operation,
+                        networkError: false,
+                    };
+                } else {
+                    return {
+                        statusCode: 0,
+                        error: {} as APIError,
+                        returnValue: {} as Operation,
+                        networkError: true,
+                    };
+                }
+            });
+    }
+
+    async archiveOperation(operationId: string): Promise<APIResponse<boolean>> {
+        return await this._axios
+            .delete(`/operations/${operationId}`)
+            .then((response: AxiosResponse) => {
+                return {
+                    statusCode: response.status,
+                    returnValue: true,
+                    networkError: false,
+                    error: {} as APIError,
+                };
+            })
+            .catch(async (error: AxiosError) => {
+                if (error.response) {
+                    if (
+                        await handleAuthenticationError({
+                            statusCode: error.response.status,
+                            error: error.response.data as APIError,
+                            returnValue: false,
+                            networkError: false,
+                        })
+                    ) {
+                        return await this.archiveOperation(operationId);
+                    }
+                    return {
+                        statusCode: error.response.status,
+                        error: error.response.data as APIError,
+                        returnValue: false,
+                        networkError: false,
+                    };
+                } else {
+                    return {
+                        statusCode: 0,
+                        error: {} as APIError,
+                        returnValue: false,
+                        networkError: true,
+                    };
+                }
+            });
+    }
+}
---
+++ src/api/api_models/operations_management/Operation.ts
@@ -0,0 +1,20 @@
+import { UC4Identifier } from "@/api/helpers/UC4Identifier";
+import { OperationStatus } from "./OperationState";
+
+export default interface Operation {
+    operationId: string;
+    transactionInfo: {
+        contractName: UC4Identifier;
+        transactionName: UC4Identifier;
+        parameters: string[];
+    };
+    state: OperationStatus;
+    reason: string;
+    existingApprovals: ApprovalList;
+    missingApprovals: ApprovalList;
+}
+
+export interface ApprovalList {
+    users: string[];
+    groups: string[];
+}
