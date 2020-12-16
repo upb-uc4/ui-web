9295df411fbc350f606675a2311ea8724aa00b8f
Tobias Wiese <tobiaswiese@online.de>
2020-12-16 15:17:46 +0100

    add initiator to operation interface

--- src/api/api_models/operations_management/Operation.ts
+++ src/api/api_models/operations_management/Operation.ts
@@ -3,6 +3,7 @@ import { OperationStatus } from "./OperationState";
 
 export default interface Operation {
     operationId: string;
+    initiator: string;
     transactionInfo: {
         contractName: UC4Identifier;
         transactionName: UC4Identifier;
