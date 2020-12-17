533416cd441da89606a13ef45094e193fc854af7
theovier <Theo@Harkenbusch.com>
2020-12-17 16:46:24 +0100

    rearrange admin menu

--- src/components/navigation/navbar/desktop/admin/administration/ManageAccountsMenuBody.vue
+++ src/components/navigation/navbar/desktop/admin/administration/ManageAccountsMenuBody.vue
@@ -1,33 +1,37 @@
 <template>
-    <section class="flex flex-col lg:flex-row px-8 py-6 border-b -mx-4">
+    <section class="px-8 py-6 border-b -mx-4">
         <ul class="w-full px-2 space-y-4">
-            <li>
-                <menu-item
-                    id="nav_desktop_admin_menu_manage_accounts_all"
-                    title="All Users"
-                    description="Show a list of all users"
-                    icon-class="fas fa-users"
-                    target-route-name="accountlist"
-                />
-            </li>
-            <li>
-                <menu-item
-                    id="nav_desktop_admin_menu_manage_accounts_create"
-                    title="New Account"
-                    description="Create a new user"
-                    icon-class="fas fa-user-plus"
-                    target-route-name="accountForm.create"
-                />
-            </li>
-            <li>
-                <menu-item
-                    id="nav_desktop_admin_menu_exreg_create"
-                    title="Create Exam Regulation"
-                    description="Create a new Exam Regulation"
-                    icon-class="fas fa-folder-plus"
-                    target-route-name="examRegForm"
-                />
-            </li>
+            <div class="flex">
+                <li class="w-1/2">
+                    <menu-item
+                        id="nav_desktop_admin_menu_manage_accounts_all"
+                        title="All Users"
+                        description="Show every user"
+                        icon-class="fas fa-users"
+                        target-route-name="accountlist"
+                    />
+                </li>
+                <li class="w-1/2">
+                    <menu-item
+                        id="nav_desktop_admin_menu_manage_accounts_create"
+                        title="New Account"
+                        description="Create a new user"
+                        icon-class="fas fa-user-plus"
+                        target-route-name="accountForm.create"
+                    />
+                </li>
+            </div>
+            <div>
+                <li>
+                    <menu-item
+                        id="nav_desktop_admin_menu_exreg_create"
+                        title="New Exam Regulation"
+                        description="Create a new exam regulation"
+                        icon-class="fas fa-folder-plus"
+                        target-route-name="examRegForm"
+                    />
+                </li>
+            </div>
         </ul>
     </section>
 </template>
