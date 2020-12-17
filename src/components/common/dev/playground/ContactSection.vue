eec39f7a589248d3da397b2e233ec91444c4743b
Paul Kramer <pkramer1503@gmail.com>
2020-12-17 18:46:21 +0100

    Add model to ContactSection.vue.

--- src/components/common/dev/playground/ContactSection.vue
+++ src/components/common/dev/playground/ContactSection.vue
@@ -4,14 +4,14 @@
             <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
                 <div class="lg:w-1/2 w-full">
                     <label class="input-label-tmp">Email</label>
-                    <input type="text" class="w-full input-text-tmp" readonly value="example@cs.uc4.upb.de" />
+                    <input v-model="myEmail" type="text" class="w-full input-text-tmp" />
                 </div>
                 <div class="lg:w-1/2 w-full" />
             </div>
             <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
                 <div class="lg:w-1/2 w-full">
                     <label class="input-label-tmp">Phone</label>
-                    <input type="text" class="w-full input-text-tmp" readonly value="+0900666666" />
+                    <input v-model="myPhoneNumber" type="text" class="w-full input-text-tmp" />
                 </div>
                 <div class="lg:w-1/2 w-full" />
             </div>
@@ -19,17 +19,31 @@
     </BaseSection>
 </template>
 
-<script>
-    import BaseSection from "@/components/common/section/BaseSection";
-    import { ref } from "vue";
+<script lang="ts">
+    import BaseSection from "@/components/common/section/BaseSection.vue";
+    import { useModelWrapper } from "@/use/helpers/ModelWrapper";
 
     export default {
         name: "InputSection",
         components: {
             BaseSection,
         },
-        setup() {
-            return {};
+        props: {
+            phoneNumber: {
+                type: String,
+                required: true,
+            },
+            email: {
+                type: String,
+                required: true,
+            },
+        },
+        emits: ["update:email", "update:phoneNumber"],
+        setup(props: any, { emit }: any) {
+            return {
+                myPhoneNumber: useModelWrapper(props, emit, "phoneNumber"),
+                myEmail: useModelWrapper(props, emit, "email"),
+            };
         },
     };
 </script>
