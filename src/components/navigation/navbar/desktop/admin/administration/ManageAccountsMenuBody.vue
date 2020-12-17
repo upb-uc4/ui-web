d26fae1693de330d49fdc79a444fe5d2ca135fd4
theovier <Theo@Harkenbusch.com>
2020-12-17 16:18:48 +0100

    move exam reg to administration navbar menu

--- src/components/navigation/navbar/desktop/admin/administration/ManageAccountsMenuBody.vue
+++ src/components/navigation/navbar/desktop/admin/administration/ManageAccountsMenuBody.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="flex flex-col lg:flex-row px-8 py-6 border-b -mx-4">
-        <ul class="w-full px-2">
-            <li class="mb-4">
+        <ul class="w-full px-2 space-y-4">
+            <li>
                 <menu-item
                     id="nav_desktop_admin_menu_manage_accounts_all"
                     title="All Users"
-                    description="List of all users"
+                    description="Show a list of all users"
                     icon-class="fas fa-users"
                     target-route-name="accountlist"
                 />
@@ -17,6 +17,15 @@
                     description="Create a new user"
                     icon-class="fas fa-user-plus"
                     target-route-name="accountForm.create"
+                />
+            </li>
+            <li>
+                <menu-item
+                    id="nav_desktop_admin_menu_exreg_create"
+                    title="Create Exam Regulation"
+                    description="Create a new Exam Regulation"
+                    icon-class="fas fa-folder-plus"
+                    target-route-name="examRegForm"
                 />
             </li>
         </ul>
