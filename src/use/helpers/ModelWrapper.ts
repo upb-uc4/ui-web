1971cd5c48014b6b544cb9db52bc25a94fb8766e
Paul Kramer <pkramer1503@gmail.com>
2020-12-17 17:37:03 +0100

    Write model wrapper for object properties

--- src/use/helpers/ModelWrapper.ts
+++ src/use/helpers/ModelWrapper.ts
@@ -1,8 +1,23 @@
 import { computed } from "vue";
+import __ from "lodash";
 
 export function useModelWrapper(props: any, emit: any, name = "modelValue") {
     return computed({
         get: () => props[name],
         set: (value) => emit(`update:${name}`, value),
+    });
+}
+
+export function useObjectModelWrapper(props: any, emit: any, name: string, ...keys: string[]) {
+    return computed({
+        get: () => {
+            const obj = props[name];
+            return __.get(obj, keys);
+        },
+        set: (value) => {
+            const newObj = __.cloneDeep(props[name]);
+            __.set(newObj, keys, value);
+            emit(`update:${name}`, newObj);
+        },
     });
 }
