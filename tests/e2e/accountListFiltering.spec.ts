0d8174f2ecd79bae6982abf4d18153d2b7284366
Tobias Wiese <tobiaswiese@online.de>
2020-12-17 17:23:05 +0100

    fix test

--- tests/e2e/accountListFiltering.spec.ts
+++ tests/e2e/accountListFiltering.spec.ts
@@ -1,23 +1,19 @@
-import Admin from "@/api/api_models/user_management/Admin";
 import { Account } from "@/entities/Account";
 import { Role } from "@/entities/Role";
 import { loginAsDefaultAdmin, logout } from "./helpers/AuthHelper";
 import { navigateToAccountList } from "./helpers/NavigationHelper";
 
 let adminAuth: Account;
-let admin: Admin;
 let studentAuth: Account;
 let lecturerAuth: Account;
 
 describe("Account creation, edition and deletion", function () {
+    const random = Math.floor(Math.random() * 9999);
+
     before(function () {
         cy.clearCookies();
         Cypress.Cookies.defaults({
             preserve: ["refresh", "login"],
-        });
-
-        cy.fixture("admin.json").then((adminFix) => {
-            admin = adminFix;
         });
 
         cy.fixture("logins/admin.json").then((adminLogin) => {
@@ -87,7 +83,9 @@ describe("Account creation, edition and deletion", function () {
         cy.get("div").contains(`@${studentAuth.username}`).should("not.exist");
 
         //working with spaces, too
-        cy.get("input[id='message']").clear().type(`${admin.firstName} ${admin.lastName}`);
+        cy.get("input[id='message']")
+            .clear()
+            .type(`${adminAuth.username.substr(0, 2)} ${adminAuth.username.substr(2, adminAuth.username.length - 1)}`);
         cy.get("div").contains(`@${adminAuth.username}`).should("exist");
         cy.get("div").contains(`@${lecturerAuth.username}`).should("not.exist");
         cy.get("div").contains(`@${studentAuth.username}`).should("not.exist");
