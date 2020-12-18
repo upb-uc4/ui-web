3b6dd06a4c60fa2272996e254971b46c023e0a4e
theovier <Theo@Harkenbusch.com>
2020-12-18 13:13:35 +0100

    rework draft of about page

--- src/views/common/About.vue
+++ src/views/common/About.vue
@@ -2,17 +2,45 @@
     <div v-if="busy">
         <loading-spinner />
     </div>
-    <div v-else class="flex flex-col items-center justify-center w-full mt-20">
-        <img src="../../assets/logo/logo_long_title_right.svg" alt="" />
-        <div class="flex flex-col items-center w-full mt-5">
-            <section class="w-full py-4">
-                <div class="">
-                    <div class="flex flex-col w-full mb-1">
-                        <label class="block mb-2 text-lg font-semibold text-gray-700">What is UC4?</label>
+    <div v-else class="max-w-screen-lg mx-auto w-full space-y-8">
+        <h1 class="text-4xl font-extrabold text-center">About Us</h1>
+        <hr class="my-4" />
+        <div class="lg:flex w-full space-y-8 lg:space-y-0">
+            <div class="lg:w-1/5">
+                <div class="mb-4">
+                    <div class="text-gray-800 text-sm font-medium">University Credits 4.0</div>
+                    <div class="text-gray-500 text-sm">Paderborn, Germany</div>
+                </div>
+                <div class="flex space-x-4">
+                    <div class="flex items-center space-x-1">
+                        <a class="text-gray-400 hover:text-gray-600" target="_blank" rel="noreferrer" href="https://github.com/upb-uc4">
+                            <i class="fab fa-github text-3xl" />
+                        </a>
                     </div>
-                    <div class="flex flex-col w-full justify-center">
-                        <label class="text-gray-700 text-lg leading-relaxed">
-                            UC4 is short for 'University Credits 4.0' - an educational research project at the
+                    <div class="flex items-center space-x-1">
+                        <a
+                            class="text-gray-400 hover:text-gray-600"
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://twitter.com/Uc4_Official"
+                        >
+                            <i class="fab fa-twitter text-3xl" />
+                        </a>
+                    </div>
+
+                    <div class="flex items-center space-x-1">
+                        <a class="text-gray-400 hover:text-gray-600" target="_blank" rel="noreferrer" href="mailto:uc4_official@web.de">
+                            <i class="fas fa-envelope text-3xl" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="lg:w-4/5 space-y-8">
+                <div>
+                    <div class="w-full mb-6 text-justify">
+                        <h2 class="block text-gray-800 text-lg font-medium mb-1">Research Project Group</h2>
+                        <h3 class="block text-gray-600 text-sm leading-relaxed">
+                            University Credits 4.0 is an educational research project at the
                             <a
                                 class="text-blue-700 hover:text-blue-800 hover:underline"
                                 target="_blank"
@@ -32,130 +60,53 @@
                             >
                                 uc4.cs.uni-paderborn.de.
                             </a>
-                        </label>
+                            Starting from April 2020 onwards, continuous support for this project will be provided until Q2 2021.
+                        </h3>
                     </div>
                 </div>
-            </section>
-            <section class="w-full py-4">
                 <div>
-                    <div class="flex flex-col w-full mb-1">
-                        <label class="block text-lg font-semibold text-gray-700">About us</label>
+                    <div class="w-full mb-6 text-justify">
+                        <h2 class="block text-gray-800 text-lg font-medium mb-1">Demo Login</h2>
+                        <h3 class="block text-gray-600 text-sm leading-relaxed">
+                            We encourage you to explore our product on your own. Feel free to use any of the following login combinations.
+                            However, please <span class="font-semibold">do not delete any of these accounts</span>. Each of the provided
+                            accounts has a different access level which in turn unlocks certain features.
+                        </h3>
                     </div>
-                    <div class="flex flex-col w-full">
-                        <label class="text-gray-700 text-lg leading-relaxed">
-                            We are a group of passionate Master's students at the Paderborn University who work hard to make your everyday
-                            life at campus more simple. Starting from April 2020 onwards, we will provide continuous support for this
-                            project until Q2 2021.
-                        </label>
-                    </div>
-                </div>
-            </section>
-            <section class="w-full py-4">
-                <div>
-                    <div class="flex flex-col w-full mb-1">
-                        <label class="block text-lg font-semibold text-gray-700">Get in touch</label>
-                    </div>
-                    <div class="flex flex-col w-full">
-                        <label class="mb-3 text-gray-700 text-lg">
-                            Did we spark your interest in our project? Let's get in touch! You can reach out to us on various channels.
-                        </label>
 
-                        <div class="flex flex-col md:flex-row md:justify-start justify-between">
-                            <label class="mb-3 md:mb-0 md:mr-8 text-lg font-medium text-gray-700">
-                                <a
-                                    class="text-blue-700 hover:text-blue-800 hover:underline"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    href="https://uc4.cs.uni-paderborn.de/"
-                                >
-                                    <i class="mr-1 fas fa-globe" />
-                                    Website
-                                </a>
-                            </label>
-                            <label class="mb-3 md:mb-0 md:mr-8 text-lg font-medium text-gray-700">
-                                <a
-                                    class="text-blue-700 hover:text-blue-800 hover:underline"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    href="https://github.com/upb-uc4"
-                                >
-                                    <i class="mr-1 fab fa-github" />
-                                    GitHub
-                                </a>
-                            </label>
-                            <label class="mb-3 md:mb-0 md:mr-8 text-lg font-medium text-gray-700">
-                                <a
-                                    class="text-blue-700 hover:text-blue-800 hover:underline"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    href="https://twitter.com/Uc4_Official"
-                                >
-                                    <i class="mr-1 fab fa-twitter" />
-                                    Twitter
-                                </a>
-                            </label>
-                            <label class="mb-0 text-lg md:mr-8 font-medium text-gray-700">
-                                <a
-                                    class="text-blue-700 hover:text-blue-800 hover:underline"
-                                    target="_blank"
-                                    rel="noreferrer"
-                                    href="mailto:uc4_official@web.de"
-                                >
-                                    <i class="mr-1 fas fa-envelope"></i>
-                                    Email
-                                </a>
-                            </label>
-                        </div>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                        <credential-card title="Administrator" emoji="👑" username="admin" password="admin" />
+                        <credential-card title="Lecturer" emoji="👨‍🏫" username="lecturer" password="lecturer" />
+                        <credential-card title="Student" emoji="👨‍🎓" username="student" password="student" />
                     </div>
                 </div>
-            </section>
-            <section class="w-full py-4">
-                <div class="flex flex-col justify-center">
-                    <div class="flex flex-col w-full mb-1">
-                        <label class="block text-lg font-semibold text-gray-700">Work in Progress</label>
-                    </div>
-                    <div class="flex flex-col items-start w-full">
-                        <label class="text-gray-700 text-lg">
-                            This application is still work in progress. If you encounter any problems, please do not hesitate to contact us.
-                        </label>
-                        <div class="flex">
-                            <label class="text-gray-700 text-lg mr-12">
-                                For testing purposes you can use the following 'username:password' combinations:
-                            </label>
-                            <div class="flex flex-col">
-                                <label class="text-green-700 text-lg">admin:admin</label>
-                                <label class="text-green-700 text-lg">lecturer:lecturer</label>
-                                <label class="text-green-700 text-lg">student:student</label>
-                            </div>
-                        </div>
-                        <label class="text-gray-700 text-xl flex">
-                            <p class="text-red-700 mr-1 font-semibold">Caution:</p>
-                            Please do
-                            <p class="font-semibold mx-1">NOT</p>
-                            delete any of these accounts!
-                        </label>
-                        <div class="flex justify-center w-full">
-                            <button class="w-56 mt-12 mb-8 btn btn-blue-primary" @click="reportProblem">
-                                <i class="mr-2 fas fa-exclamation-triangle" />
-                                Report a problem
-                            </button>
-                        </div>
-                        <versions @versions="updateVersions" />
+                <div>
+                    <div class="w-full mb-6 text-justify">
+                        <h2 class="block text-gray-800 text-lg font-medium mb-1">Server Status</h2>
                     </div>
                 </div>
-            </section>
+            </div>
+        </div>
+
+        <div class="flex flex-col items-center justify-center w-full mt-20">
+            <div class="flex flex-col items-center w-full mt-5">
+                <versions @versions="updateVersions" />
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import Versions from "@/components/common/dev/Versions.vue";
+    import Versions from "@/components/common/dev/about/Versions.vue";
+    import CredentialCard from "@/components/common/dev/about/CredentialCard.vue";
+
     import { reactive, computed, ref, onBeforeMount } from "vue";
     import axios, { AxiosResponse } from "axios";
     import LoadingSpinner from "@/components/common/loading/Spinner.vue";
 
     export default {
         components: {
+            CredentialCard,
             Versions,
             LoadingSpinner,
         },
