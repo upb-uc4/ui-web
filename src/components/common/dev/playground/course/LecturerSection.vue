8d64d631e980482ef7b9a8fa5e65047bd14f9a03
Paul Kramer <pkramer1503@gmail.com>
2020-12-18 14:59:21 +0100

    Add lecturer section using basesection

---
+++ src/components/common/dev/playground/course/LecturerSection.vue
@@ -0,0 +1,98 @@
+<template>
+    <BaseSection subtitle="Select a lecturer that will hold the course" title="Lecturer">
+        <searchableSelect :label="'Lecturer-ID'" :elements="myLecturers" @update:selected="updateLecturerId" />
+        <div :hidden="Object.keys(currentLecturer).length === 0" class="text-gray-700 text-md font-medium my-3">
+            <label v-if="currentLecturer.username">
+                <i class="text-green-400 fas fa-check mr-2"></i>
+                {{ currentLecturer.firstName }} {{ currentLecturer.lastName }} (
+                <router-link
+                    class="navigation-link cursor-pointer hover:underline"
+                    target="_blank"
+                    :to="{ name: 'profile.public', params: { username: currentLecturer.username } }"
+                >
+                    @{{ currentLecturer.username }}
+                </router-link>
+                )
+            </label>
+            <label v-else>
+                <i class="text-red-400 fas fa-times mr-2"></i>
+                Lecturer-ID not found!
+            </label>
+        </div>
+    </BaseSection>
+</template>
+
+<script lang="ts">
+    import SearchableSelect from "@/components/common/SearchableSelect.vue";
+    import BaseSection from "@/components/common/section/BaseSection.vue";
+    import { computed, onBeforeMount, ref } from "vue";
+    import SearchSelectOption from "@/use/helpers/SearchSelectOption";
+    import Lecturer from "@/api/api_models/user_management/Lecturer";
+    import UserManagement from "@/api/UserManagement";
+    import GenericResponseHandler from "@/use/helpers/GenericResponseHandler";
+    import { Role } from "@/entities/Role";
+
+    export default {
+        name: "Lecturer",
+        components: {
+            BaseSection,
+            SearchableSelect,
+        },
+        props: {
+            lecturerId: {
+                type: String,
+                required: true,
+            },
+        },
+        emits: ["update:lecturerId"],
+        setup(props: any, { emit }: any) {
+            const lecturers = ref([] as Lecturer[]);
+
+            const currentLecturer = ref({} as Lecturer);
+
+            const myLecturers = computed(() =>
+                lecturers.value.map((l: Lecturer) => {
+                    return {
+                        value: l,
+                        display: createOptionString(l),
+                    } as SearchSelectOption;
+                })
+            );
+
+            onBeforeMount(async () => {
+                await getLecturers();
+            });
+
+            async function getLecturers() {
+                const userManagement: UserManagement = new UserManagement();
+                const handler = new GenericResponseHandler("lecturers");
+                const response = await userManagement.getUsers(Role.LECTURER);
+                const result = handler.handleResponse(response);
+                if (result) {
+                    lecturers.value = result as Lecturer[];
+                    if (props.lecturerId != "") {
+                        const initialLecturer = lecturers.value.find((l) => l.username === props.lecturerId);
+                        if (initialLecturer) {
+                            currentLecturer.value = initialLecturer;
+                        }
+                    }
+                }
+            }
+
+            function updateLecturerId(element: any) {
+                currentLecturer.value = element.value as Lecturer;
+                emit("update:lecturerId", currentLecturer.value.username);
+            }
+
+            function createOptionString(l: Lecturer): string {
+                return `${l.firstName} ${l.lastName} (@${l.username})`;
+            }
+
+            return {
+                updateLecturerId,
+                myLecturers,
+                currentLecturer,
+            };
+        },
+    };
+</script>
