44b951f38ab54ff29dda11ce51e061e19b2dd50b
theovier <Theo@Harkenbusch.com>
2020-12-18 23:38:56 +0100

    bugfix that were was a grid system within a grid system

--- src/components/common/dev/about/ServiceStatusCard.vue
+++ src/components/common/dev/about/ServiceStatusCard.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <div class="w-56 rounded-md border-gray-200 border h-32 p-4 mx-auto md:mx-0">
-            <div class="flex justify-between">
-                <div class="font-medium text-lg">{{ title }}</div>
-                <div>
-                    <img v-if="isLoading" src="../../../../assets/loading-spinner-alt.svg" class="w-6 h-6" />
-                    <i v-else-if="isServiceUnreachable" class="text-xl text-red-500 fas fa-times-circle" />
-                    <i v-else class="text-xl text-green-500 fas fa-check-circle" />
-                </div>
+    <div class="w-56 rounded-md border-gray-200 border h-32 p-4 mx-auto md:mx-0">
+        <div class="flex justify-between">
+            <div class="font-medium text-lg">{{ title }}</div>
+            <div>
+                <img v-if="isLoading" src="../../../../assets/loading-spinner-alt.svg" class="w-6 h-6" />
+                <i v-else-if="isServiceUnreachable" class="text-xl text-red-500 fas fa-times-circle" />
+                <i v-else class="text-xl text-green-500 fas fa-check-circle" />
             </div>
-            <div class="text-sm text-gray-700">
-                <div v-if="isLoading">
-                    <div>Checking Version...</div>
-                </div>
-                <div v-else-if="isServiceUnreachable">
-                    <span class="navigation-link-tmp">Version not available</span>
-                    <div>Unreachable</div>
-                </div>
-                <div v-else>
-                    <a :href="changelogURL" class="navigation-link-tmp">Version {{ version }}</a>
-                    <div>Active</div>
-                </div>
+        </div>
+        <div class="text-sm text-gray-700">
+            <div v-if="isLoading">
+                <div>Checking Version...</div>
+            </div>
+            <div v-else-if="isServiceUnreachable">
+                <span class="navigation-link-tmp">Version not available</span>
+                <div>Unreachable</div>
+            </div>
+            <div v-else>
+                <a :href="changelogURL" class="navigation-link-tmp">Version {{ version }}</a>
+                <div>Active</div>
             </div>
         </div>
     </div>
