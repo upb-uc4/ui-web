6da19cd196b0177d92b291430bdb3c5c71a8db67
Paul Kramer <pkramer1503@gmail.com>
2020-12-18 18:14:25 +0100

    Rearrange inputs.

--- src/components/common/dev/playground/course/CourseInfoSection.vue
+++ src/components/common/dev/playground/course/CourseInfoSection.vue
@@ -1,14 +1,13 @@
 <template>
     <BaseSection subtitle="General Information about the Course" title="Basics">
         <div class="space-y-6">
-            <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
-                <div class="lg:w-1/2 w-full">
-                    <label class="input-label-tmp">Course Name</label>
-                    <input v-model="myName" type="text" class="w-full input-text-tmp" />
-                </div>
-                <div class="lg:w-1/2 w-full">
-                    <label class="input-label-tmp">ECTS</label>
-                    <input v-model.number="myEcts" min="0" type="number" class="w-full input-text-tmp" />
+            <div class="space-y-6">
+                <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
+                    <div class="lg:w-1/2 w-full">
+                        <label class="input-label-tmp">Course Name</label>
+                        <input v-model="myName" type="text" class="w-full input-text-tmp" />
+                    </div>
+                    <div class="lg:w-1/2 w-full invisible" />
                 </div>
             </div>
             <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
@@ -17,14 +16,21 @@
                     <Select v-model:selection="myType" :elements="availableTypes" />
                 </div>
                 <div class="lg:w-1/2 w-full">
+                    <label class="input-label-tmp">ECTS</label>
+                    <input v-model.number="myEcts" min="0" type="number" class="lg:w-32 w-full input-text-tmp" />
+                </div>
+            </div>
+            <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
+                <div class="lg:w-1/2 w-full">
                     <label class="input-label-tmp">Language</label>
                     <Select v-model:selection="myLanguage" :elements="availableLanguages" />
                 </div>
+                <div class="lg:w-1/2 w-full invisible" />
             </div>
             <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
                 <div class="w-full">
                     <label class="input-label-tmp">Course Description</label>
-                    <textarea v-model="myDescription" class="w-full input-text-tmp" />
+                    <textarea v-model="myDescription" rows="5" class="w-full input-text-tmp" />
                 </div>
             </div>
         </div>
