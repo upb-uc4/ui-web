1f10038a1a3846af36a6b732baa34b59f2f82c58
Paul Kramer <pkramer1503@gmail.com>
2020-12-18 14:59:41 +0100

    Add EditCoursePlayground.vue

---
+++ src/views/common/dev/EditCoursePlayground.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-full">
+        <section-header title="Edit Course" />
+        <lecturer-section v-model:lecturerId="lecturerId" />
+        <button-section>
+            <template #left>
+                <button type="button" class="sm:w-32 w-full btn-secondary-remove-tmp">Delete</button>
+            </template>
+            <template #right>
+                <button class="sm:w-32 w-full btn-secondary-tmp">Cancel</button>
+                <button class="sm:w-48 w-full btn-tmp">Save</button>
+            </template>
+        </button-section>
+    </div>
+</template>
+
+<script lang="ts">
+    import SectionHeader from "@/components/common/section/SectionHeader.vue";
+    import ButtonSection from "@/components/common/section/ButtonSection.vue";
+    import { ref } from "vue";
+    import LecturerSection from "@/components/common/dev/playground/course/LecturerSection.vue";
+
+    export default {
+        name: "Playground",
+        components: {
+            SectionHeader,
+            ButtonSection,
+            LecturerSection,
+        },
+        setup() {
+            const lecturerId = ref("lecturer");
+
+            return {
+                lecturerId,
+            };
+        },
+    };
+</script>
