d7272bb14c7e99cf3f46ad7af1041eb2d273d8cc
Paul Kramer <pkramer1503@gmail.com>
2020-12-18 17:43:36 +0100

    Add RestrictionsSection.vue.

---
+++ src/components/common/dev/playground/course/RestrictionsSection.vue
@@ -0,0 +1,36 @@
+<template>
+    <BaseSection subtitle="Information of Participation Restrictions for the Course" title="Restrictions">
+        <div class="space-y-6">
+            <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
+                <div class="lg:w-1/2 w-full">
+                    <label class="input-label-tmp">Participation Limit</label>
+                    <input v-model.number="myMaxParticipant" min="0" type="number" class="w-full input-text-tmp" />
+                </div>
+            </div>
+        </div>
+    </BaseSection>
+</template>
+
+<script lang="ts">
+    import { useModelWrapper } from "@/use/helpers/ModelWrapper";
+    import BaseSection from "@/components/common/section/BaseSection.vue";
+
+    export default {
+        name: "RestrictionSection",
+        components: {
+            BaseSection,
+        },
+        props: {
+            maxParticipants: {
+                type: Number,
+                required: true,
+            },
+        },
+        emits: ["update:maxParticipants"],
+        setup(props: any, { emit }: any) {
+            return {
+                myMaxParticipant: useModelWrapper(props, emit, "maxParticipants"),
+            };
+        },
+    };
+</script>
