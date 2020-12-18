3d7fa2a8ccf9c949dc6965475c4ab98edf86f9f8
Paul Kramer <pkramer1503@gmail.com>
2020-12-18 17:44:28 +0100

    Add course info and restrictions to EditCoursePlayground.vue.

--- src/views/common/dev/EditCoursePlayground.vue
+++ src/views/common/dev/EditCoursePlayground.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="w-full">
         <section-header title="Edit Course" />
-        <lecturer-section v-model:lecturerId="lecturerId" />
+        <lecturer-section v-model:lecturerId="course.lecturerId" />
+        <course-info-section
+            v-model:name="course.courseName"
+            v-model:type="course.courseType"
+            v-model:language="course.courseLanguage"
+            v-model:ects="course.ects"
+            v-model:description="course.courseDescription"
+        />
+        <restrictions-section v-model:max-participants="course.maxParticipants" />
         <button-section>
             <template #left>
                 <button type="button" class="sm:w-32 w-full btn-secondary-remove-tmp">Delete</button>
@@ -19,19 +27,39 @@
     import ButtonSection from "@/components/common/section/ButtonSection.vue";
     import { ref } from "vue";
     import LecturerSection from "@/components/common/dev/playground/course/LecturerSection.vue";
+    import CourseInfoSection from "@/components/common/dev/playground/course/CourseInfoSection.vue";
+    import Course from "@/api/api_models/course_management/Course";
+    import { CourseType } from "@/entities/CourseType";
+    import { Language } from "@/entities/Language";
+    import RestrictionsSection from "@/components/common/dev/playground/course/RestrictionsSection.vue";
 
     export default {
         name: "Playground",
         components: {
+            CourseInfoSection,
             SectionHeader,
             ButtonSection,
             LecturerSection,
+            RestrictionsSection,
         },
         setup() {
-            const lecturerId = ref("lecturer");
+            const course = ref({
+                courseType: CourseType.LECTURE,
+                moduleIds: ["M1", "M2"],
+                courseId: "course1",
+                courseName: "Some Course Name",
+                lecturerId: "lecturer",
+                ects: 6,
+                maxParticipants: 40,
+                currentParticipants: 10,
+                courseDescription: "This course has a description.",
+                courseLanguage: Language.ENGLISH,
+                startDate: "2020-06-17",
+                endDate: "2020-09-25",
+            } as Course);
 
             return {
-                lecturerId,
+                course,
             };
         },
     };
