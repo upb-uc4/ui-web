687f01eef5807e2773ddf2f92f85d912369fb8f2
theovier <Theo@Harkenbusch.com>
2020-12-21 20:18:36 +0100

    add new course list card style for student and restyle student course list to match the other one

--- src/components/course/list/lecturer/LecturerCourse.vue
+++ src/components/course/list/lecturer/LecturerCourse.vue
@@ -1,62 +1,58 @@
 <template>
-    <div :id="'course_' + course.courseId" class="">
-        <div class="w-full px-4 sm:px-8">
-            <div class="flex items-center justify-between sm:justify-start space-x-4">
-                <div class="flex items-center space-x-2">
-                    <span
-                        class="w-3 h-3 rounded-full"
-                        :class="[
-                            { 'bg-lime-400': course.courseType === 'Lecture' },
-                            { 'bg-red-400': course.courseType === 'Seminar' },
-                            { 'bg-yellow-400': course.courseType === 'Project Group' },
-                        ]"
-                    />
-                    <span class="text-xs font-medium tracking-wide text-gray-500">
-                        {{ course.courseType }}
-                    </span>
-                </div>
-
-                <div class="text-xs font-medium tracking-wide text-gray-500 uppercase">{{ course.ects }} ECTS</div>
-                <div class="hidden sm:flex ml-4 text-xs font-medium tracking-wide text-gray-500">
-                    {{ course.courseLanguage }}
-                </div>
-                <!-- todo replace with flag component -->
-                <img :src="flagSrc" class="sm:hidden inline rounded ml-4 w-6 h-4 opacity-90" />
-                <div class="text-xs font-medium tracking-wide text-gray-500 flex items-center space-x-1">
-                    <i class="block fas fa-users" style="font-size: 1.25em" />
-                    <span> {{ course.currentParticipants }} / {{ course.maxParticipants }} </span>
-                </div>
+    <div :id="'course_' + course.courseId" class="w-full px-4 sm:px-8">
+        <div class="flex items-center justify-between sm:justify-start space-x-4">
+            <div class="flex items-center space-x-2">
+                <span
+                    class="w-3 h-3 rounded-full"
+                    :class="[
+                        { 'bg-lime-400': course.courseType === 'Lecture' },
+                        { 'bg-red-400': course.courseType === 'Seminar' },
+                        { 'bg-yellow-400': course.courseType === 'Project Group' },
+                    ]"
+                />
+                <span class="text-xs font-medium tracking-wide text-gray-500">{{ course.courseType }}</span>
             </div>
 
-            <div class="flex mt-2">
-                <div class="items-start w-2/3 md:w-5/6">
-                    <div id="courseName" class="text-lg navigation-link-tmp font-bold">
-                        {{ course.courseName }}
-                    </div>
-                    <router-link
-                        id="showLecturer"
-                        :to="{ name: 'profile.public', params: { username: course.lecturerId } }"
-                        class="mt-1 font-medium text-sm navigation-link-tmp"
-                    >
-                        {{ lecturerDisplayName }}
-                    </router-link>
+            <div class="text-xs font-medium tracking-wide text-gray-500 uppercase">{{ course.ects }} ECTS</div>
+            <div class="hidden sm:flex ml-4 text-xs font-medium tracking-wide text-gray-500">
+                {{ course.courseLanguage }}
+            </div>
+            <!-- todo replace with flag component -->
+            <img :src="flagSrc" class="sm:hidden inline rounded ml-4 w-6 h-4 opacity-90" />
+            <div class="text-xs font-medium tracking-wide text-gray-500 flex items-center space-x-1">
+                <i class="block fas fa-users" style="font-size: 1.25em" />
+                <span> {{ course.currentParticipants }} / {{ course.maxParticipants }} </span>
+            </div>
+        </div>
 
-                    <div class="w-full sm:flex sm:items-center">
-                        <div class="mt-1 sm:w-5/6 sm:mr-4">
-                            <read-more
-                                v-if="course.courseDescription !== ''"
-                                more-str="Show more"
-                                :text="course.courseDescription"
-                                less-str="Show less"
-                                :max-chars="240"
-                            ></read-more>
-                            <div v-else class="text-gray-500 text-sm italic">No course description available.</div>
-                        </div>
+        <div class="flex mt-2">
+            <div class="items-start w-2/3 md:w-5/6">
+                <div id="courseName" class="text-lg navigation-link-tmp font-bold">
+                    {{ course.courseName }}
+                </div>
+                <router-link
+                    id="showLecturer"
+                    :to="{ name: 'profile.public', params: { username: course.lecturerId } }"
+                    class="mt-1 font-medium text-sm navigation-link-tmp"
+                >
+                    {{ lecturerDisplayName }}
+                </router-link>
+
+                <div class="w-full sm:flex sm:items-center">
+                    <div class="mt-1 sm:w-5/6 sm:mr-4">
+                        <read-more
+                            v-if="course.courseDescription !== ''"
+                            more-str="Show more"
+                            :text="course.courseDescription"
+                            less-str="Show less"
+                            :max-chars="240"
+                        ></read-more>
+                        <div v-else class="text-gray-500 text-sm italic">No course description available.</div>
                     </div>
                 </div>
-                <div class="w-full md:w-1/6 mt-6 sm:mt-1 hidden">
-                    <button v-if="allowEdit" id="editCourse" class="w-full py-2 btn btn-gray-primary" @click="editCourse()">Edit</button>
-                </div>
+            </div>
+            <div class="w-full md:w-1/6 mt-6 sm:mt-1 hidden">
+                <button v-if="allowEdit" id="editCourse" class="w-full py-2 btn btn-gray-primary" @click="editCourse()">Edit</button>
             </div>
         </div>
     </div>
--- src/components/course/list/student/StudentCourse.vue
+++ src/components/course/list/student/StudentCourse.vue
@@ -1,50 +1,59 @@
 <template>
-    <div :id="'course_' + course.courseId" class="flex shadow-xl">
-        <div class="flex flex-col w-full p-4 sm:px-8 bg-white rounded-lg">
-            <div class="flex items-center justify-between sm:justify-start">
-                <span class="inline-block px-2 text-xs font-semibold tracking-wide text-teal-800 uppercase bg-teal-200 rounded-full">
-                    {{ course.courseType }}
-                </span>
-                <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">{{ course.ects }} ECTS</div>
-                <div class="hidden sm:flex ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-                    {{ course.courseLanguage }}
-                </div>
-                <!-- todo replace with flag component -->
-                <img :src="flagSrc" class="sm:hidden inline rounded ml-4 w-6 h-4 opacity-90" />
-                <div class="ml-4 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-                    <i class="inline text-lg fas fa-users" />
-                    {{ course.currentParticipants }} / {{ course.maxParticipants }}
-                </div>
+    <div :id="'course_' + course.courseId" class="w-full px-4 sm:px-8">
+        <div class="flex items-center justify-between sm:justify-start space-x-4">
+            <div class="flex items-center space-x-2">
+                <span
+                    class="w-3 h-3 rounded-full"
+                    :class="[
+                        { 'bg-lime-400': course.courseType === 'Lecture' },
+                        { 'bg-red-400': course.courseType === 'Seminar' },
+                        { 'bg-yellow-400': course.courseType === 'Project Group' },
+                    ]"
+                />
+                <span class="text-xs font-medium tracking-wide text-gray-500">{{ course.courseType }}</span>
             </div>
 
-            <div class="flex flex-wrap mb-4">
-                <div class="flex flex-col items-start w-2/3 md:w-5/6">
-                    <div id="courseName" class="mt-2 text-2xl font-semibold leading-tight text-gray-900">
-                        {{ course.courseName }}
-                    </div>
-                    <router-link
-                        id="showLecturer"
-                        :to="{ name: 'profile.public', params: { username: course.lecturerId } }"
-                        class="mt-1 font-semibold navigation-link hover:cursor-pointer"
-                    >
-                        {{ lecturerDisplayName }}
-                    </router-link>
+            <div class="text-xs font-medium tracking-wide text-gray-500 uppercase">{{ course.ects }} ECTS</div>
+            <div class="hidden sm:flex ml-4 text-xs font-medium tracking-wide text-gray-500">
+                {{ course.courseLanguage }}
+            </div>
+            <!-- todo replace with flag component -->
+            <img :src="flagSrc" class="sm:hidden inline rounded ml-4 w-6 h-4 opacity-90" />
+            <div class="text-xs font-medium tracking-wide text-gray-500 flex items-center space-x-1">
+                <i class="block fas fa-users" style="font-size: 1.25em" />
+                <span> {{ course.currentParticipants }} / {{ course.maxParticipants }} </span>
+            </div>
+        </div>
 
-                    <div class="w-full sm:flex sm:items-center">
-                        <div class="mt-3 sm:w-5/6 sm:mr-4">
-                            <read-more
-                                more-str="Show more"
-                                :text="course.courseDescription"
-                                less-str="Show less"
-                                :max-chars="180"
-                            ></read-more>
-                        </div>
+        <div class="flex mt-2">
+            <div class="items-start w-2/3 md:w-5/6">
+                <div id="courseName" class="text-lg navigation-link-tmp font-bold">
+                    {{ course.courseName }}
+                </div>
+                <router-link
+                    id="showLecturer"
+                    :to="{ name: 'profile.public', params: { username: course.lecturerId } }"
+                    class="mt-1 font-medium text-sm navigation-link-tmp"
+                >
+                    {{ lecturerDisplayName }}
+                </router-link>
+
+                <div class="w-full sm:flex sm:items-center">
+                    <div class="mt-1 sm:w-5/6 sm:mr-4">
+                        <read-more
+                            v-if="course.courseDescription !== ''"
+                            more-str="Show more"
+                            :text="course.courseDescription"
+                            less-str="Show less"
+                            :max-chars="240"
+                        ></read-more>
+                        <div v-else class="text-gray-500 text-sm italic">No course description available.</div>
                     </div>
                 </div>
-                <div class="w-full md:w-1/6 mt-6 sm:mt-1">
-                    <button id="joinCourse" class="w-full py-2 btn btn-blue-primary" :disabled="isCourseFull">Join</button>
-                    <p v-if="isCourseFull" class="font-semibold text-red-700">This course is already full.</p>
-                </div>
+            </div>
+            <div class="w-full md:w-1/6 mt-6 sm:mt-1">
+                <button id="joinCourse" class="w-full btn btn-tmp" :disabled="isCourseFull">Join</button>
+                <p v-if="isCourseFull" class="input-label-error-tmp">Course is already full.</p>
             </div>
         </div>
     </div>
