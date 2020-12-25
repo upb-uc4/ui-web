da1520dbb49da4cce90d9258a295bf5d9af1451d
theovier <Theo@Harkenbusch.com>
2020-12-25 14:20:06 +0100

    adjust about headline to match the other views headlines

--- src/views/common/About.vue
+++ src/views/common/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-w-screen-lg mx-auto w-full space-y-8">
-        <h1 class="text-2xl font-bold text-center">About Us</h1>
+        <h1 class="text-2xl font-medium text-gray-800 text-center">About Us</h1>
         <hr class="my-4" />
         <div class="lg:flex w-full space-y-8 lg:space-y-0">
             <div class="lg:w-1/5">
