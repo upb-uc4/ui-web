9ad87536befad143c9390b87f6cccfd30bfec753
theovier <Theo@Harkenbusch.com>
2020-12-28 14:44:57 +0100

    darkmode for spinner

--- src/components/common/loading/Spinner.vue
+++ src/components/common/loading/Spinner.vue
@@ -2,7 +2,7 @@
     <div class="mt-10 items-center justify-center flex flex-col space-y-4">
         <img src="@/assets/loading-spinner-alt.svg" class="h-20 w-20" />
         <div>
-            <h2 class="block font-medium text-gray-800">{{ title }}</h2>
+            <h2 class="block font-medium text-gray-800 dark:text-gray-300">{{ title }}</h2>
             <h3 class="block text-sm text-gray-500">{{ subtitle }}</h3>
         </div>
     </div>
