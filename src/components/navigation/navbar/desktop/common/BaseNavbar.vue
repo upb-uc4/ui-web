fbd281643a10abf37bdbb818fd9c3aa3d12ec600
theovier <Theo@Harkenbusch.com>
2020-12-28 16:15:25 +0100

    use relative path for logo

--- src/components/navigation/navbar/desktop/common/BaseNavbar.vue
+++ src/components/navigation/navbar/desktop/common/BaseNavbar.vue
@@ -2,7 +2,7 @@
     <header class="flex items-center justify-between bg-grayalt bg-normalgray-900 md:h-16 min-h-16">
         <div class="flex items-center w-full">
             <div class="items-center h-16 pl-8 rounded-lg cursor-pointer outline-none" title="Home" @click="routeLogo">
-                <img src="../../../../../assets/logo/logo.svg" alt="uc4-logo" class="h-full py-3" />
+                <img src="@/assets/logo/logo.svg" alt="uc4-logo" class="h-full py-3" />
             </div>
 
             <div class="flex w-full rounded-lg pl-8 lg:pr-8 pr-4">
