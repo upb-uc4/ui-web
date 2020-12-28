753d42a936abee5c2585d06a186378457344341c
theovier <Theo@Harkenbusch.com>
2020-12-28 14:55:27 +0100

    darkmode account list headline and filtering

--- src/views/admin/AdminAccountList.vue
+++ src/views/admin/AdminAccountList.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-w-screen-lg mx-auto w-full">
-        <div class="text-2xl text-center font-medium text-gray-800 mb-4">User Management</div>
+        <div class="text-2xl text-center font-medium text-gray-800 dark:text-gray-300 mb-4">User Management</div>
         <div>
             <div class="flex flex-col-reverse md:flex-row items-center justify-between md:space-x-2 space-y-2 space-y-reverse md:space-y-0">
                 <div class="w-full md:flex items-center md:space-x-2 space-y-2 md:space-y-0">
@@ -19,8 +19,8 @@
                     </router-link>
                 </div>
             </div>
-            <hr class="my-4" />
-            <div v-show="isFiltering()" class="text-gray-800 text-sm">
+            <hr class="my-4 dark:border-normalgray-700" />
+            <div v-show="isFiltering()" class="text-gray-800 dark:text-gray-300 text-sm">
                 <div class="flex justify-between">
                     <div>
                         <div v-if="isFilteringRole() && isFilteringStatus()">
@@ -58,7 +58,7 @@
                     </div>
                     <div class="btn-tertiary-tmp" @click="clearFilter()">Clear filter</div>
                 </div>
-                <hr class="mt-4 mb-8" />
+                <hr class="mt-4 mb-8 dark:border-normalgray-700" />
             </div>
             <div>
                 <accountList
