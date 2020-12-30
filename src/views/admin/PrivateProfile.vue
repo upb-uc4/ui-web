9b2cd671d068acffefa7be4dc5ba840167dd2813
Paul Kramer <pkramer1503@gmail.com>
2020-12-30 15:17:47 +0100

    Change from edit cancel save to update workflow using objectModelWrapper.

--- src/views/admin/PrivateProfile.vue
+++ src/views/admin/PrivateProfile.vue
@@ -2,14 +2,9 @@
     <div>
         <section-header :title="title" />
         <profile-picture-section />
-        <personal-section
-            v-model:first-name="admin.firstName"
-            v-model:last-name="admin.lastName"
-            v-model:birth-date="admin.birthDate"
-            :readonly="true"
-        />
-        <editable-contact-section v-model:email="admin.email" v-model:phone-number="admin.phoneNumber" @save="onSave()" />
-        <editable-address-section v-model:address="admin.address" @save="onSave()" />
+        <personal-section v-model:first-name="firstName" v-model:last-name="lastName" v-model:birth-date="birthDate" :readonly="true" />
+        <contact-section v-model:email="email" v-model:phone-number="phoneNumber" :error-bag="errorBag" />
+        <address-section v-model:address="address" :error-bag="errorBag" />
     </div>
 </template>
 
@@ -17,20 +12,21 @@
     import SectionHeader from "@/components/common/section/SectionHeader.vue";
     import ProfilePictureSection from "@/components/common/dev/playground/ProfilePictureSection.vue";
     import PersonalSection from "@/components/common/dev/playground/PersonalSection.vue";
-    import EditableContactSection from "@/components/common/dev/playground/EditableContactSection.vue";
-    import EditableAddressSection from "@/components/common/dev/playground/EditableAddressSection.vue";
+    import ContactSection from "@/components/common/dev/playground/ContactSection.vue";
+    import AddressSection from "@/components/common/dev/playground/AddressSection.vue";
 
-    import { computed, reactive, ref } from "vue";
+    import { computed } from "vue";
     import Admin from "@/api/api_models/user_management/Admin";
     import ErrorBag from "@/use/helpers/ErrorBag";
+    import { useObjectModelWrapper } from "@/use/helpers/ModelWrapper";
 
     export default {
         components: {
             SectionHeader,
             ProfilePictureSection,
             PersonalSection,
-            EditableAddressSection,
-            EditableContactSection,
+            AddressSection,
+            ContactSection,
         },
         props: {
             user: {
@@ -42,21 +38,19 @@
                 type: Object as () => ErrorBag,
             },
         },
-        emits: ["update:user", "save"],
+        emits: ["update:user"],
         setup(props: any, { emit }: any) {
             //todo move the components to the right location once the old components are deleted
-            const admin = reactive(props.user);
-            const title = computed(() => `${admin.firstName} ${admin.lastName} (@${admin.username})`);
-
-            function onSave() {
-                emit("update:user", admin);
-                emit("save");
-            }
+            const title = computed(() => `${props.user.firstName} ${props.user.lastName} (@${props.user.username})`);
 
             return {
                 title,
-                admin,
-                onSave,
+                firstName: useObjectModelWrapper(props, emit, "user", "firstName"),
+                lastName: useObjectModelWrapper(props, emit, "user", "lastName"),
+                birthDate: useObjectModelWrapper(props, emit, "user", "birthDate"),
+                email: useObjectModelWrapper(props, emit, "user", "email"),
+                phoneNumber: useObjectModelWrapper(props, emit, "user", "phoneNumber"),
+                address: useObjectModelWrapper(props, emit, "user", "address"),
             };
         },
     };
