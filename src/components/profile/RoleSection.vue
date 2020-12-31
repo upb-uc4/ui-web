44c6dc44b7526597f72ade4930840381972625d6
Paul Kramer <pkramer1503@gmail.com>
2020-12-31 16:00:23 +0100

    Add role section for account creation.

---
+++ src/components/profile/RoleSection.vue
@@ -0,0 +1,59 @@
+<template>
+    <base-section title="Select a Role" subtitle="">
+        <div class="w-full">
+            <div id="roleSelection" class="flex w-full">
+                <div v-for="vrole in roles" :key="vrole" class="mr-4">
+                    <label class="flex items-center">
+                        <input
+                            :id="'role-' + vrole"
+                            v-model="accountRole"
+                            type="radio"
+                            class="form-radio radio"
+                            :disabled="editMode"
+                            :value="vrole"
+                        />
+                        <span class="ml-4 mt-1 input-label-tmp">{{ vrole }}</span>
+                    </label>
+                </div>
+            </div>
+            <label v-if="errorBag.hasNested('role')" class="input-label-error-tmp">{{ errorBag.getNested("role") }}</label>
+        </div>
+    </base-section>
+</template>
+
+<script lang="ts">
+    import { Role } from "@/entities/Role";
+    import { useModelWrapper } from "@/use/helpers/ModelWrapper";
+    import ErrorBag from "@/use/helpers/ErrorBag";
+    import BaseSection from "@/components/common/section/BaseSection.vue";
+
+    export default {
+        name: "RoleSection",
+        components: {
+            BaseSection,
+        },
+        props: {
+            errorBag: {
+                type: ErrorBag,
+                required: true,
+            },
+            role: {
+                type: String,
+                required: true,
+            },
+            editMode: {
+                type: Boolean,
+                required: true,
+            },
+        },
+        emits: ["update:role"],
+        setup(props: any, { emit }: any) {
+            let roles = Object.values(Role).filter((e) => e != Role.NONE);
+
+            return {
+                roles,
+                accountRole: useModelWrapper(props, emit, "role"),
+            };
+        },
+    };
+</script>
