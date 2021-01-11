fd820c72a31c1013b630bd46d74235c519c03332
Tobias Wiese <tobiaswiese@online.de>
2021-01-11 11:11:54 +0100

    create toast for creation of an operation

--- src/use/helpers/Toasts.ts
+++ src/use/helpers/Toasts.ts
@@ -1,8 +1,20 @@
 //import ReportBug from "@/components/common/ReportBug.vue";
 //import { markRaw } from "vue";
 import { useToast } from "@/toast";
+import Router from "@/use/router";
 
 const toast = useToast();
+
+function routeToDashboard() {
+    Router.push({ name: "welcome" });
+}
+
+export function showOperationCreatedToast(operationType: String) {
+    toast.info(
+        `Successfully created ${operationType} request. You can check its current state in your dashboard. Click here to go there immediately.`,
+        { onClick: routeToDashboard }
+    );
+}
 
 export function showNetworkErrorToast() {
     toast.error("Network error: make sure to establish a stable internet connection.");
