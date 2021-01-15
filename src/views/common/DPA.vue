035761fc58f4ac977f421fa7ac3aeddf0b9ba66b
Tobias Wiese <tobiaswiese@online.de>
2021-01-15 15:52:22 +0100

    add a placeholder data protection agreement page

---
+++ src/views/common/DPA.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full mt-20">
+        <img src="../../assets/logo/logo_long_title_right.svg" alt="" />
+        <h1 class="text-3xl text-gray-800 mt-20">Data Protection Agreement</h1>
+        <p class="mt-5 text-xl text-gray-700">
+            This application is a result of an educational project group within the master program of
+            <a
+                class="text-blue-700 hover:text-blue-800 hover:underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://cs.uni-paderborn.de/en/studies/degree-programs/computer-science/"
+            >
+                Paderborn University
+            </a>
+            . Please refer to the legal notice of the university.
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {};
+</script>
