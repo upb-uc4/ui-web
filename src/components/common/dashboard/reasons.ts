365d3edf96adc493c10d48fdbb9687c98289e3ef
Tobias Wiese <tobiaswiese@online.de>
2021-01-22 11:04:11 +0100

    remove mock rejection reason

--- src/components/common/dashboard/reasons.ts
+++ src/components/common/dashboard/reasons.ts
@@ -1,6 +1,5 @@
 export enum RejectionReasons {
     FEE_NOT_TRANSFERRED = "Semester fees not transferred",
     INVALID_MATRICULATION = "Matriculation invalid",
-    ARBITRARY_REASON = "Reason",
     OTHER = "Other",
 }
