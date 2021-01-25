d2a6474dfb7950788b8ef25e7482826cd0eb397a
theovier <Theo@Harkenbusch.com>
2021-01-25 16:59:49 +0100

    rm unused month const
    Co-authored-by: tobias-wiese <tobiaswiese@online.de>

--- src/entities/Month.ts
+++ src/entities/Month.ts
@@ -28,19 +28,4 @@ export let months = [
     "December",
 ];
 
-export let monthsToNumber = {
-    January: "01",
-    February: "02",
-    March: "03",
-    April: "04",
-    May: "05",
-    June: "06",
-    July: "07",
-    August: "08",
-    September: "09",
-    October: "10",
-    November: "11",
-    December: "12",
-};
-
 export let days = Array.from({ length: 31 }, (_, i) => String(i + 1));
