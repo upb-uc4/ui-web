db730c8cedf7a0451f8c315d0f2f69584d5542ae
nklsd <n.doppelstein@web.de>
2021-01-26 18:05:06 +0100

    make lecturer clickable on course presenter section

--- src/components/course/info/sections/PresenterSection.vue
+++ src/components/course/info/sections/PresenterSection.vue
@@ -7,7 +7,9 @@
             <div class="lg:flex lg:space-x-12 lg:space-y-0 space-y-4 w-full">
                 <div class="lg:w-1/2 w-full">
                     <label class="input-label">Lecturer</label>
-                    <input class="input-text w-full" readonly :value="name" />
+                    <label id="showLecturer" class="mt-1 font-semibold navigation-link cursor-pointer" @click.stop="showLecturer()">
+                        {{ name }}
+                    </label>
                 </div>
                 <div class="lg:w-1/2 w-full invisible" />
             </div>
@@ -18,6 +20,7 @@
 <script lang="ts">
     import BaseSection from "@/components/common/section/BaseSection.vue";
     import LecturerEntity from "@/entities/LecturerEntity";
+    import Router from "@/use/router/index";
     import { computed } from "vue";
 
     export default {
@@ -35,9 +38,14 @@
             const profilePicture = process.env.VUE_APP_API_BASE_URL + "/user-management/users/" + props.lecturer.username + "/image";
             const name = computed(() => `${props.lecturer.firstName} ${props.lecturer.lastName}`);
 
+            function showLecturer() {
+                Router.push({ name: "profile.public", params: { username: props.lecturer.username } });
+            }
+
             return {
                 profilePicture,
                 name,
+                showLecturer,
             };
         },
     };
