d136081bd4390d537347f9566dffc0915ec0e34c
Tobias Wiese <tobiaswiese@online.de>
2021-03-03 17:13:52 +0100

    add placeholder component

---
+++ src/components/common/ListPlaceholder.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-full h-full flex items-center justify-center">
+        <label class="text-gray-500 font-medium flex"> No relevant {{ contentType }} found. </label>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "DefaultListContent",
+        props: {
+            contentType: {
+                type: String,
+                required: false,
+                default: "content",
+            },
+        },
+    };
+</script>
